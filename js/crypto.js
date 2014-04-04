82150f4bb23e0a499c466a26ea244232ef8e1a67
lilia <liliakai@gmail.com>
2014-04-04 00:18:07 -0700

    Add CryptoJS impl of hmac-sha256 and aes-ctr
    Just a holdover til Web Crypto gets here, I Promise.

--- js/crypto.js
+++ js/crypto.js
@@ -2,11 +2,55 @@ window.crypto.subtle = (function() {
 	if (window.crypto.subtle !== undefined && window.crypto.subtle !== null) {
 			return window.crypto.subtle;
 	} else {
+		// private implementation functions
+		function HmacSHA256(input, key) {
+			return CryptoJS.HmacSHA256(
+					CryptoJS.lib.WordArray.create(toArrayBuffer(input)),
+					CryptoJS.enc.Latin1.parse(getString(key))
+				).toString(CryptoJS.enc.Latin1);
+		};
+
+		function encryptAESCTR(plaintext, key, counter) {
+				return CryptoJS.AES.encrypt(CryptoJS.enc.Latin1.parse(getString(plaintext)),
+						CryptoJS.enc.Latin1.parse(getString(key)),
+						{mode: CryptoJS.mode.CTR, iv: CryptoJS.enc.Latin1.parse(""),
+							padding: CryptoJS.pad.NoPadding})
+					.ciphertext.toString(CryptoJS.enc.Latin1);
+		};
+
+		function decryptAESCTR(ciphertext, key, counter) {
+				return CryptoJS.AES.decrypt(btoa(getString(ciphertext)),
+						CryptoJS.enc.Latin1.parse(getString(key)),
+						{mode: CryptoJS.mode.CTR, iv: CryptoJS.enc.Latin1.parse(""),
+							padding: CryptoJS.pad.NoPadding})
+					.toString(CryptoJS.enc.Latin1);
+		};
+
+		// utility function for connecting front and back ends via promises
+		// Takes an implementation function and 0 or more arguments
+		function promise(implementation) {
+			var args = Array.prototype.slice.call(arguments);
+			args.shift();
+			return new Promise(function(resolve) {
+				resolve(implementation.apply(this, args));
+			});
+		}
+
+		// public interface functions
 		function encrypt(algorithm, key, data) {
+			if (algorithm.name === "AES-CTR") {
+					return promise(encryptAESCTR, data, key, algorithm.counter);
+			}
 		};
 		function decrypt(algorithm, key, data) {
+			if (algorithm.name === "AES-CTR") {
+					return promise(decryptAESCTR, data, key, algorithm.counter);
+			}
 		};
 		function sign(algorithm, key, data) {
+			if (algorithm.name === "HMAC" && algorithm.hash === "SHA256") {
+				return promise(HmacSHA256, data, key);
+			}
 		};
 		function verify(algorithm, key, signature, data) {
 		};
@@ -24,7 +68,8 @@ window.crypto.subtle = (function() {
 		};
 		function wrapKey(format, key, wrappingKey, wrapAlgorithm) {
 		};
-		function unwrapKey(format, wrappedKey, unwrappingKey, unwrapAlgorithm, unwrappedKeyAlgorithm, extractable, keyUsages) {
+		function unwrapKey(format, wrappedKey, unwrappingKey, unwrapAlgorithm,
+				unwrappedKeyAlgorithm, extractable, keyUsages) {
 		};
 
 		return {
