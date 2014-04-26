1ff82221437a59f72b415612bc6d41a4574f7526
lilia <liliakai@gmail.com>
2014-04-26 00:49:50 -0700

    Correct argument names

--- js/crypto.js
+++ js/crypto.js
@@ -2,12 +2,12 @@ function HmacSHA256(key, input) {
 	return window.crypto.subtle.sign({name: "HMAC", hash: "SHA-256"}, key, input);
 }
 
-function encryptAESCTR(plaintext, key, counter) {
+function encryptAESCTR(input, key, counter) {
 	return window.crypto.subtle.encrypt(
 		{name: "AES-CTR", hash: "SHA-256", counter : counter}, key, input);
 }
 
-function decryptAESCTR(ciphertext, key, counter) {
+function decryptAESCTR(input, key, counter) {
 	return window.crypto.subtle.decrypt(
 		{name: "AES-CTR", hash: "SHA-256", counter: counter}, key, input);
 }
