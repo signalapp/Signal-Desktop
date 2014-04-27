127a8f3b1ff75b4a30b67940a5d81804ea6dd2fb
lilia <liliakai@gmail.com>
2014-04-27 14:42:29 -0700

    Remove unneeded params

--- js/crypto.js
+++ js/crypto.js
@@ -3,16 +3,13 @@ function HmacSHA256(key, input) {
 }
 
 function encryptAESCTR(input, key, counter) {
-	return window.crypto.subtle.encrypt(
-		{name: "AES-CTR", hash: "SHA-256", counter: counter}, key, input);
+	return window.crypto.subtle.encrypt({name: "AES-CTR", counter: counter}, key, input);
 }
 
 function decryptAESCTR(input, key, counter) {
-	return window.crypto.subtle.decrypt(
-		{name: "AES-CTR", hash: "SHA-256", counter: counter}, key, input);
+	return window.crypto.subtle.decrypt({name: "AES-CTR", counter: counter}, key, input);
 }
 
 function decryptAESCBC(input, key, iv) {
-	return window.crypto.subtle.decrypt(
-		{name: "AES-CBC", hash: "SHA-256", iv: iv}, key, input);
+	return window.crypto.subtle.decrypt({name: "AES-CBC", iv: iv}, key, input);
 }
