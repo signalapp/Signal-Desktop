8d408e6d8f429ac12ebeb6c6ae183d124614b36f
Matt Corallo <git@bluematt.me>
2014-05-14 14:58:12 -0400

    THOUGH SALL USE TABSTOP AND SHIFTWIDTH 4 (so that indents read right)

--- js/popup.js
+++ js/popup.js
@@ -1,4 +1,5 @@
-/*
+/* vim: ts=4:sw=4
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
