0cf5ae3bbfe3df843a9a7a6d316a72e383312c3c
lilia <liliakai@gmail.com>
2014-09-04 00:25:08 -0700

    Reorganize message view test a bit

--- test/views/message_view_test.js
+++ test/views/message_view_test.js
@@ -1,27 +1,25 @@
 describe('MessageView', function() {
   var message = Whisper.Messages.add({
-    threadId: 'test-thread',
     body: 'hello world',
     type: 'outgoing',
     timestamp: new Date().getTime()
   });
 
-  describe('#render', function() {
+  it('should display the message text', function() {
     var view = new Whisper.MessageView({model: message});
-    var div = $('<div>').append(view.render().$el);
+    assert.match(view.render().$el.html(), /hello world/);
+  });
 
-    it('should include the message text', function() {
-      assert.match(view.$el.html(), /hello world/);
-    });
+  it('should auto-update the message text', function() {
+    var view = new Whisper.MessageView({model: message});
+    message.set('body', 'goodbye world');
+    assert.match(view.$el.html(), /goodbye world/);
+  });
 
-    it('should auto-update the message text', function() {
-      message.set('body', 'goodbye world');
-      assert.match(view.$el.html(), /goodbye world/);
-    });
-
-    it('should go away when the model is destroyed', function() {
-      message.destroy();
-      assert.strictEqual(div.find(view.$el).length, 0);
-    });
+  it('should go away when the model is destroyed', function() {
+    var view = new Whisper.MessageView({model: message});
+    var div = $('<div>').append(view.$el);
+    message.destroy();
+    assert.strictEqual(div.find(view.$el).length, 0);
   });
 });
