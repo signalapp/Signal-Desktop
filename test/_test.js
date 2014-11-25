5638b20046d81fdc3d706c92feaa020a28866ed8
Arlo Breault <arlolra@gmail.com>
2014-11-24 17:28:51 -0800

    Expose test results to sauce
     * Do what blanket does, adding another level of indirection in the
       reporting.

--- test/_test.js
+++ test/_test.js
@@ -16,6 +16,43 @@
 mocha.setup("bdd");
 window.assert = chai.assert;
 
+(function() {
+  var OriginalReporter = mocha._reporter;
+
+  var SauceReporter = function(runner) {
+    var failedTests = [];
+
+    runner.on('end', function() {
+      window.mochaResults = runner.stats;
+      window.mochaResults.reports = failedTests;
+    });
+
+    runner.on('fail', function(test, err) {
+      var flattenTitles = function(test) {
+        var titles = [];
+        while (test.parent.title) {
+          titles.push(test.parent.title);
+          test = test.parent;
+        }
+        return titles.reverse();
+      };
+      failedTests.push({
+        name: test.title,
+        result: false,
+        message: err.message,
+        stack: err.stack,
+        titles: flattenTitles(test)
+      });
+    });
+
+    new OriginalReporter(runner);
+  };
+
+  SauceReporter.prototype = OriginalReporter.prototype;
+
+  mocha.reporter(SauceReporter);
+}());
+
 /*
  * global helpers for tests
  */
