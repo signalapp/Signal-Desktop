538198b1f81854108158cf0da3c95de90bd08e90
Emily Chao <elchao96@live.com>
2015-01-11 16:48:16 -1000

    making slight edits to the scope of variables for the view

--- js/views/phone-input-view.js
+++ js/views/phone-input-view.js
@@ -1,7 +1,7 @@
+var Whisper = Whisper || {};
+
 (function () {
 	'use strict';
-	window.Whisper = window.Whisper || {};
-
 	Whisper.PhoneInputView = Backbone.View.extend({
 		className: 'phone-input',
 		initialize: function() {
