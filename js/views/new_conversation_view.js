273d364bf59c587be14be9a3e3ec858f18f6a3a7
Emily Chao <elchao96@live.com>
2015-01-13 20:29:51 -1000

    phone-validation finish up

--- js/views/new_conversation_view.js
+++ js/views/new_conversation_view.js
@@ -54,7 +54,6 @@ var Whisper = Whisper || {};
       this.template = $('#new-message-form').html();
       Mustache.parse(this.template);
       this.$el.html($(Mustache.render(this.template)));
-      /*this.input = new MessageRecipientInputView({el: this.$el.find('input.number')});*/
       this.input = new Whisper.PhoneInputView({el: this.$el.find('div.phone-number-input')});
       this.fileInput = new Whisper.FileInputView({el: this.$el.find('.attachments')});
       this.$el.find('#phone-number-input').append(this.input.render().el);
