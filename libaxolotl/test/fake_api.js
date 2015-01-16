298c8624b2fc9d9a9eeab9592547386195368e17
Matt Corallo <git@bluematt.me>
2015-01-15 23:38:29 -1000

    Move test vectors to libaxolotl

---
+++ libaxolotl/test/fake_api.js
@@ -0,0 +1,43 @@
+'use strict';
+
+var testSessionMap = {};
+
+;(function() {
+    window.axolotl = window.axolotl || {};
+    window.axolotl.api = {
+        getMyRegistrationId: function() {
+            return window.myRegistrationId;
+        },
+        storage: {
+            put: function(key, value) {
+                if (value === undefined)
+                    throw new Error("Tried to store undefined");
+                localStorage.setItem(key, textsecure.utils.jsonThing(value));
+            },
+            get: function(key, defaultValue) {
+                var value = localStorage.getItem(key);
+                if (value === null)
+                    return defaultValue;
+                return JSON.parse(value);
+            },
+            remove: function(key) {
+                localStorage.removeItem(key);
+            },
+
+            sessions: {
+                get: function(identifier) {
+                    return testSessionMap[identifier];
+                },
+                put: function(identifier, record) {
+                    testSessionMap[identifier] = record;
+                }
+            }
+        },
+        updateKeys: function(keys) {
+            return textsecure.api.registerKeys(keys).catch(function(e) {
+                //TODO: Notify the user somehow?
+                console.error(e);
+            });
+        },
+    };
+})();
