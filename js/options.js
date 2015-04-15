signalapp/Signal-Desktop

commit d3c158f4cfcd3f1a1a97cd9a61b230a150510e53

Move libaxolotl out-of-tree

js/options.js
@@ -26,7 +26,7 @@
         } else {
             $('#init-setup').show().addClass('in');
             $('#status').text("Connecting...");
-            axolotl.protocol.createIdentityKeyRecvSocket().then(function(cryptoInfo) {
+            textsecure.protocol_wrapper.createIdentityKeyRecvSocket().then(function(cryptoInfo) {
                 var qrCode = new QRCode(document.getElementById('qr'));
                 var socket = textsecure.api.getTempWebsocket();
                 new WebSocketResource(socket, function(request) {

Gruntfile.js
@@ -10,11 +10,6 @@ module.exports = function(grunt) {
     components.push('components/' + bower.concat.app[i] + '/**/*.js');
   }
 
-  var libaxolotlcomponents = [];
-  for (i in bower.concat.libaxolotl) {
-    libaxolotlcomponents.push('components/' + bower.concat.libaxolotl[i] + '/**/*.js');
-  }
-
   var libtextsecurecomponents = [];
   for (i in bower.concat.libtextsecure) {
     libtextsecurecomponents.push('components/' + bower.concat.libtextsecure[i] + '/**/*.js');
@@ -27,42 +22,9 @@ module.exports = function(grunt) {
         src: components,
         dest: 'js/components.js',
       },
-      libaxolotlcomponents: {
-        src: libaxolotlcomponents,
-        dest: 'libaxolotl/components.js',
-      },
       libtextsecurecomponents: {
         src: libtextsecurecomponents,
         dest: 'libtextsecure/components.js',
-      },
-      curve25519: {
-        src: [
-          'build/curve25519_compiled.js',
-          'build/curve25519.js',
-        ],
-        dest: 'libaxolotl/curve25519_concat.js',
-        options: {
-          banner: ';(function(){\n',
-          footer: '\n})();'
-        }
-      },
-      webcrypto: {
-        src: [
-          'components/cryptojs/src/core.js',
-          'components/cryptojs/src/sha256.js',
-          'components/cryptojs/src/hmac.js',
-          'components/cryptojs/src/enc-base64.js',
-          'components/cryptojs/src/md5.js',
-          'components/cryptojs/src/evpkdf.js',
-          'components/cryptojs/src/cipher-core.js',
-          'components/cryptojs/src/aes.js',
-          'build/webcrypto.js'
-        ],
-        dest: 'libaxolotl/webcrypto_concat.js',
-        options: {
-          banner: ';(function(){\n',
-          footer: '\n})();'
-        }
       },
       test: {
         src: [
@@ -72,25 +34,12 @@ module.exports = function(grunt) {
         ],
         dest: 'test/test.js',
       },
-      libaxolotl: {
-        src: [
-          'libaxolotl/curve25519_concat.js',
-          'libaxolotl/webcrypto_concat.js',
-          'libaxolotl/components.js',
-
-          'libaxolotl/crypto.js',
-          'libaxolotl/protocol.js',
-          'libaxolotl/protobufs.js',
-          'libaxolotl/session_storage.js',
-        ],
-        dest: 'libtextsecure/libaxolotl_concat.js',
-      },
       //TODO: Move errors back down?
       libtextsecure: {
         src: [
           'libtextsecure/errors.js',
+          'libtextsecure/libaxolotl.js',
           'libtextsecure/axolotl_wrapper.js',
-          'libtextsecure/libaxolotl_concat.js',
 
           'libtextsecure/crypto.js',
           'libtextsecure/storage.js',
@@ -113,15 +62,6 @@ module.exports = function(grunt) {
           'libtextsecure/test/_test.js'
         ],
         dest: 'libtextsecure/test/test.js',
-      },
-      libaxolotltest: {
-        src: [
-          'components/mocha/mocha.js',
-          'components/chai/chai.js',
-          'components/jquery/**/*.js',
-          'libaxolotl/test/_test.js'
-        ],
-        dest: 'libaxolotl/test/test.js',
       }
     },
     sass: {
@@ -206,7 +146,6 @@ module.exports = function(grunt) {
           urls: [
             'http://127.0.0.1:9999/test/index.html',
             'http://127.0.0.1:9999/libtextsecure/test/index.html',
-            'http://127.0.0.1:9999/libaxolotl/test/index.html'
           ],
           build: process.env.TRAVIS_JOB_ID,
           browsers: [

libaxolotl/crypto.js
@@ -1,134 +1,0 @@
-/* vim: ts=4:sw=4
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-;(function() {
-    window.axolotl = window.axolotl || {};
-
-    /*
-     *  axolotl.crypto
-     *    glues together various implementations into a single interface
-     *    for all low-level crypto operations,
-     */
-
-    window.axolotl.crypto = {
-        getRandomBytes: function(size) {
-            var array = new Uint8Array(size);
-            window.crypto.getRandomValues(array);
-            return array.buffer;
-        },
-        encrypt: function(key, data, iv) {
-            return window.crypto.subtle.importKey('raw', key, {name: 'AES-CBC'}, false, ['encrypt']).then(function(key) {
-                return window.crypto.subtle.encrypt({name: 'AES-CBC', iv: new Uint8Array(iv)}, key, data);
-            });
-        },
-        decrypt: function(key, data, iv) {
-            return window.crypto.subtle.importKey('raw', key, {name: 'AES-CBC'}, false, ['decrypt']).then(function(key) {
-                return window.crypto.subtle.decrypt({name: 'AES-CBC', iv: new Uint8Array(iv)}, key, data);
-            });
-        },
-        sign: function(key, data) {
-            return window.crypto.subtle.importKey('raw', key, {name: 'HMAC', hash: {name: 'SHA-256'}}, false, ['sign']).then(function(key) {
-                return window.crypto.subtle.sign( {name: 'HMAC', hash: 'SHA-256'}, key, data);
-            });
-        },
-
-        HKDF: function(input, salt, info) {
-            // Specific implementation of RFC 5869 that only returns the first 3 32-byte chunks
-            // TODO: We dont always need the third chunk, we might skip it
-            return window.axolotl.crypto.sign(salt, input).then(function(PRK) {
-                var infoBuffer = new ArrayBuffer(info.byteLength + 1 + 32);
-                var infoArray = new Uint8Array(infoBuffer);
-                infoArray.set(new Uint8Array(info), 32);
-                infoArray[infoArray.length - 1] = 1;
-                return window.axolotl.crypto.sign(PRK, infoBuffer.slice(32)).then(function(T1) {
-                    infoArray.set(new Uint8Array(T1));
-                    infoArray[infoArray.length - 1] = 2;
-                    return window.axolotl.crypto.sign(PRK, infoBuffer).then(function(T2) {
-                        infoArray.set(new Uint8Array(T2));
-                        infoArray[infoArray.length - 1] = 3;
-                        return window.axolotl.crypto.sign(PRK, infoBuffer).then(function(T3) {
-                            return [ T1, T2, T3 ];
-                        });
-                    });
-                });
-            });
-        },
-
-        // Curve 25519 crypto
-        createKeyPair: function(privKey) {
-            if (privKey === undefined) {
-                privKey = axolotl.crypto.getRandomBytes(32);
-            }
-            if (privKey.byteLength != 32) {
-                throw new Error("Invalid private key");
-            }
-
-            return window.curve25519.keyPair(privKey).then(function(raw_keys) {
-                // prepend version byte
-                var origPub = new Uint8Array(raw_keys.pubKey);
-                var pub = new Uint8Array(33);
-                pub.set(origPub, 1);
-                pub[0] = 5;
-
-                return { pubKey: pub.buffer, privKey: raw_keys.privKey };
-            });
-        },
-        ECDHE: function(pubKey, privKey) {
-            pubKey = validatePubKeyFormat(pubKey);
-            if (privKey === undefined || privKey.byteLength != 32)
-                throw new Error("Invalid private key");
-
-            if (pubKey === undefined || pubKey.byteLength != 32)
-                throw new Error("Invalid public key");
-
-            return window.curve25519.sharedSecret(pubKey, privKey);
-        },
-        Ed25519Sign: function(privKey, message) {
-            if (privKey === undefined || privKey.byteLength != 32)
-                throw new Error("Invalid private key");
-
-            if (message === undefined)
-                throw new Error("Invalid message");
-
-            return window.curve25519.sign(privKey, message);
-        },
-        Ed25519Verify: function(pubKey, msg, sig) {
-            pubKey = validatePubKeyFormat(pubKey);
-
-            if (pubKey === undefined || pubKey.byteLength != 32)
-                throw new Error("Invalid public key");
-
-            if (msg === undefined)
-                throw new Error("Invalid message");
-
-            if (sig === undefined || sig.byteLength != 64)
-                throw new Error("Invalid signature");
-
-            return window.curve25519.verify(pubKey, msg, sig);
-        }
-    };
-
-    var validatePubKeyFormat = function(pubKey) {
-        if (pubKey === undefined || ((pubKey.byteLength != 33 || new Uint8Array(pubKey)[0] != 5) && pubKey.byteLength != 32))
-            throw new Error("Invalid public key");
-        if (pubKey.byteLength == 33) {
-            return pubKey.slice(1);
-        } else {
-            console.error("WARNING: Expected pubkey of length 33, please report the ST and client that generated the pubkey");
-            return pubKey;
-        }
-    };
-
-})();

libaxolotl/protobufs.js
@@ -1,18 +1,0 @@
-;(function() {
-    function loadProtoBufs(filename) {
-        return dcodeIO.ProtoBuf.loadProtoFile({root: 'protos', file: filename}).build('textsecure');
-    };
-
-    var protocolMessages = loadProtoBufs('WhisperTextProtocol.proto');
-    var deviceMessages   = loadProtoBufs('DeviceMessages.proto');
-
-    window.axolotl = window.axolotl || {};
-    window.axolotl.protobuf = {
-        WhisperMessage            : protocolMessages.WhisperMessage,
-        PreKeyWhisperMessage      : protocolMessages.PreKeyWhisperMessage,
-        DeviceInit                : deviceMessages.DeviceInit,
-        IdentityKey               : deviceMessages.IdentityKey,
-        DeviceControl             : deviceMessages.DeviceControl,
-        ProvisionMessage          : deviceMessages.ProvisionMessage,
-    };
-})();

commit e345024ab6a14581d4aa7f501bf2f1273570a7d9

Replaced frontend getString calls broken in d3c158f.
//FREEBIE

js/options.js
@@ -54,7 +54,7 @@
                     if (request.path == "/v1/address" && request.verb == "PUT") {
                         var proto = textsecure.protobuf.ProvisioningUuid.decode(request.body);
                         var url = [ 'tsdevice:/', '?uuid=', proto.uuid, '&pub_key=',
-                            encodeURIComponent(btoa(getString(cryptoInfo.pubKey))) ].join('');
+                            encodeURIComponent(btoa(String.fromCharCode.apply(null, new Uint8Array(cryptoInfo.pubKey)))) ].join('');
                         $('#status').text('');
                         qrCode.makeCode(url);
                         request.respond(200, 'OK');

js/register.js
@@ -85,7 +85,7 @@
         var verificationCode = $('#code').val().replace(/\D+/g, "");
         var signalingKey = textsecure.crypto.getRandomBytes(32 + 20);
 
-        var password = btoa(getString(textsecure.crypto.getRandomBytes(16)));
+        var password = btoa(String.fromCharCode.apply(null, new Uint8Array(textsecure.crypto.getRandomBytes(16))));
         password = password.substring(0, password.length - 2);
 
         var registrationId = new Uint16Array(textsecure.crypto.getRandomBytes(2))[0];