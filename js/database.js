2e272b689420dc9b96c86f9f5c2dd286f58d023d
lilia <liliakai@gmail.com>
2015-07-15 14:27:11 -0700

    Turn off idb query logging
    ++speed

--- js/database.js
+++ js/database.js
@@ -19,6 +19,7 @@
     window.Whisper             = window.Whisper             || {};
     window.Whisper.Database    = window.Whisper.Database    || {};
     window.Whisper.Database.id = window.Whisper.Database.id || 'signal';
+    window.Whisper.Database.nolog = true;
 
     Whisper.Database.migrations = [
         {
