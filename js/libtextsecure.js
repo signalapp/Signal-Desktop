signalapp/Signal-Desktop

commit b790f828491681c8a50bf217124fc77ca1fb135c

Remove libaxolotl dependency on textsecure/errors.js
Let libaxolotl throw a generic error instead of a replayable error, and
add an helper function in libtextsecure's axolotl_wrapper to catch and
convert from the generic error to the replayable one. This allows the
ReplayableError to remain a libtextsecure-level concept only.

Somewhat unrelatedly, but nearby, fix some whitespace and add missing
semicolon.

js/libtextsecure.js
@@ -140,11 +140,24 @@
 
         if ((finalMessage.flags & textsecure.protobuf.PushMessageContent.Flags.END_SESSION)
                 == textsecure.protobuf.PushMessageContent.Flags.END_SESSION &&
-				finalMessage.sync !== null)
+                finalMessage.sync !== null)
             res[1]();
 
         return finalMessage;
-    }
+    };
+
+    var handlePreKeyWhisperMessage = function(from, message) {
+        try {
+            return axolotl.protocol.handlePreKeyWhisperMessage(from, message);
+        } catch(e) {
+            if (e.message === 'Unknown identity key') {
+                // create an error that the UI will pick up and ask the
+                // user if they want to re-negotiate
+                throw new textsecure.IncomingIdentityKeyError(from, message);
+            }
+            throw e;
+        }
+    };
 
     window.textsecure = window.textsecure || {};
     window.textsecure.protocol_wrapper = {
@@ -159,7 +172,7 @@
                 if (proto.message.readUint8() != ((3 << 4) | 3))
                     throw new Error("Bad version byte");
                 var from = proto.source + "." + (proto.sourceDevice == null ? 0 : proto.sourceDevice);
-                return axolotl.protocol.handlePreKeyWhisperMessage(from, getString(proto.message)).then(decodeMessageContents);
+                return handlePreKeyWhisperMessage(from, getString(proto.message)).then(decodeMessageContents);
             case textsecure.protobuf.IncomingPushMessageSignal.Type.RECEIPT:
                 return Promise.resolve(null);
             default:
@@ -37187,8 +37200,7 @@ window.axolotl.protocol = function() {
                 if (open_session !== undefined)
                     closeSession(open_session); // To be returned and saved later
             } else {
-                // ...otherwise create an error that the UI will pick up and ask the user if they want to re-negotiate
-                throw new textsecure.IncomingIdentityKeyError(encodedNumber, getString(message.encode()));
+                throw new Error('Unknown identity key');
             }
         }
         return initSession(false, preKeyPair, signedPreKeyPair, encodedNumber, toArrayBuffer(message.identityKey), toArrayBuffer(message.baseKey), undefined)

libaxolotl/protocol.js
@@ -386,8 +386,7 @@ window.axolotl.protocol = function() {
                 if (open_session !== undefined)
                     closeSession(open_session); // To be returned and saved later
             } else {
-                // ...otherwise create an error that the UI will pick up and ask the user if they want to re-negotiate
-                throw new textsecure.IncomingIdentityKeyError(encodedNumber, getString(message.encode()));
+                throw new Error('Unknown identity key');
             }
         }
         return initSession(false, preKeyPair, signedPreKeyPair, encodedNumber, toArrayBuffer(message.identityKey), toArrayBuffer(message.baseKey), undefined)

libtextsecure/axolotl_wrapper.js
@@ -63,11 +63,24 @@
 
         if ((finalMessage.flags & textsecure.protobuf.PushMessageContent.Flags.END_SESSION)
                 == textsecure.protobuf.PushMessageContent.Flags.END_SESSION &&
-				finalMessage.sync !== null)
+                finalMessage.sync !== null)
             res[1]();
 
         return finalMessage;
-    }
+    };
+
+    var handlePreKeyWhisperMessage = function(from, message) {
+        try {
+            return axolotl.protocol.handlePreKeyWhisperMessage(from, message);
+        } catch(e) {
+            if (e.message === 'Unknown identity key') {
+                // create an error that the UI will pick up and ask the
+                // user if they want to re-negotiate
+                throw new textsecure.IncomingIdentityKeyError(from, message);
+            }
+            throw e;
+        }
+    };
 
     window.textsecure = window.textsecure || {};
     window.textsecure.protocol_wrapper = {
@@ -82,7 +95,7 @@
                 if (proto.message.readUint8() != ((3 << 4) | 3))
                     throw new Error("Bad version byte");
                 var from = proto.source + "." + (proto.sourceDevice == null ? 0 : proto.sourceDevice);
-                return axolotl.protocol.handlePreKeyWhisperMessage(from, getString(proto.message)).then(decodeMessageContents);
+                return handlePreKeyWhisperMessage(from, getString(proto.message)).then(decodeMessageContents);
             case textsecure.protobuf.IncomingPushMessageSignal.Type.RECEIPT:
                 return Promise.resolve(null);
             default:

libtextsecure/libaxolotl_concat.js
@@ -37001,8 +37001,7 @@ window.axolotl.protocol = function() {
                 if (open_session !== undefined)
                     closeSession(open_session); // To be returned and saved later
             } else {
-                // ...otherwise create an error that the UI will pick up and ask the user if they want to re-negotiate
-                throw new textsecure.IncomingIdentityKeyError(encodedNumber, getString(message.encode()));
+                throw new Error('Unknown identity key');
             }
         }
         return initSession(false, preKeyPair, signedPreKeyPair, encodedNumber, toArrayBuffer(message.identityKey), toArrayBuffer(message.baseKey), undefined)

commit 1ab5af338a8acfe4c26ac7cc55e08aaf62c998a8

Update libaxolotl, un-revert b790f828491681c8a5
Accidentally reverted in d3c158f

js/libtextsecure.js
@@ -37278,7 +37278,7 @@ window.axolotl.protocol = function(storage_interface) {
                                     closeSession(open_session); // To be returned and saved later
                             } else {
                                 // ...otherwise create an error that the UI will pick up and ask the user if they want to re-negotiate
-                                throw new textsecure.IncomingIdentityKeyError(encodedNumber, axolotlInternal.utils.convertToString(message.encode()));
+                                throw new Error('Unknown identity key');
                             }
                         }
                         return initSession(false, preKeyPair, signedPreKeyPair, encodedNumber, axolotlInternal.utils.convertToArrayBuffer(message.identityKey), axolotlInternal.utils.convertToArrayBuffer(message.baseKey), undefined)

libtextsecure/libaxolotl.js
@@ -37201,7 +37201,7 @@ window.axolotl.protocol = function(storage_interface) {
                                     closeSession(open_session); // To be returned and saved later
                             } else {
                                 // ...otherwise create an error that the UI will pick up and ask the user if they want to re-negotiate
-                                throw new textsecure.IncomingIdentityKeyError(encodedNumber, axolotlInternal.utils.convertToString(message.encode()));
+                                throw new Error('Unknown identity key');
                             }
                         }
                         return initSession(false, preKeyPair, signedPreKeyPair, encodedNumber, axolotlInternal.utils.convertToArrayBuffer(message.identityKey), axolotlInternal.utils.convertToArrayBuffer(message.baseKey), undefined)