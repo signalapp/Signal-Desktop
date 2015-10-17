b0112e8c2e2e8f7f14e597dfa2d3b1940a799f62
lilia <liliakai@gmail.com>
2015-10-16 18:18:25 -0700

    Remove unnecessary check in gruntfile
    There's only one reference to the server url nowadays.
    
    // FREEBIE

--- Gruntfile.js
+++ Gruntfile.js
@@ -123,7 +123,7 @@ module.exports = function(grunt) {
         files: [{ expand: true, dest: 'dist/', src: ['<%= dist.src %>'] }],
         options: {
           process: function(content, srcpath) {
-            if (srcpath.match('libtextsecure') || srcpath.match('background.js')) {
+            if (srcpath.match('background.js')) {
               return content.replace(
                 /textsecure-service-staging.whispersystems.org/g,
                 'textsecure-service-ca.whispersystems.org:4433');
