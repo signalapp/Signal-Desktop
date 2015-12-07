ae3a834b4c05d978083db7d19ca9674ede251dcb
lilia <liliakai@gmail.com>
2015-12-07 13:42:19 -0800

    Log global errors
    Use the global error handler, window.onerror, to catch miscellaneous
    exceptions and pipe them into the debug log.
    
    Fixes #456
    
    // FREEBIE

--- js/index.js
+++ js/index.js
@@ -5,21 +5,36 @@
 (function () {
     'use strict';
 
+    function logError(error) {
+        extension.windows.getBackground(function(bg) {
+            bg.console.log('index.html: ', error);
+        });
+    }
+
+    window.onerror = function(message, script, line, col, error) {
+        logError(error);
+    };
+
     var view;
 
     function render() {
         extension.windows.getBackground(function(bg) {
             extension.windows.getCurrent(function(appWindow) {
-                if (view) { view.remove(); }
-                var $body = bg.$('body',document).empty();
-                view = new bg.Whisper.InboxView({appWindow: appWindow});
-                view.$el.prependTo($body);
-                window.openConversation = function(conversation) {
-                    if (conversation) {
-                        view.openConversation(null, conversation);
-                    }
-                };
-                openConversation(bg.getOpenConversation());
+                try {
+                    if (view) { view.remove(); }
+                    var $body = bg.$('body',document).empty();
+                    view = new bg.Whisper.InboxView({appWindow: appWindow});
+                    view.$el.prependTo($body);
+                    window.openConversation = function(conversation) {
+                        if (conversation) {
+                            view.openConversation(null, conversation);
+                        }
+                    };
+                    openConversation(bg.getOpenConversation());
+                } catch (e) {
+                    logError(e);
+                }
+
             });
         });
     }
