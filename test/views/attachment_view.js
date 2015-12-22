239ec8e318c10009516be75663556e9cd4ed1ec0
lilia <liliakai@gmail.com>
2015-12-21 17:40:31 -0800

    Fix test
    // FREEBIE

--- test/views/attachment_view.js
+++ test/views/attachment_view.js
@@ -1,9 +1,7 @@
 describe('AttachmentView', function() {
 
   it('should display an error for an unsupported type', function() {
-    var attachment = {
-      contentType: 'html/text';
-    }
+    var attachment = { contentType: 'html/text' };
     var view = new Whisper.AttachmentView({model: attachment}).render();
     assert.match(view.$el.text(), /Sorry, your attachment has a type, html, that is not currently supported./);
   });
