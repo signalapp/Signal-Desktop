6e30d00929892bcc8ed83a097b5b3fd95c59a19c
codedust <codedust@users.noreply.github.com>
2016-01-04 18:23:27 +0100

    Fix attachment button and drop area in Firefox

--- js/views/file_input_view.js
+++ js/views/file_input_view.js
@@ -40,7 +40,7 @@
 
         open: function() {
             // hack
-            if (this.window && this.window.chrome && this.window.chrome.fileSystem) {
+            if (typeof chrome != 'undefined' && this.window.chrome.fileSystem) {
                 this.window.chrome.fileSystem.chooseEntry({type: 'openFile'}, function(entry) {
                     if (!entry) {
                         return;
@@ -250,7 +250,10 @@
         },
 
         openDropped: function(e) {
-            if (e.originalEvent.dataTransfer.types[0] != 'Files') return;
+            var dropType = e.originalEvent.dataTransfer.types[0];
+            if (dropType != 'Files' && dropType != 'application/x-moz-file') {
+              return;
+            }
 
             e.stopPropagation();
             e.preventDefault();
@@ -260,7 +263,10 @@
         },
 
         showArea: function(e) {
-            if (e.originalEvent.dataTransfer.types[0] != 'Files') return;
+            var dropType = e.originalEvent.dataTransfer.types[0];
+            if (dropType != 'Files' && dropType != 'application/x-moz-file') {
+              return;
+            }
 
             e.stopPropagation();
             e.preventDefault();
@@ -268,7 +274,10 @@
         },
 
         hideArea: function(e) {
-            if (e.originalEvent.dataTransfer.types[0] != 'Files') return;
+            var dropType = e.originalEvent.dataTransfer.types[0];
+            if (dropType != 'Files' && dropType != 'application/x-moz-file') {
+              return;
+            }
 
             e.stopPropagation();
             e.preventDefault();
