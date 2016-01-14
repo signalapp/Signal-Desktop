6a268f0325c54192268e1ffa900dfd3c12f682d9
lilia <liliakai@gmail.com>
2016-01-14 10:38:46 -0800

    Migrate registration state
    Follow up to 5223e6e for existing installations that never set this flag.
    
    // FREEBIE

--- js/database.js
+++ js/database.js
@@ -80,6 +80,14 @@
                     });
                 });
             }
+        },
+        {
+            version: "5.0",
+            migrate: function(transaction, next) {
+                if (storage.get("chromiumRegistrationDone") === "") {
+                    storage.put("chromiumRegistrationDoneEver", "");
+                }
+            }
         }
     ];
 }());
