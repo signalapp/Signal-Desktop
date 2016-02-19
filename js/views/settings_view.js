74aeddcbda51f575bb238f297422ef11589979cc
lilia <liliakai@gmail.com>
2016-02-18 16:13:53 -0800

    Add missing view file
    // FREEBIE

---
+++ js/views/settings_view.js
@@ -0,0 +1,37 @@
+/*
+ * vim: ts=4:sw=4:expandtab
+ */
+(function () {
+    'use strict';
+    window.Whisper = window.Whisper || {};
+
+    Whisper.SettingsView = Whisper.View.extend({
+        className: 'settings modal',
+        templateName: 'settings',
+        events: {
+            'change': 'change',
+            'click .close': 'remove'
+        },
+        change: function(e) {
+            var value = this.$(e.target).val();
+            storage.put('notification-setting', value);
+            console.log('notification setting changed to', value);
+        },
+        update: function() {
+            var setting = storage.get('notification-setting');
+            if (!setting) {
+                setting = 'message';
+            }
+            this.$('#notification-setting-' + setting).attr('checked','checked');
+        },
+        render_attributes: function() {
+            return {
+              settings: i18n('settings'),
+              off: i18n('off'),
+              nameAndMessage: i18n('nameAndMessage'),
+              noNameOrMessage: i18n('noNameOrMessage'),
+              nameOnly: i18n('nameOnly'),
+            };
+        }
+    });
+})();
