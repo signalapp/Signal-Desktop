1c891427525f7850e44754a2f394f25a4535d045
lilia <liliakai@gmail.com>
2016-09-01 12:21:23 -0700

    Customize emoji conversion
    Use image tags with :emoji: in a title attribute for an optimal
    copy/paste ux.
    
    // FREEBIE

--- js/emoji_util.js
+++ js/emoji_util.js
@@ -6,20 +6,38 @@
     'use strict';
     window.emoji_util = window.emoji_util || {};
 
-    // The default version of this function has issues initing from the
-    // background page. Since we only support one environment, we can
-    // preconfigure it here.
+    // EmojiConverter overrides
     EmojiConvertor.prototype.init_env = function() {
         if (this.inits.env) {
             return;
         }
-		this.inits.env = 1;
+        this.inits.env = 1;
         this.include_title = true;
         this.img_sets.apple.path = 'images/emoji/apple/';
-
-        this.img_path = 'images/emoji/unicode/';
         this.replace_mode = 'img';
-        this.supports_css = true;
+    };
+    EmojiConvertor.prototype.replace_unified = function(str) {
+        var self = this;
+        self.init_unified();
+        return str.replace(self.rx_unified, function(m, p1, p2) {
+            var val = self.map.unified[p1];
+            if (!val) { return m; }
+            var idx = null;
+            if (p2 == '\uD83C\uDFFB') { idx = '1f3fb'; }
+            if (p2 == '\uD83C\uDFFC') { idx = '1f3fc'; }
+            if (p2 == '\uD83C\uDFFD') { idx = '1f3fd'; }
+            if (p2 == '\uD83C\uDFFE') { idx = '1f3fe'; }
+            if (p2 == '\uD83C\uDFFF') { idx = '1f3ff'; }
+            if (idx) {
+                return self.replacement(val, null, null, {
+                    idx : idx,
+                    actual  : p2,
+                    wrapper : ':'
+                });
+            }
+            // wrap names in :'s
+            return self.replacement(val, ':' + self.data[val][3][0] + ':');
+        });
     };
     window.emoji = new EmojiConvertor();
     emoji.init_colons();
