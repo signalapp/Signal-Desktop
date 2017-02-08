e8de2f0c5249f2a8f6bcaaa472f353c06dabe405
haffenloher <brumsel@losecatcher.de>
2017-02-08 18:31:12 +0100

    Increase image constraints to 6 MB / 4096 px
    Matches WhisperSystems/Signal-Android@d2be49af
    
    Closes #1055
    
    // FREEBIE

--- js/views/file_input_view.js
+++ js/views/file_input_view.js
@@ -79,9 +79,9 @@
                 img.onload = function () {
                     URL.revokeObjectURL(url);
 
-                    var maxSize = 4000 * 1024;
-                    var maxHeight = 2048;
-                    var maxWidth = 2048;
+                    var maxSize = 6000 * 1024;
+                    var maxHeight = 4096;
+                    var maxWidth = 4096;
                     if (img.width <= maxWidth && img.height <= maxHeight &&
                         file.size <= maxSize) {
                         resolve(file);
@@ -140,9 +140,9 @@
                 var blobType = file.type === 'image/gif' ? 'gif' : type;
                 switch (blobType) {
                     case 'image':
-                        limitKb = 4000; break;
+                        limitKb = 6000; break;
                     case 'gif':
-                        limitKb = 5000; break;
+                        limitKb = 6000; break;
                     case 'audio':
                         limitKb = 100000; break;
                     case 'video':
