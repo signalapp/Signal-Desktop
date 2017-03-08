99441e76210be48b9c8ce78aac91021079bf21c3
lilia <liliakai@gmail.com>
2017-03-07 16:54:46 -0800

    Use relative paths
    // FREEBIE

--- js/models/conversations.js
+++ js/models/conversations.js
@@ -722,7 +722,7 @@
                 content: title ? title.trim()[0] : '#'
             };
         } else {
-            return { url: '/images/group_default.png', color: color };
+            return { url: 'images/group_default.png', color: color };
         }
     },
 
