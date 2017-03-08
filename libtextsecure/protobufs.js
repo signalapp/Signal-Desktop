bb6415a8df1104f733fdd40082bcb9bc6d72b5eb
lilia <liliakai@gmail.com>
2017-03-07 16:54:46 -0800

    Use relative paths
    // FREEBIE

--- libtextsecure/protobufs.js
+++ libtextsecure/protobufs.js
@@ -4,7 +4,7 @@
     window.textsecure.protobuf = {};
 
     function loadProtoBufs(filename) {
-        return dcodeIO.ProtoBuf.loadProtoFile({root: '/protos', file: filename}, function(error, result) {
+        return dcodeIO.ProtoBuf.loadProtoFile({root: 'protos', file: filename}, function(error, result) {
            var protos = result.build('textsecure');
            for (var protoName in protos) {
               textsecure.protobuf[protoName] = protos[protoName];
