d68af00ee4f596bf58750554e24bed2aab0187ef
lilia <liliakai@gmail.com>
2017-03-30 16:30:01 -0700

    Enable sandbox
    // FREEBIE

--- main.js
+++ main.js
@@ -17,7 +17,11 @@ let mainWindow
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600, webPreferences: {nodeIntegration: false}})
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: { nodeIntegration: false, sandbox: true }
+  })
 
   // and load the index.html of the app.
   mainWindow.loadURL(url.format({
