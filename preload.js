6c5338edae171722c63f736f0b2d38e55f9575ea
lilia <liliakai@gmail.com>
2017-04-04 13:39:24 -0700

    Separate development and production environments
    Set NODE_ENV at run time or build time to switch the app between dev and
    production modes.
    
    At build time, the current NODE_ENV will be included in the packaged
    app's package.json file. At runtime we read NODE_ENV from package.json,
    but also allow the local environment variable to override. A query
    string parsed by a preload script exposes the value to the renderer,
    which then determines whether we use the staging or production server.
    Additionally, different environments have different user data
    directories.
    
    // FREEBIE

---
+++ preload.js
@@ -0,0 +1,10 @@
+/*
+ * Pending electron 1.6.x
+ * const env = require('url').parse(window.location, true).query;
+*/
+
+window.env = {};
+window.location.search.substring(1).split('&').forEach(function(variable) {
+  var pair = variable.split('=');
+  env[decodeURIComponent(pair[0])] = decodeURIComponent(pair[1]);
+});
