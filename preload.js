9feab973a4a2439a2999c252d5e3ca1fe6b465ee
lilia <liliakai@gmail.com>
2017-04-03 23:32:46 -0700

    Quick fix for i18n
    Just use the english locale for now. Load locale data from the
    filesystem in the main process and pass it to the renderer preload
    script via ipc. Note that we need the locale data to be available by the
    time view scripts are loaded.
    
    // FREEBIE

--- preload.js
+++ preload.js
@@ -8,3 +8,6 @@ window.location.search.substring(1).split('&').forEach(function(variable) {
   var pair = variable.split('=');
   env[decodeURIComponent(pair[0])] = decodeURIComponent(pair[1]);
 });
+
+const ipc = require('electron').ipcRenderer
+window.env.locale_json = ipc.sendSync('locale-data');
