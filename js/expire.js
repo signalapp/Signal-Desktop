c2cc69eda9eed3d2ac0438ca83fed51e68a4139c
lilia <liliakai@gmail.com>
2017-04-13 10:52:12 -0700

    Build expiration date
    Add the buildExpiration config and add it to the renderer's config
    object. Use grunt to write the build expiration to
    config/local-production.json which will override the default value (no
    expiration) in production. Finally, run this grunt task as part of the
    build process.
    
    // FREEBIE

--- js/expire.js
+++ js/expire.js
@@ -1,6 +1,12 @@
 ;(function() {
     'use strict';
     var BUILD_EXPIRATION = 0;
+    try {
+        BUILD_EXPIRATION = parseInt(window.config.buildExpiration);
+        if (BUILD_EXPIRATION) {
+            console.log("Build expires: ", new Date(BUILD_EXPIRATION).toISOString());
+        }
+    } catch (e) {}
 
     window.extension = window.extension || {};
 
