9a859082c272e10458791a9a5f07c9f57499196c
lilia <liliakai@gmail.com>
2017-04-19 15:39:21 -0700

    Use built-in url parsing in preload script
    Latest electron allows acces to the url module in a preload script.
    Also add a wrapper to shield global scope and use strict inside.
    
    // FREEBIE

--- preload.js
+++ preload.js
@@ -1,15 +1,11 @@
-/*
- * Pending electron 1.6.x
- * const env = require('url').parse(window.location, true).query;
-*/
+(function () {
+  'use strict';
+  console.log('preload');
+  const electron = require('electron')
 
-console.log('preload');
+  window.config = require('url').parse(window.location.toString(), true).query;
 
-window.config = {};
-window.location.search.substring(1).split('&').forEach(function(variable) {
-  var pair = variable.split('=');
-  config[decodeURIComponent(pair[0])] = decodeURIComponent(pair[1]);
-});
+  const ipc = electron.ipcRenderer
+  window.config.locale_json = ipc.sendSync('locale-data');
 
-const ipc = require('electron').ipcRenderer
-window.config.locale_json = ipc.sendSync('locale-data');
+})();
