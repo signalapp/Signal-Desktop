50ddbf1defa1b09a02a319af6123c7f6ea4c5ba5
lilia <liliakai@gmail.com>
2017-04-24 17:59:11 -0700

    Collect references to Whisper.events
    Fixup
    
    // FREEBIE

--- js/views/network_status_view.js
+++ js/views/network_status_view.js
@@ -29,7 +29,7 @@
             'click .openInstaller': 'openInstaller'
         },
         openInstaller: function() {
-            Whisper.events.trigger('openInstaller');
+            this.$el.trigger('openInstaller');
         },
         onReconnectTimer: function() {
           this.setSocketReconnectInterval(60000);
