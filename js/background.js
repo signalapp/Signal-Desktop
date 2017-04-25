7d02ecd0e564c6235d72b9f9428e42506aabfcdc
lilia <liliakai@gmail.com>
2017-04-24 19:20:43 -0700

    More refactoring to reduce global event dependencies
    All Whisper.events listeners are now defined and bound in background.js,
    and we no longer need global methods for opening the inbox and
    conversation views, as those are handled by AppView or internally by
    InboxView.
    
    // FREEBIE

--- js/background.js
+++ js/background.js
@@ -81,13 +81,12 @@
             appView.inboxView.networkStatusView.setSocketReconnectInterval(60000);
         });
 
-        [
-          'openInbox',
-          'openInstaller',
-          'openConversation',
-          'openStandalone'
-        ].forEach(function(eventName) {
-          appView.listenTo(Whisper.events, eventName, appView[eventName]);
+        Whisper.Notifications.on('click', function(conversation) {
+            if (conversation) {
+              appView.openConversation(conversation);
+            } else {
+              appView.openInbox();
+            }
         });
     });
 
