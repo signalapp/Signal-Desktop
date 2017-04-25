7d3089463b301b1a48e3d31321b95cc566b7e72f
lilia <liliakai@gmail.com>
2017-04-24 17:59:11 -0700

    Collect references to Whisper.events
    Fixup
    
    // FREEBIE

--- js/background.js
+++ js/background.js
@@ -65,7 +65,7 @@
             init(true);
         });
 
-        var appView = window.owsDesktopApp.appView = new Whisper.AppView({el: $('body'), events: Whisper.events});
+        var appView = window.owsDesktopApp.appView = new Whisper.AppView({el: $('body')});
 
         Whisper.WallClockListener.init(Whisper.events);
         Whisper.RotateSignedPreKeyListener.init(Whisper.events);
@@ -87,6 +87,14 @@
             appView.inboxView.networkStatusView.setSocketReconnectInterval(60000);
         });
 
+        [
+          'openInbox',
+          'openInstaller',
+          'openConversation',
+          'openStandalone'
+        ].forEach(function(eventName) {
+          appView.listenTo(Whisper.events, eventName, appView[eventName]);
+        });
     });
 
     window.getSyncRequest = function() {
