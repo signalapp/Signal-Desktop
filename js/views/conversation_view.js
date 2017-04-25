c0124d84065e83585f1f08ac959a89a30402e383
lilia <liliakai@gmail.com>
2017-04-24 19:00:49 -0700

    Remove dead code
    This view doesn't contain any elements with class openInbox.
    
    // FREEBIE

--- js/views/conversation_view.js
+++ js/views/conversation_view.js
@@ -177,7 +177,6 @@
             'click .show-identity': 'showSafetyNumber',
             'click .show-members': 'showMembers',
             'click .conversation-menu .hamburger': 'toggleMenu',
-            'click .openInbox' : 'openInbox',
             'click' : 'onClick',
             'click .bottom-bar': 'focusMessageField',
             'click .back': 'resetPanel',
@@ -546,10 +545,6 @@
         },
         updateMessage: function(message) {
             this.model.messageCollection.add(message, {merge: true});
-        },
-
-        openInbox: function() {
-            openInbox();
         },
 
         onClick: function(e) {
