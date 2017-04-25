6034876d8eb0dfc23e960157123de80cd4994e5d
lilia <liliakai@gmail.com>
2017-04-24 19:20:43 -0700

    More refactoring to reduce global event dependencies
    All Whisper.events listeners are now defined and bound in background.js,
    and we no longer need global methods for opening the inbox and
    conversation views, as those are handled by AppView or internally by
    InboxView.
    
    // FREEBIE

--- js/panel_controller.js
+++ js/panel_controller.js
@@ -18,10 +18,6 @@
     };
     var inboxWindowId = 'inbox';
 
-    window.openInbox = function(options) {
-        Whisper.events.trigger('openInbox', options);
-    };
-
     window.setUnreadCount = function(count) {
         if (count > 0) {
             window.setBadgeCount(count);
@@ -30,10 +26,6 @@
             window.setBadgeCount(0);
             window.document.title = "Signal";
         }
-    };
-
-    window.openConversation = function(conversation) {
-        Whisper.events.trigger('openConversation', conversation);
     };
 
 })();
