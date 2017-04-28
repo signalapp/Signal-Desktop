a72fa6553f8594f21b158c2e08004f9dfc8f0a34
lilia <liliakai@gmail.com>
2017-04-27 18:03:22 -0700

    Add ability to unhide the window from the renderer
    via ipc, for instance if a notification is clicked but the window is
    closed/hidden.
    
    // FREEBIE

--- main.js
+++ main.js
@@ -141,7 +141,11 @@ function createWindow () {
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
     mainWindow = null
-  })
+  });
+
+  ipc.on('show-window', function() {
+    mainWindow.show();
+  });
 }
 
 // This method will be called when Electron has finished
