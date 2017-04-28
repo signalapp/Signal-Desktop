206ba1275a67edde57e335f5320ccaa8403044a4
lilia <liliakai@gmail.com>
2017-04-27 18:21:52 -0700

    Unfurl notifications
    Since we no longer have support for list-style notifications, stop
    coalescing notifications into batches and just show contents of the last
    message received. Also open the window when clicking on a notification
    if it has previously been closed.
    
    // FREEBIE

--- js/background.js
+++ js/background.js
@@ -96,6 +96,7 @@
         });
 
         Whisper.Notifications.on('click', function(conversation) {
+            showWindow();
             if (conversation) {
                 appView.openConversation(conversation);
             } else {
