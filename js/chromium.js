d85ccbad4f3ccc69bffa3ca402a5da0613db85d8
lilia <liliakai@gmail.com>
2017-05-04 11:06:18 -0700

    Remove extension.notification

--- js/chromium.js
+++ js/chromium.js
@@ -11,18 +11,4 @@
             window.addEventListener('beforeunload', callback);
         }
     };
-
-    extension.notification = {
-        update: function(options) {
-            var notification = new Notification(options.title, {
-                body : options.message,
-                icon : options.iconUrl,
-                tag  : 'signal'
-            });
-            notification.onclick = function() {
-                Whisper.Notifications.onclick();
-            };
-        }
-    };
-
 }());
