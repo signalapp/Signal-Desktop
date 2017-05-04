0dfdd3e965a8838e7943a673c903095bee403d64
lilia <liliakai@gmail.com>
2017-05-03 22:15:35 -0700

    Remove dead code
    These are all unused, obsolete, or no-op functions at this point.
    
    // FREEBIE

--- js/models/conversations.js
+++ js/models/conversations.js
@@ -809,7 +809,7 @@
         if (!message.isIncoming()) {
             return;
         }
-        if (window.isOpen() && window.isFocused()) {
+        if (window.isFocused()) {
             return;
         }
         window.drawAttention();
