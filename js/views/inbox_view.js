04a8a6b0df4961c97e1ad9e8443fdddcbb92a864
lilia <liliakai@gmail.com>
2017-05-07 14:39:43 -0700

    Make 'Restart Signal' actually restart Signal
    Previously it would just reload the page.
    
    // FREEBIE

--- js/views/inbox_view.js
+++ js/views/inbox_view.js
@@ -168,7 +168,7 @@
             'click .showSettings': 'showSettings',
             'select .gutter .conversation-list-item': 'openConversation',
             'input input.search': 'filterContacts',
-            'click .restart-signal': 'reloadBackgroundPage',
+            'click .restart-signal': window.restart,
             'show .lightbox': 'showLightbox'
         },
         startConnectionListener: function() {
