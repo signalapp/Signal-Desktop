b75891069483720961c96e3d0dccf76862ff7e1c
lilia <liliakai@gmail.com>
2017-05-07 14:51:46 -0700

    Remove remaining call to extension.notification.update

--- js/notifications.js
+++ js/notifications.js
@@ -71,11 +71,6 @@
                 iconUrl = last.get('iconUrl');
                 break;
             }
-            extension.notification.update({
-                title    : title,
-                message  : message,
-                iconUrl  : iconUrl
-            });
             var notification = new Notification(title, {
                 body : message,
                 icon : iconUrl,
