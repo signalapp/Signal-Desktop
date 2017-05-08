7b5266b68f7c1ea423dc71b5b323a4ce62784790
lilia <liliakai@gmail.com>
2017-05-08 10:29:42 -0700

    Restructure prep-release script slightly
    Order of operations is now:
    - regenerate local assets
    - clean the release dir
    - download linux/windows releases
    - build osx release
    - smoke all test releases
    
    // FREEBIE

--- Gruntfile.js
+++ Gruntfile.js
@@ -407,7 +407,7 @@ module.exports = function(grunt) {
   grunt.registerTask('test', ['jshint', 'jscs' ]);
   grunt.registerTask('copy_dist', ['gitinfo', 'copy']);
   grunt.registerTask('date', ['gitinfo', 'getExpireTime']);
-  grunt.registerTask('prep-release', ['gitinfo', 'fetch-release', 'test-release']);
+  grunt.registerTask('prep-release', ['gitinfo', 'clean-release', 'fetch-release']);
   grunt.registerTask('default', ['concat', 'sass', 'date']);
 
 };
