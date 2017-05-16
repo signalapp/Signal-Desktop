5158be6be8e4b8f8f7da90501dcb4a2ecd826418
Scott Nonnenberg <scott@nonnenberg.com>
2017-05-16 16:17:25 -0700

    Proto-loading: Throw if we get an error

--- libtextsecure/protobufs.js
+++ libtextsecure/protobufs.js
@@ -5,6 +5,9 @@
 
     function loadProtoBufs(filename) {
         return dcodeIO.ProtoBuf.loadProtoFile({root: 'protos', file: filename}, function(error, result) {
+           if (error) {
+              throw error;
+           }
            var protos = result.build('textsecure');
            for (var protoName in protos) {
               textsecure.protobuf[protoName] = protos[protoName];
