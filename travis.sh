a5441984ceca1e64b53359b0f8c7b6c38b30dcd2
Scott Nonnenberg <scott@nonnenberg.com>
2017-05-15 17:55:48 -0700

    Increase unit-tests timeout, improve error display, add set -e
    FREEBIE

--- travis.sh
+++ travis.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 
+set -e
+
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
   export DISPLAY=:99.0
   sh -e /etc/init.d/xvfb start
   sleep 3
 fi
+
 grunt test
+
 grunt test-release:$TRAVIS_OS_NAME
