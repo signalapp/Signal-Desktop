2908a94846de928faa6744cf9fde84a4c2aab60d
Scott Nonnenberg <scott@nonnenberg.com>
2017-05-18 12:26:43 -0700

    LastSeenIndicatorView: programmatic tests, tweak to manual tests
    FREEBIE

---
+++ test/views/last_seen_indicator_view_test.js
@@ -0,0 +1,18 @@
+/*
+ * vim: ts=4:sw=4:expandtab
+ */
+describe('LastSeenIndicatorView', function() {
+    // TODO: in electron branch, wheere we have access to real i18n, test rendered HTML
+
+    it('renders provided count', function() {
+        var view = new Whisper.LastSeenIndicatorView({count: 10});
+        assert.equal(view.count, 10);
+    });
+
+    it('increments count', function() {
+        var view = new Whisper.LastSeenIndicatorView({count: 4});
+        assert.equal(view.count, 4);
+        view.increment(3);
+        assert.equal(view.count, 7);
+    });
+});
