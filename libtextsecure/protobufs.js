a0fc03f64fb6bbb7ea5071e64dd40250164f8830
Scott Nonnenberg <scott@nonnenberg.com>
2017-05-17 18:01:33 -0700

    Remove test/protos symlink, use window.PROTO_ROOT for config

--- libtextsecure/protobufs.js
+++ libtextsecure/protobufs.js
@@ -4,14 +4,18 @@
     window.textsecure.protobuf = {};
 
     function loadProtoBufs(filename) {
-        return dcodeIO.ProtoBuf.loadProtoFile({root: 'protos', file: filename}, function(error, result) {
-           if (error) {
-              throw error;
-           }
-           var protos = result.build('textsecure');
-           for (var protoName in protos) {
-              textsecure.protobuf[protoName] = protos[protoName];
-           }
+        return dcodeIO.ProtoBuf.loadProtoFile({root: window.PROTO_ROOT, file: filename}, function(error, result) {
+            if (error) {
+                throw error;
+            }
+            var protos = result.build('textsecure');
+            if (!protos) {
+                var text = 'Error loading protos from ' + filename + ' (root: ' + window.PROTO_ROOT + ')';
+                throw new Error(text);
+            }
+            for (var protoName in protos) {
+                textsecure.protobuf[protoName] = protos[protoName];
+            }
         });
     };
 
