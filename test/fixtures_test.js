a90960df629d175445f6afb5a459db57f5347ff5
Scott Nonnenberg <scott@nonnenberg.com>
2017-05-18 11:10:25 -0700

    Restore interactivity to InboxViews hosted on test page
    FREEBIE

--- test/fixtures_test.js
+++ test/fixtures_test.js
@@ -12,14 +12,14 @@ describe("Fixtures", function() {
 
   it('renders', function(done) {
     ConversationController.updateInbox().then(function() {
-      var view = new Whisper.InboxView({appWindow: {contentWindow: window}});
+      var view = new Whisper.InboxView({window: window});
       view.$el.prependTo($('#render-android'));
 
-      var view = new Whisper.InboxView({appWindow: {contentWindow: window}});
+      var view = new Whisper.InboxView({window: window});
       view.$el.removeClass('android').addClass('ios');
       view.$el.prependTo($('#render-ios'));
 
-      var view = new Whisper.InboxView({appWindow: {contentWindow: window}});
+      var view = new Whisper.InboxView({window: window});
       view.$el.removeClass('android').addClass('android-dark');
       view.$el.prependTo($('#render-android-dark'));
     }).then(done, done);
