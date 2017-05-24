135939aae86b6647f0b8f29dd77e669c29156878
Ivan Markin <twim@riseup.net>
2017-05-24 20:14:27 +0000

    Do not use output of libphonenumber for anything but displaying.
    This borks most conversations and lots of other logic on receiving
    messages from magic (misparsed by libphonenumber) numbers.

--- js/models/conversations.js
+++ js/models/conversations.js
@@ -109,7 +109,7 @@
             var regionCode = storage.get('regionCode');
             var number = libphonenumber.util.parseNumber(this.id, regionCode);
             if (number.isValidNumber) {
-                this.set({ id: number.e164 });
+                this.set({ id: this.id });
             } else {
                 return number.error || "Invalid phone number";
             }
