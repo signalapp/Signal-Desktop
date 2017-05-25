e0cc8cf7ddca6c13a7524300bb3660ad6bcc9a4f
Scott Nonnenberg <scott@nonnenberg.com>
2017-05-25 11:32:57 -0700

    Reset last seen indicator timing on focus of window
    FREEBIE

--- js/views/conversation_view.js
+++ js/views/conversation_view.js
@@ -116,7 +116,7 @@
 
             var onFocus = function() {
                 if (this.$el.css('display') !== 'none') {
-                    this.markRead();
+                    this.updateUnread();
                 }
             }.bind(this);
             this.window.addEventListener('focus', onFocus);
