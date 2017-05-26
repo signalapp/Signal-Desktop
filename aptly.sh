8e529595fc22adaf6620fadab133d8eee1ee41b2
lilia <liliakai@gmail.com>
2017-05-25 18:01:57 -0700

    Point aptly at ./release instead of ./dist

--- aptly.sh
+++ aptly.sh
@@ -8,7 +8,7 @@
 REPO=signal-desktop
 DISTRO=xenial
 ENDPOINT=signal-desktop-apt # Matches endpoint name in .aptly.conf
-DEB_PATH=dist
+DEB_PATH=release
 SNAPSHOT=signal-desktop_v$VERSION
 GPG_KEYID=57F6FB06
 aptly repo add $REPO $DEB_PATH/$REPO\_$VERSION\_*.deb
