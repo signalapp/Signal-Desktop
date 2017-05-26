8b245f3475ef90e40b5166dcf3ca69b36206d96e
lilia <liliakai@gmail.com>
2017-05-25 18:01:47 -0700

    Add grunt todo

--- Gruntfile.js
+++ Gruntfile.js
@@ -346,6 +346,8 @@ module.exports = function(grunt) {
       });
       done();
     });
+
+    // TODO: chmod u+r win-unpacked/**/*
   });
 
   grunt.registerTask('unit-tests', 'Run unit tests inside Electron', function() {
