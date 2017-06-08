51fca5746043f9ec713af948b10a3c3f802f8a1f
Scott Nonnenberg <scott@nonnenberg.com>
2017-06-08 10:32:48 -0700

    fix_broken_perms.sh: Restore signal-desktop execute permission

--- fix_broken_perms.sh
+++ fix_broken_perms.sh
@@ -3,3 +3,4 @@ find release/linux-unpacked -type d | xargs chmod 755
 find release/linux-unpacked -type f | xargs chmod 644
 find release/win-unpacked -type d | xargs chmod 755
 find release/win-unpacked -type f | xargs chmod 644
+chmod +x release/linux-unpacked/signal-desktop
