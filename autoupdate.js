8e9e2a4b84e96254f5127aedb3c21714205fc04e
David Balatero <dbalatero@gmail.com>
2017-06-09 12:37:01 -0700

    Show dialog box when there's an update for download

---
+++ autoupdate.js
@@ -0,0 +1,62 @@
+const autoUpdater = require('electron-updater').autoUpdater
+const { dialog } = require('electron');
+
+const hour = 60 * 60;
+const autoUpdaterInterval = hour * 1000;
+
+const RESTART_BUTTON = 0;
+const LATER_BUTTON = 1;
+
+function autoUpdateDisabled(config) {
+  return process.mas || config.get('disableAutoUpdate');
+}
+
+function checkForUpdates() {
+  autoUpdater.checkForUpdates();
+}
+
+function showUpdateDialog(localeMessages) {
+  return function() {
+    const options = {
+      type: 'info',
+      buttons: [
+        localeMessages.autoUpdateRestartButtonLabel.message,
+        localeMessages.autoUpdateLaterButtonLabel.message
+      ],
+      title: localeMessages.autoUpdateNewVersionTitle.message,
+      message: localeMessages.autoUpdateNewVersionMessage.message,
+      detail: localeMessages.autoUpdateNewVersionInstructions.message,
+      defaultId: RESTART_BUTTON,
+      cancelId: LATER_BUTTON
+    }
+
+    dialog.showMessageBox(options, function(response) {
+      if (response == RESTART_BUTTON) {
+        autoUpdater.quitAndInstall();
+      }
+    });
+  }
+}
+
+function onError(error) {
+  console.log("Got an error while updating: ", error.stack);
+}
+
+function initializeAutoUpdater(config, localeMessages) {
+  if (autoUpdateDisabled(config)) {
+    return;
+  }
+
+  const onUpdateDownloaded = showUpdateDialog(localeMessages);
+
+  autoUpdater.addListener('update-downloaded', onUpdateDownloaded);
+  autoUpdater.addListener('error', onError);
+
+  checkForUpdates();
+
+  setInterval(checkForUpdates, autoUpdaterInterval);
+}
+
+module.exports = {
+  initializeAutoUpdater
+};
