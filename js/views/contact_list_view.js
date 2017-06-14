3ddf67c3af21a64002b06ea668cb50f620c096bf
Scott Nonnenberg <scott@nonnenberg.com>
2017-06-14 11:45:23 -0700

    Remove comments after NewGroupUpdateView investigation
    It doesn't run today - it depends on RecipientsInputView, which depends
    on Backbone.TypeaheadCollection which doesn't currently exist in the
    product.
    
    FREEBIE

--- js/views/contact_list_view.js
+++ js/views/contact_list_view.js
@@ -5,7 +5,6 @@
     'use strict';
     window.Whisper = window.Whisper || {};
 
-    // Contact list view is used in the list group members senario, as well as the NewGroupUpdate view
     Whisper.ContactListView = Whisper.ListView.extend({
         tagName: 'div',
         itemView: Whisper.View.extend({
