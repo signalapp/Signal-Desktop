ceaff68d55588c6fb60f96f139b799a10fe37dfc
David Balatero <dbalatero@gmail.com>
2017-06-21 18:13:36 -0700

    Refactor locale to only expose the data we care about

--- app/locale.js
+++ app/locale.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const fs = require('fs');
+const app = require('electron').app;
 
 function normalizeLocaleName(locale) {
   if (/^en-/.test(locale)) {
@@ -23,7 +24,25 @@ function getLocaleMessages(locale) {
   return JSON.parse(fs.readFileSync(targetFile, 'utf-8'))
 }
 
+// Load locale - if we can't load messages for the current locale, we
+// default to 'en'
+//
+// possible locales:
+// https://github.com/electron/electron/blob/master/docs/api/locales.md
+let localeName = normalizeLocaleName(app.getLocale());
+let messages;
+
+try {
+  messages = getLocaleMessages(localeName);
+} catch (e) {
+  console.log('Problem loading messages for locale ', localeName, e.stack);
+  console.log('Falling back to en locale');
+
+  localeName = 'en';
+  messages = getLocaleMessages(localeName);
+}
+
 module.exports = {
-  normalizeLocaleName,
-  getLocaleMessages
+  name: localeName,
+  messages
 }
