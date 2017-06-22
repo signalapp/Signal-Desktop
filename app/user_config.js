5e4471c157ef89213e271cd4461b871f3e32ee34
David Balatero <dbalatero@gmail.com>
2017-06-21 18:04:19 -0700

    Refactor configuration out into reusable files

---
+++ app/user_config.js
@@ -0,0 +1,22 @@
+const app = require('electron').app;
+const path = require('path');
+const ElectronConfig = require('electron-config');
+
+const config = require('./config');
+
+// use a separate data directory for development
+if (config.has('storageProfile')) {
+  const userData = path.join(
+    app.getPath('appData'),
+    'Signal-' + config.get('storageProfile')
+  );
+
+  app.setPath('userData', userData);
+}
+
+console.log('userData ' + app.getPath('userData'));
+
+// this needs to be below our update to the appData path
+const userConfig = new ElectronConfig();
+
+module.exports = userConfig;
