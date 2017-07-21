2f673ed65e5d345ebba3252644afd5035c0c1054
Scott Nonnenberg <scott@nonnenberg.com>
2017-07-21 12:00:29 -0700

    Remove questions from aptly.sh - they interrupted automated scripts

--- aptly.sh
+++ aptly.sh
@@ -13,22 +13,6 @@ SNAPSHOT=signal-desktop_v$VERSION
 GPG_KEYID=57F6FB06
 aptly repo add $REPO $DEB_PATH/$REPO\_$VERSION\_*.deb
 
-while true; do
-    read -p "Create snapshot?" yn
-    case $yn in
-        [Yy]* ) break;;
-        [Nn]* ) exit;;
-        * ) echo "Please answer yes or no.";
-    esac
-done
 aptly snapshot create $SNAPSHOT from repo $REPO
-while true; do
-    read -p "Deploy snapshot?" yn
-    case $yn in
-        [Yy]* ) break;;
-        [Nn]* ) exit;;
-        * ) echo "Please answer yes or no.";
-    esac
-done
 aptly publish switch -gpg-key=$GPG_KEYID $DISTRO $SNAPSHOT
 aptly publish switch -gpg-key=$GPG_KEYID -config=.aptly.conf $DISTRO s3:$ENDPOINT: $SNAPSHOT
