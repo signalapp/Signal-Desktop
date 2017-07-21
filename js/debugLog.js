2c244e3b855b7f22d65d7c765142f9d68823da6e
Scott Nonnenberg <scott@nonnenberg.com>
2017-07-21 14:46:30 -0700

    Increase log length to 5000, since we're logging so much now
    FREEBIE

--- js/debugLog.js
+++ js/debugLog.js
@@ -40,7 +40,7 @@
         }
     });
 
-    var MAX_MESSAGES = 3000;
+    var MAX_MESSAGES = 5000;
     var PHONE_REGEX = /\+\d{7,12}(\d{3})/g;
     var log = new DebugLog();
     if (window.console) {
