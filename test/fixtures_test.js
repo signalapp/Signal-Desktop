53f2bfbb57284065d187eba55c6851ae4bb60b0a
Scott Nonnenberg <scott@nonnenberg.com>
2017-07-24 18:43:35 -0700

    Animated loading screens on startup and first conversation load
    FREEBIE

--- test/fixtures_test.js
+++ test/fixtures_test.js
@@ -13,14 +13,17 @@ describe("Fixtures", function() {
   it('renders', function(done) {
     ConversationController.updateInbox().then(function() {
       var view = new Whisper.InboxView({window: window});
+      view.onEmpty();
       view.$el.prependTo($('#render-android'));
 
       var view = new Whisper.InboxView({window: window});
       view.$el.removeClass('android').addClass('ios');
+      view.onEmpty();
       view.$el.prependTo($('#render-ios'));
 
       var view = new Whisper.InboxView({window: window});
       view.$el.removeClass('android').addClass('android-dark');
+      view.onEmpty();
       view.$el.prependTo($('#render-android-dark'));
     }).then(done, done);
   });
