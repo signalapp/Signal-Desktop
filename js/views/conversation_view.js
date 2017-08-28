061abef1b2e400e896d38d37e29f50450d76e6b2
lilia <liliakai@gmail.com>
2017-08-28 18:21:40 +0200

    Update conversation header if profile avatar changes
    // FREEBIE

--- js/views/conversation_view.js
+++ js/views/conversation_view.js
@@ -106,6 +106,7 @@
             this.listenTo(this.model, 'destroy', this.stopListening);
             this.listenTo(this.model, 'change:verified', this.onVerifiedChange);
             this.listenTo(this.model, 'change:color', this.updateColor);
+            this.listenTo(this.model, 'change:avatar change:profileAvatar', this.updateAvatar);
             this.listenTo(this.model, 'newmessage', this.addMessage);
             this.listenTo(this.model, 'delivered', this.updateMessage);
             this.listenTo(this.model, 'opened', this.onOpened);
@@ -987,6 +988,15 @@
             if (color) {
                 header.addClass(color);
             }
+            var avatarView = new (Whisper.View.extend({
+                templateName: 'avatar',
+                render_attributes: { avatar: this.model.getAvatar() }
+            }))();
+            header.find('.avatar').replaceWith(avatarView.render().$('.avatar'));
+        },
+
+        updateAvatar: function(model) {
+            var header = this.$('.conversation-header');
             var avatarView = new (Whisper.View.extend({
                 templateName: 'avatar',
                 render_attributes: { avatar: this.model.getAvatar() }
