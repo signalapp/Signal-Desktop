2fecca6630e9c1cd33558d5131b2677c82b10c25
Scott Nonnenberg <scott@nonnenberg.com>
2017-09-05 10:57:38 -0700

    main.js: check for truthiness of mainwindow, not === null
    FREEBIE

--- main.js
+++ main.js
@@ -201,10 +201,10 @@ app.on('window-all-closed', function () {
 app.on('activate', function () {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow();
-  } else {
+  if (mainWindow) {
     mainWindow.show();
+  } else {
+    createWindow();
   }
 })
 
