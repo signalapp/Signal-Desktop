7622382c99120561a1b145457a43ce426d34e771
Scott Nonnenberg <scott@nonnenberg.com>
2017-10-13 22:42:39 -0700

    Remove windows from fetch-release task, fix permissions
    We no longer use the pre-built assets from AppVeyor for our Windows
    build.

--- fix_broken_perms.sh
+++ fix_broken_perms.sh
@@ -1,6 +1,4 @@
 set -e
 find release/linux -type d | xargs chmod 755
 find release/linux -type f | xargs chmod 644
-find release/windows -type d | xargs chmod 755
-find release/windows -type f | xargs chmod 644
 chmod +x release/linux/signal-desktop
