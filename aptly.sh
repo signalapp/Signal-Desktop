680b352c62b4e08857dc155411887953eaeeed13
Scott Nonnenberg <scott@nonnenberg.com>
2017-10-30 11:26:24 -0700

    aply.sh: More instructions for initial setup and testing

--- aptly.sh
+++ aptly.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
-# Setup - creates the local repo which will be mirrored up to S3:
+# Setup - creates the local repo which will be mirrored up to S3, then back-fill it. Your
+#         future deploys will eliminate all old versions without these backfill steps:
 #   aptly repo create signal-desktop
+#   aptly mirror create -ignore-signatures backfill-mirror https://updates.signal.org/desktop/apt xenial
+#   aptly mirror update -ignore-signatures backfill-mirror
+#   aptly repo import backfill-mirror signal-desktop signal-desktop signal-desktop-beta
+#   aptly repo show -with-packages signal-desktop
 #
-# First run on a machine: uncomment the first two 'aptly publish snapshot' commands,
+# First run on a machine - uncomment the first two 'aptly publish snapshot' commands,
 #   comment the other two. Sets up the two publish channels, one local, one to S3.
 #
-# Testing: comment out the lines with s3:$ENDPOINT to publish only locally
+# Testing - comment out the lines with s3:$ENDPOINT to publish only locally. To eliminate
+#          effects of testing, remove package from repo, then move back to old snapshot:
+#   aptly repo remove signal-desktop signal-desktop_1.0.35_amd64
+#   aptly publish switch -gpg-key=57F6FB06 xenial signal-desktop_v1.0.34
 #
 # Release:
 #   NAME=signal-desktop(-beta) VERSION=X.X.X ./aptly.sh
