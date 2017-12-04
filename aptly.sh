bc12521e3af76a402af5d2b89aff6799f9908fc1
Scott Nonnenberg <scott@nonnenberg.com>
2017-12-04 15:22:52 -0800

    aptly.sh: support for current and previous ubuntu versions
    artful and xenial. Sadly, this appears to double the upload time for a
    new build. :0(

--- aptly.sh
+++ aptly.sh
@@ -7,7 +7,7 @@
 #   aptly repo import backfill-mirror signal-desktop signal-desktop signal-desktop-beta
 #   aptly repo show -with-packages signal-desktop
 #
-# First run on a machine - uncomment the first two 'aptly publish snapshot' commands,
+# First run on a machine - uncomment the first set of 'aptly publish snapshot' commands,
 #   comment the other two. Sets up the two publish channels, one local, one to S3.
 #
 # Testing - comment out the lines with s3:$ENDPOINT to publish only locally. To eliminate
@@ -26,7 +26,8 @@
 echo "Releasing $NAME build version $VERSION"
 
 REPO=signal-desktop
-DISTRO=xenial
+CURRENT=artful
+PREVIOUS=xenial
 ENDPOINT=signal-desktop-apt # Matches endpoint name in .aptly.conf
 SNAPSHOT=signal-desktop_v$VERSION
 GPG_KEYID=57F6FB06
@@ -34,12 +35,17 @@ GPG_KEYID=57F6FB06
 aptly repo add $REPO release/$NAME\_$VERSION\_*.deb
 aptly snapshot create $SNAPSHOT from repo $REPO
 
-# run these two only on first release to a given repo from a given machine
+# run these only on first release to a given repo from a given machine. the first set is
+# for local testing, the second set is to set up the production server.
 #   https://www.aptly.info/doc/aptly/publish/snapshot/
-# aptly publish snapshot -gpg-key=$GPG_KEYID $SNAPSHOT
-# aptly publish snapshot -gpg-key=$GPG_KEYID -config=.aptly.conf $SNAPSHOT s3:$ENDPOINT:
+# aptly publish snapshot -gpg-key=$GPG_KEYID -distribution=$CURRENT $SNAPSHOT
+# aptly publish snapshot -gpg-key=$GPG_KEYID -distribution=$PREVIOUS $SNAPSHOT
+# aptly publish snapshot -gpg-key=$GPG_KEYID -distribution=$CURRENT -config=.aptly.conf $SNAPSHOT s3:$ENDPOINT:
+# aptly publish snapshot -gpg-key=$GPG_KEYID -distribution=$PREVIOUS -config=.aptly.conf $SNAPSHOT s3:$ENDPOINT:
 
 # these update already-published repos, run every time after that
 #   https://www.aptly.info/doc/aptly/publish/switch/
-aptly publish switch -gpg-key=$GPG_KEYID $DISTRO $SNAPSHOT
-aptly publish switch -gpg-key=$GPG_KEYID -config=.aptly.conf $DISTRO s3:$ENDPOINT: $SNAPSHOT
+aptly publish switch -gpg-key=$GPG_KEYID -distribution=$CURRENT $SNAPSHOT
+aptly publish switch -gpg-key=$GPG_KEYID -distribution=$PREVIOUS $SNAPSHOT
+aptly publish switch -gpg-key=$GPG_KEYID -beta-config=.aptly.conf $CURRENT s3:$ENDPOINT: $SNAPSHOT
+aptly publish switch -gpg-key=$GPG_KEYID -beta-config=.aptly.conf $PREVIOUS s3:$ENDPOINT: $SNAPSHOT
