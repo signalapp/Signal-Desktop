0290ba632a863b47ffb7029b4b3c1d1dd571e6ea
Scott Nonnenberg <scott@nonnenberg.com>
2017-12-21 17:46:46 -0800

    Log out information from DOMException when import fails
    This should give us the information we're looking for when imports fail
    mysteriously!
    
    https://developer.mozilla.org/en-US/docs/Web/API/DOMException

--- test/backup_test.js
+++ test/backup_test.js
@@ -1,6 +1,37 @@
 'use strict';
 
 describe('Backup', function() {
+  describe('handleDOMException', function() {
+    it('handles null, still calls reject', function() {
+      var called = 0;
+      var reject = function() {
+        called += 1;
+      };
+      var error = null;
+      var prefix = 'something';
+
+      Whisper.Backup.handleDOMException(prefix, error, reject);
+
+      assert.strictEqual(called, 1);
+    });
+
+    it('handles object code and message', function() {
+      var called = 0;
+      var reject = function() {
+        called += 1;
+      };
+      var error = {
+        code: 4,
+        message: 'some cryptic error',
+      };
+      var prefix = 'something';
+
+      Whisper.Backup.handleDOMException(prefix, error, reject);
+
+      assert.strictEqual(called, 1);
+    });
+  });
+
   describe('sanitizeFileName', function() {
     it('leaves a basic string alone', function() {
       var initial = 'Hello, how are you #5 (\'fine\' + great).jpg';
