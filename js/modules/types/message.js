70a6c4201925f57be1f94d9da3547fdefc7bbb53
Daniel Gasienica <daniel@gasienica.ch>
2018-02-13 15:46:34 -0500

    :art: Fix lint errors

--- js/modules/types/message.js
+++ js/modules/types/message.js
@@ -4,7 +4,7 @@ exports.GROUP = 'group';
 exports.PRIVATE = 'private';
 
 // Message -> Promise Message
-exports.process = async (message) =>
+exports.process = async message =>
   Object.assign({}, message, {
     attachments: await Promise.all(message.attachments.map(Attachment.process)),
-  })
+  });
