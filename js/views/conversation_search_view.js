4b326c9974bf73e2f42dffa3eb7a35e36ac037c6
Daniel Gasienica <daniel@gasienica.ch>
2018-02-26 17:41:30 -0500

    Remove unnecessary escaping for parens
    Suggested by ESLint `no-useless-escape` rule.

--- js/views/conversation_search_view.js
+++ js/views/conversation_search_view.js
@@ -164,7 +164,7 @@
     },
 
     maybeNumber(number) {
-      return number.replace(/[\s-.\(\)]*/g, '').match(/^\+?[0-9]*$/);
+      return number.replace(/[\s-.()]*/g, '').match(/^\+?[0-9]*$/);
     },
   });
 }());
