0e2f8a8a066949ed52e8e3a140e9c916872c9c5b
Daniel Gasienica <daniel@gasienica.ch>
2018-03-06 12:46:08 -0500

    Extract `Errors.redactSensitivePaths`

--- js/modules/types/errors.js
+++ js/modules/types/errors.js
@@ -2,7 +2,8 @@
 
 const Path = require('path');
 
-const toError = require('ensure-error');
+const ensureError = require('ensure-error');
+const isString = require('lodash/isString');
 
 
 const APP_ROOT_PATH = Path.join(__dirname, '..', '..', '..');
@@ -10,8 +11,16 @@ const APP_ROOT_PATH_PATTERN = new RegExp(APP_ROOT_PATH, 'g');
 
 //      toLogFormat :: Error -> String
 exports.toLogFormat = (error) => {
-  const normalizedError = toError(error);
-  const stackWithoutPrivatePaths =
-    normalizedError.stack.replace(APP_ROOT_PATH_PATTERN, '<REDACTED_PATH>');
-  return stackWithoutPrivatePaths;
+  const normalizedError = ensureError(error);
+  const stackWithRedactedPaths = exports.redactSensitivePaths(normalizedError.stack);
+  return stackWithRedactedPaths;
+};
+
+//      redactSensitivePaths :: String -> String
+exports.redactSensitivePaths = (logLine) => {
+  if (!isString(logLine)) {
+    return logLine;
+  }
+
+  return logLine.replace(APP_ROOT_PATH_PATTERN, '<REDACTED_PATH>');
 };
