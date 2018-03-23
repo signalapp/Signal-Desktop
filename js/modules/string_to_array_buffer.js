70cdd2b350bf2777ddc6ff41a1481b526bc438b5
Daniel Gasienica <daniel@signal.org>
2018-03-23 15:49:08 -0400

    Add `stringToArrayBuffer` utility
    This way we can avoid an extra dependency.

---
+++ js/modules/string_to_array_buffer.js
@@ -0,0 +1,11 @@
+exports.stringToArrayBuffer = (string) => {
+  if (typeof string !== 'string') {
+    throw new TypeError('`string` must be a string');
+  }
+
+  const array = new Uint8Array(string.length);
+  for (let i = 0; i < string.length; i += 1) {
+    array[i] = string.charCodeAt(i);
+  }
+  return array.buffer;
+};
