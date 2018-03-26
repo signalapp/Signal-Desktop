bd9fe69b691606dde1d6d93b3bd3658607f31b0a
Daniel Gasienica <daniel@signal.org>
2018-03-26 16:23:34 -0400

    Manually close database connection after migration

--- js/modules/migrations/migrations_1_database_without_attachment_data.js
+++ js/modules/migrations/migrations_1_database_without_attachment_data.js
@@ -11,5 +11,5 @@ exports.migrations = [
   },
 ];
 
-exports.run = ({ Backbone, closeDatabase, Database } = {}) =>
-  runMigrations({ Backbone, closeDatabase, database: Database });
+exports.run = ({ Backbone, Database } = {}) =>
+  runMigrations({ Backbone, database: Database });
