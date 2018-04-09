6ff82adf0a8a9d8a2d5281233aec80652a355c95
Daniel Gasienica <daniel@signal.org>
2018-04-09 19:24:47 -0400

    Add `MIME.isImage` and `MIME.isVideo`

--- ts/types/MIME.ts
+++ ts/types/MIME.ts
@@ -1,14 +1,14 @@
 export type MIMEType = string & { _mimeTypeBrand: any };
 
 
-export const isVideo = (value: MIMEType): boolean =>
-  value.startsWith('video/') && value !== 'video/wmv';
+export const isJPEG = (value: MIMEType): boolean =>
+  value === 'image/jpeg';
 
 export const isImage = (value: MIMEType): boolean =>
-  value.startsWith('image/') && value !== 'image/tiff';
+  value.startsWith('image/');
+
+export const isVideo = (value: MIMEType): boolean =>
+  value.startsWith('video/');
 
 export const isAudio = (value: MIMEType): boolean =>
   value.startsWith('audio/');
-
-export const isJPEG = (value: MIMEType): boolean =>
-  value === 'image/jpeg';
