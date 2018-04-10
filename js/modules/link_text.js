d7b845326db1aff4dd7e0609b1b2cb6dcd8bf59b
Daniel Gasienica <daniel@signal.org>
2018-04-10 15:00:55 -0400

    ESLint auto-fix `link_text`

--- js/modules/link_text.js
+++ js/modules/link_text.js
@@ -1,45 +1,39 @@
 // Fork of https://github.com/uiureo/link-text with HTML escaping disabled as we leverage
 // jQuery’s escaping mechanism:
 
-var linkify = require('linkify-it')()
+const linkify = require('linkify-it')();
 
-function createLink (url, text, attrs) {
-  attrs = attrs || {}
+function createLink(url, text, attrs = {}) {
+  const html = [];
+  html.push('<a ');
+  html.push(`href="${url}"`);
+  Object.keys(attrs).forEach((key) => {
+    html.push(` ${key}="${attrs[key]}"`);
+  });
+  html.push('>');
+  html.push(decodeURIComponent(text));
+  html.push('</a>');
 
-  var html = []
-  html.push('<a ')
-  html.push('href="' + url + '"')
-  Object.keys(attrs).forEach(function (key) {
-    html.push(' ' + key + '="' + attrs[key] + '"')
-  })
-  html.push('>')
-  html.push(decodeURIComponent(text))
-  html.push('</a>')
-
-  return html.join('')
+  return html.join('');
 }
 
-module.exports = function (text, attrs) {
-  attrs = attrs || {}
+module.exports = (text, attrs = {}) => {
+  const matchData = linkify.match(text) || [];
 
-  var matchData = linkify.match(text) || []
+  const result = [];
+  let last = 0;
 
-  var result = []
-  var last = 0
-
-  matchData.forEach(function (match) {
+  matchData.forEach((match) => {
     if (last < match.index) {
-      result.push(text.slice(last, match.index))
+      result.push(text.slice(last, match.index));
     }
 
-    result.push(
-      createLink(match.url, match.text, attrs)
-    )
+    result.push(createLink(match.url, match.text, attrs));
 
-    last = match.lastIndex
-  })
+    last = match.lastIndex;
+  });
 
-  result.push(text.slice(last))
+  result.push(text.slice(last));
 
-  return result.join('')
-}
+  return result.join('');
+};
