44debd123d98143b252b5bfd1ccfeb0f78e647b4
Daniel Gasienica <daniel@signal.org>
2018-04-11 18:49:46 -0400

    Add basic implementation of `Conversation.updateFromLastMessage`

---
+++ ts/types/Conversation.ts
@@ -0,0 +1,45 @@
+import is from '@sindresorhus/is';
+import { Message } from './Message';
+
+
+interface ConversationLastMessageUpdate {
+  lastMessage: string | null;
+  timestamp: number | null;
+}
+
+export const createLastMessageUpdate = ({
+  currentLastMessageText,
+  currentTimestamp,
+  lastMessage,
+  lastMessageNotificationText,
+}: {
+  currentLastMessageText: string | null,
+  currentTimestamp: number | null,
+  lastMessage: Message | null,
+  lastMessageNotificationText: string | null,
+}): ConversationLastMessageUpdate => {
+  if (lastMessage === null) {
+    return {
+      lastMessage: '',
+      timestamp: null,
+    };
+  }
+
+  const { type } = lastMessage;
+  const isVerifiedChangeMessage = type === 'verified-change';
+  const isExpiringMessage = is.object(lastMessage.expirationTimerUpdate);
+  const shouldUpdateTimestamp = !isVerifiedChangeMessage && !isExpiringMessage;
+
+  const newTimestamp = shouldUpdateTimestamp ?
+    lastMessage.sent_at :
+    currentTimestamp;
+
+  const shouldUpdateLastMessageText = !isVerifiedChangeMessage;
+  const newLastMessageText = shouldUpdateLastMessageText ?
+    lastMessageNotificationText : currentLastMessageText;
+
+  return {
+    lastMessage: newLastMessageText,
+    timestamp: newTimestamp,
+  };
+};
