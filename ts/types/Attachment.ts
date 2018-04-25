4a5e61eaf4a0faa811fd491531c2e0fe360aac53
Daniel Gasienica <daniel@signal.org>
2018-04-25 13:27:28 -0400

    Implement `Attachment.save`

--- ts/types/Attachment.ts
+++ ts/types/Attachment.ts
@@ -2,8 +2,10 @@
  * @prettier
  */
 import is from '@sindresorhus/is';
+import moment from 'moment';
 
 import * as GoogleChrome from '../util/GoogleChrome';
+import { arrayBufferToObjectURL } from '../util/arrayBufferToObjectURL';
 import { MIMEType } from './MIME';
 
 export interface Attachment {
@@ -23,6 +25,8 @@ export interface Attachment {
   // flags?: number;
 }
 
+const SAVE_CONTENT_TYPE = 'application/octet-stream' as MIMEType;
+
 export const isVisualMedia = (attachment: Attachment): boolean => {
   const { contentType } = attachment;
 
@@ -33,4 +37,56 @@ export const isVisualMedia = (attachment: Attachment): boolean => {
   const isSupportedImageType = GoogleChrome.isImageTypeSupported(contentType);
   const isSupportedVideoType = GoogleChrome.isVideoTypeSupported(contentType);
   return isSupportedImageType || isSupportedVideoType;
+};
+
+export const save = ({
+  attachment,
+  timestamp,
+}: {
+  attachment: Attachment;
+  timestamp?: number;
+}): void => {
+  const url = arrayBufferToObjectURL({
+    data: attachment.data,
+    type: SAVE_CONTENT_TYPE,
+  });
+  const anchorElement = document.createElement('a');
+  anchorElement.href = url;
+  anchorElement.download = getSuggestedFilename({ attachment, timestamp });
+  anchorElement.click();
+  URL.revokeObjectURL(url);
+};
+
+export const getSuggestedFilename = ({
+  attachment,
+  timestamp,
+}: {
+  attachment: Attachment;
+  timestamp?: number | Date;
+}): string => {
+  if (attachment.fileName) {
+    return attachment.fileName;
+  }
+
+  const prefix = 'signal-attachment';
+  const suffix = timestamp
+    ? moment(timestamp).format('-YYYY-MM-DD-HHmmss')
+    : '';
+  const fileType = getFileExtension(attachment);
+  const extension = fileType ? `.${fileType}` : '';
+  return `${prefix}${suffix}${extension}`;
+};
+
+export const getFileExtension = (attachment: Attachment): string | null => {
+  if (!attachment.contentType) {
+    return null;
+  }
+
+  switch (attachment.contentType) {
+    case 'video/quicktime':
+      return 'mov';
+    default:
+      // TODO: Use better MIME --> file extension mapping:
+      return attachment.contentType.split('/')[1];
+  }
 };
