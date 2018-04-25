6ee56b8445b24df667636db866b868d986ae4663
Daniel Gasienica <daniel@signal.org>
2018-04-25 18:02:27 -0400

    Remove newlines

--- ts/types/MIME.ts
+++ ts/types/MIME.ts
@@ -4,9 +4,6 @@
 export type MIMEType = string & { _mimeTypeBrand: any };
 
 export const isJPEG = (value: MIMEType): boolean => value === 'image/jpeg';
-
 export const isImage = (value: MIMEType): boolean => value.startsWith('image/');
-
 export const isVideo = (value: MIMEType): boolean => value.startsWith('video/');
-
 export const isAudio = (value: MIMEType): boolean => value.startsWith('audio/');
