b8d20052e10692edba0184650c74834c3d818173
Daniel Gasienica <daniel@signal.org>
2018-04-30 11:00:38 -0400

    Remove unnecessary `AttachmentTS` reference

--- preload.js
+++ preload.js
@@ -205,7 +205,6 @@ window.Signal.Startup = require('./js/modules/startup');
 
 window.Signal.Types = {};
 window.Signal.Types.Attachment = Attachment;
-window.Signal.Types.AttachmentTS = require('./ts/types/Attachment');
 window.Signal.Types.Conversation = require('./ts/types/Conversation');
 window.Signal.Types.Errors = require('./js/modules/types/errors');
 
