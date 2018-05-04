5d230d377867e9665edccb805cc9e7b9697050e1
Daniel Gasienica <daniel@signal.org>
2018-05-04 15:27:53 -0400

    Add `Settings.isNotificationGroupingSupported`
    See: https://github.com/electron/electron/issues/11189

--- ts/types/Settings.ts
+++ ts/types/Settings.ts
@@ -4,3 +4,8 @@ const MIN_WINDOWS_VERSION = '8.0.0';
 
 export const isAudioNotificationSupported = () =>
   OS.isWindows(MIN_WINDOWS_VERSION) || OS.isMacOS();
+
+// Using `Notification::tag` has a bug on Windows 7:
+// https://github.com/electron/electron/issues/11189
+export const isNotificationGroupingSupported = () =>
+  !OS.isWindows() || OS.isWindows(MIN_WINDOWS_VERSION);
