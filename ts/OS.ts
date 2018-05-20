900b452cecbbffb1583390ec2b40eacd620c2161
jython234 <jython234@users.noreply.github.com>
2018-05-20 13:52:13 -0500

    Fix formatting for OS.ts

--- ts/OS.ts
+++ ts/OS.ts
@@ -5,8 +5,8 @@ import semver from 'semver';
 export const isMacOS = () => process.platform === 'darwin';
 export const isLinux = () => process.platform === 'linux';
 export const isWindows = (minVersion?: string) => {
-  if(process.platform !== 'win32') return false;
-  
+  if (process.platform !== 'win32') return false;
+
   const osRelease = os.release();
   const isVersionValid = is.undefined(minVersion)
     ? true
