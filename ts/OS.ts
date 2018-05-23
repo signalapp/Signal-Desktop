925e02e3973b7578c7ec99a599c709f9cb779750
Scott Nonnenberg <scott@signal.org>
2018-05-23 16:04:39 -0700

    Fix lint error in OS.ts

--- ts/OS.ts
+++ ts/OS.ts
@@ -5,12 +5,11 @@ import semver from 'semver';
 export const isMacOS = () => process.platform === 'darwin';
 export const isLinux = () => process.platform === 'linux';
 export const isWindows = (minVersion?: string) => {
-  if (process.platform !== 'win32') return false;
-
   const osRelease = os.release();
-  const isVersionValid = is.undefined(minVersion)
-    ? true
-    : semver.gte(osRelease, minVersion);
 
-  return isVersionValid;
+  if (process.platform !== 'win32') {
+    return false;
+  }
+
+  return is.undefined(minVersion) ? true : semver.gte(osRelease, minVersion);
 };
