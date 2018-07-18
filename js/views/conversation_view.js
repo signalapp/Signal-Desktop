6e4b73ec0cac962fa7b4985f5d4298ef1a58f589
Scott Nonnenberg <scott@signal.org>
2018-07-18 09:44:14 -0700

    Fix lint errors in conversation_view.js

--- js/views/conversation_view.js
+++ js/views/conversation_view.js
@@ -14,7 +14,6 @@
   'use strict';
 
   window.Whisper = window.Whisper || {};
-  const { Migrations } = Signal;
   const { Message } = window.Signal.Types;
   const {
     upgradeMessageSchema,
@@ -968,8 +967,6 @@
     },
 
     downloadAttachment({ attachment, message }) {
-      const { getAbsoluteAttachmentPath } = Migrations;
-
       Signal.Types.Attachment.save({
         attachment,
         document,
@@ -994,7 +991,6 @@
     },
 
     showLightbox({ attachment, message }) {
-      const { getAbsoluteAttachmentPath } = Migrations;
       const { contentType, path } = attachment;
 
       if (
@@ -1044,7 +1040,6 @@
     showContactDetail({ contact, hasSignalAccount }) {
       const regionCode = storage.get('regionCode');
       const { contactSelector } = Signal.Types.Contact;
-      const { getAbsoluteAttachmentPath } = window.Signal.Migrations;
 
       const view = new Whisper.ReactWrapperView({
         Component: Signal.Components.ContactDetail,
