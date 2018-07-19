d9e4473c9df700eaddea1ee98dddf1b2467d8f65
Scott Nonnenberg <scott@signal.org>
2018-07-19 16:15:51 -0700

    Workaround for linux w/tmp mounted as noexec; spellcheck fails

--- js/background.js
+++ js/background.js
@@ -192,6 +192,10 @@
     };
 
     const startSpellCheck = () => {
+      if (!window.enableSpellCheck || !window.disableSpellCheck) {
+        return;
+      }
+
       if (window.Events.getSpellCheck()) {
         window.enableSpellCheck();
       } else {
