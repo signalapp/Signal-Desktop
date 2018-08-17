b043a469a25583fc8655363b295449e7f9dea525
sachaaaaa <sacha@loki.network>
2018-08-17 16:11:07 +1000

    Change regex that filters potentially valid contacts in search bar to match a pub key

--- js/views/conversation_search_view.js
+++ js/views/conversation_search_view.js
@@ -173,7 +173,7 @@
     },
 
     maybeNumber(number) {
-      return number.replace(/[\s-.()]*/g, '').match(/^\+?[0-9]*$/);
+      return number.replace(/[\s]*/g, '').match(/^[0-9a-zA-Z\/+]+$/);
     },
   });
 })();
