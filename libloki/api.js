1238cbc4e61724cfd24017c97cb8bc704f29f7fd
sachaaaaa <sacha@loki.network>
2019-01-23 14:58:28 +1100

    Send profile along with friend request accept message

--- libloki/api.js
+++ libloki/api.js
@@ -1,4 +1,4 @@
-/* global window, textsecure, log */
+/* global window, textsecure */
 
 // eslint-disable-next-line func-names
 (function() {
@@ -9,29 +9,19 @@
   }
 
   async function sendEmptyMessage(pubKey) {
-    // empty content message
-    const content = new textsecure.protobuf.Content();
-
-    // will be called once the transmission succeeded or failed
-    const callback = res => {
-      if (res.errors.length > 0) {
-        res.errors.forEach(error => log.error(error));
-      } else {
-        log.info('empty message sent successfully');
-      }
-    };
     const options = {};
-    // send an empty message. The logic in ougoing_message will attach the prekeys.
-    const outgoingMessage = new textsecure.OutgoingMessage(
-      null, // server
-      Date.now(), // timestamp,
-      [pubKey], // numbers
-      content, // message
-      true, // silent
-      callback, // callback
+    // send an empty message.
+    // The logic downstream will attach the prekeys and our profile.
+    await textsecure.messaging.sendMessageToNumber(
+      pubKey, // number
+      null, // messageText
+      [], // attachments
+      null, // quote
+      Date.now(), // timestamp
+      null, // expireTimer
+      null, // profileKey
       options
     );
-    await outgoingMessage.sendToNumber(pubKey);
   }
 
   window.libloki.api = {
