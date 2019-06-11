d496b7f5ef4013b160745f9cee3f857376ac6bd3
Peter Uithoven <peter@peteruithoven.nl>
2019-06-11 17:54:05 +0200

    Alter desktopName in prepare beta build script

--- prepare_beta_build.js
+++ prepare_beta_build.js
@@ -38,6 +38,10 @@ const STARTUP_WM_CLASS_PATH = 'build.linux.desktop.StartupWMClass';
 const PRODUCTION_STARTUP_WM_CLASS = 'Signal';
 const BETA_STARTUP_WM_CLASS = 'Signal Beta';
 
+const DESKTOP_NAME_PATH = 'desktopName';
+const PRODUCTION_DESKTOP_NAME = PRODUCTION_NAME + '.desktop';
+const BETA_DESKTOP_NAME = BETA_NAME + '.desktop';
+
 // -------
 
 function checkValue(object, objectPath, expected) {
@@ -53,6 +57,7 @@ checkValue(packageJson, NAME_PATH, PRODUCTION_NAME);
 checkValue(packageJson, PRODUCT_NAME_PATH, PRODUCTION_PRODUCT_NAME);
 checkValue(packageJson, APP_ID_PATH, PRODUCTION_APP_ID);
 checkValue(packageJson, STARTUP_WM_CLASS_PATH, PRODUCTION_STARTUP_WM_CLASS);
+checkValue(packageJson, DESKTOP_NAME_PATH, PRODUCTION_DESKTOP_NAME);
 
 // -------
 
@@ -60,6 +65,7 @@ _.set(packageJson, NAME_PATH, BETA_NAME);
 _.set(packageJson, PRODUCT_NAME_PATH, BETA_PRODUCT_NAME);
 _.set(packageJson, APP_ID_PATH, BETA_APP_ID);
 _.set(packageJson, STARTUP_WM_CLASS_PATH, BETA_STARTUP_WM_CLASS);
+_.set(packageJson, DESKTOP_NAME_PATH, BETA_DESKTOP_NAME);
 
 // -------
 
