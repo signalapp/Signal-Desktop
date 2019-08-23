680720267eec0d144c22f089d27b0c6e050a26bd
sha-265 <4103710+sha-265@users.noreply.github.com>
2019-08-23 19:18:59 +0300

    Add RTL support for conversation list message text

--- ts/components/ConversationListItem.tsx
+++ ts/components/ConversationListItem.tsx
@@ -157,7 +157,7 @@ export class ConversationListItem extends React.PureComponent<Props> {
 
     return (
       <div className="module-conversation-list-item__message">
-        <div
+        <div dir="auto"
           className={classNames(
             'module-conversation-list-item__message__text',
             unreadCount > 0
