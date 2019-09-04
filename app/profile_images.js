a9189979e1f46b272470748fc9c1e28e59306dbb
Mikunj <mikunj@live.com.au>
2019-09-05 09:24:24 +1000

    Added JazzIcon

--- app/profile_images.js
+++ app/profile_images.js
@@ -1,7 +1,6 @@
 const fs = require('fs');
 const mkdirp = require('mkdirp');
 const path = require('path');
-const sha224 = require('js-sha512').sha512_224;
 
 const { app } = require('electron').remote;
 
@@ -36,11 +35,10 @@ const writePNGImage = (base64String, pubKey) => {
   const imagePath = getImagePath(pubKey);
   fs.writeFileSync(imagePath, base64String, 'base64');
   return imagePath;
-}
+};
 
 module.exports = {
   writePNGImage,
-  getOrCreateImagePath,
   getImagePath,
   hasImage,
   removeImage,
