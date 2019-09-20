9db42bc4c65d4ba527b5f113e1fcdbc8ff699196
Scott Nonnenberg <scott@signal.org>
2019-09-20 11:30:55 -0700

    sql_channel: Ensure we save reference to MultipleQueue to start

--- app/sql_channel.js
+++ app/sql_channel.js
@@ -73,12 +73,14 @@ function initialize() {
           makeNewMultipleQueue();
           multipleQueue.pause();
 
+          const multipleQueueRef = multipleQueue;
           const singleQueueRef = singleQueue;
+
           singleQueue = null;
-          const promise = multipleQueue.add(() => fn(...args));
+          const promise = multipleQueueRef.add(() => fn(...args));
           await singleQueueRef.onIdle();
 
-          multipleQueue.start();
+          multipleQueueRef.start();
           result = await promise;
         } else {
           makeNewMultipleQueue();
