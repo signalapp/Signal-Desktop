8194b2446b115d7cb195e62a55a79fde18d230e6
Scott Nonnenberg <scott@signal.org>
2019-11-14 18:37:25 -0800

    Move to CTRL+= for windows, remove the need for the shift

--- app/menu.js
+++ app/menu.js
@@ -86,7 +86,7 @@ exports.createTemplate = (options, messages) => {
           label: messages.viewMenuResetZoom.message,
         },
         {
-          accelerator: platform === 'darwin' ? 'Command+=' : null,
+          accelerator: platform === 'darwin' ? 'Command+=' : 'Control+=',
           role: 'zoomin',
           label: messages.viewMenuZoomIn.message,
         },
