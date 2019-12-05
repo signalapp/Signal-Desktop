24e24681a6af8aef74e5a53a16250e2b0ba0a0e4
Audric Ackermann <audric@loki.network>
2019-12-05 11:04:48 +1100

    add a custom session button

---
+++ ts/components/session/SessionButton.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import classNames from 'classnames';
+
+//import { LocalizerType } from '../../types/Util';
+
+export enum SessionButtonTypes {
+  fullGreen = 'fullGreen',
+  white = 'white',
+  green = 'green',
+}
+
+interface Props {
+  //i18n: LocalizerType;
+  text: string;
+  buttonType: SessionButtonTypes;
+}
+
+
+export class SessionButton extends React.PureComponent<Props> {
+
+  public render() {
+    const {
+      buttonType,
+      text
+    } = this.props;
+
+    return (
+        <div  
+          className={classNames(
+            'session-button',
+            buttonType === SessionButtonTypes.green? 'green' : '',
+            buttonType === SessionButtonTypes.fullGreen? 'full-green' : '',
+            buttonType === SessionButtonTypes.white? 'white' : '',
+            )}
+            role='button'
+        >
+          {text}
+        </div>
+    );
+  }
+}
