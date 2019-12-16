cca4b51c8f779ea2bce60142cb6ce7780573bd61
Audric Ackermann <audric@loki.network>
2019-12-16 11:27:27 +1100

    remove showSubtitle option on registration page

--- ts/components/session/AccentText.tsx
+++ ts/components/session/AccentText.tsx
@@ -2,28 +2,10 @@ import React from 'react';
 
 import { SessionHtmlRenderer } from './SessionHTMLRenderer';
 
-interface Props {
-  showSubtitle?: boolean;
-}
-export class AccentText extends React.PureComponent<Props> {
-  constructor(props: any) {
-    super(props);
-  }
-
-  public render() {
-    const { showSubtitle } = this.props;
-
-    return (
-      <div className="session-content-accent-text">
-        <div className="session-content-accent-text title">
-          <SessionHtmlRenderer html={window.i18n('beginYourSession')} />
-        </div>
-        {showSubtitle && (
-          <div className="session-content-accent-text subtitle">
-            <SessionHtmlRenderer html={window.i18n('ensuringPeaceOfMind...')} />
-          </div>
-        )}
-      </div>
-    );
-  }
-}
+export const AccentText: React.FC = () => (
+  <div className="session-content-accent-text">
+    <div className="session-content-accent-text title">
+      <SessionHtmlRenderer html={window.i18n('beginYourSession')} />
+    </div>
+  </div>
+);
--- ts/components/session/SessionRegistrationView.tsx
+++ ts/components/session/SessionRegistrationView.tsx
@@ -3,28 +3,14 @@ import { AccentText } from './AccentText';
 
 import { RegistrationTabs } from './RegistrationTabs';
 
-interface Props {
-  showSubtitle: boolean;
-}
-
-export class SessionRegistrationView extends React.Component<Props> {
-  constructor(props: Props) {
-    super(props);
-  }
-
-  public render() {
-    const { showSubtitle } = this.props;
-
-    return (
-      <div className="session-content">
-        <div id="error" className="collapse" />
-        <div className="session-content-accent">
-          <AccentText showSubtitle={showSubtitle} />
-        </div>
-        <div className="session-content-registration">
-          <RegistrationTabs />
-        </div>
-      </div>
-    );
-  }
-}
+export const SessionRegistrationView: React.FC = () => (
+  <div className="session-content">
+    <div id="error" className="collapse" />
+    <div className="session-content-accent">
+      <AccentText />
+    </div>
+    <div className="session-content-registration">
+      <RegistrationTabs />
+    </div>
+  </div>
+);
