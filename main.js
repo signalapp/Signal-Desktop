b113eb19f0d2aa0be8755f9934a65dd0d64adb5f
Ken Powers <ken@signal.org>
2020-01-17 12:49:01 -0500

    Increase minimum window width to 680px
    When we grew the left sidebar we neglected to increase the minimum window size to allow for context menus to show up in the timeline for attachments.

--- main.js
+++ main.js
@@ -196,7 +196,7 @@ function captureClicks(window) {
 
 const DEFAULT_WIDTH = 800;
 const DEFAULT_HEIGHT = 610;
-const MIN_WIDTH = 640;
+const MIN_WIDTH = 680;
 const MIN_HEIGHT = 550;
 const BOUNDS_BUFFER = 100;
 
