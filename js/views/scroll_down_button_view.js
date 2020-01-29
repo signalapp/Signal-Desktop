6c2aefbe1188035bfc208ec11b64a832124b4248
Audric Ackermann <audric@loki.network>
2020-01-29 13:35:23 +1100

    fix unit tests: remove not used count in scrolldown view and assert svg present

--- js/views/scroll_down_button_view.js
+++ js/views/scroll_down_button_view.js
@@ -7,22 +7,12 @@
   window.Whisper = window.Whisper || {};
 
   Whisper.ScrollDownButtonView = Whisper.View.extend({
-    initialize(options = {}) {
-      this.count = options.count || 0;
-    },
-
-    increment(count = 0) {
-      this.count += count;
-      this.render();
-    },
+    initialize() {},
 
     render() {
       this.scrollButtonView = new Whisper.ReactWrapperView({
         className: 'module-scroll-down',
         Component: window.Signal.Components.SessionScrollButton,
-        props: {
-          count: this.count,
-        },
       });
 
       this.$el.append(this.scrollButtonView.el);
