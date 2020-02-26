8066cc0709c05bcdcdf8079ba9751dc2b6510a6b
Audric Ackermann <audric@loki.network>
2020-02-26 15:58:12 +1100

    add link to explain tslint issue with sfc and props type

--- ts/components/session/PillDivider.tsx
+++ ts/components/session/PillDivider.tsx
@@ -3,6 +3,8 @@ import React from 'react';
 interface ReceivedProps {
   text: string;
 }
+
+// Needed because of https://github.com/microsoft/tslint-microsoft-contrib/issues/339
 type Props = ReceivedProps;
 
 export const PillDivider: React.SFC<Props> = props => {
