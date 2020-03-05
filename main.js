12457d47a337cd413d044bc77fde544048c60857
Josh Perez <josh.p@signal.org>
2020-03-05 13:24:51 -0800

    Move to new logo and primary blue color

--- main.js
+++ main.js
@@ -249,7 +249,7 @@ function createWindow() {
       backgroundColor:
         config.environment === 'test' || config.environment === 'test-lib'
           ? '#ffffff' // Tests should always be rendered on a white background
-          : '#2090EA',
+          : '#3a76f0',
       vibrancy: 'appearance-based',
       webPreferences: {
         nodeIntegration: false,
@@ -494,7 +494,7 @@ function showAbout() {
     resizable: false,
     title: locale.messages.aboutSignalDesktop.message,
     autoHideMenuBar: true,
-    backgroundColor: '#2090EA',
+    backgroundColor: '#3a76f0',
     show: false,
     vibrancy: 'appearance-based',
     webPreferences: {
@@ -542,7 +542,7 @@ async function showSettingsWindow() {
     resizable: false,
     title: locale.messages.signalDesktopPreferences.message,
     autoHideMenuBar: true,
-    backgroundColor: '#2090EA',
+    backgroundColor: '#3a76f0',
     show: false,
     modal: true,
     vibrancy: 'appearance-based',
@@ -612,7 +612,7 @@ async function showStickerCreator() {
     height: 650,
     title: locale.messages.signalDesktopStickerCreator,
     autoHideMenuBar: true,
-    backgroundColor: '#2090EA',
+    backgroundColor: '#3a76f0',
     show: false,
     webPreferences: {
       nodeIntegration: false,
@@ -662,7 +662,7 @@ async function showDebugLogWindow() {
     resizable: false,
     title: locale.messages.debugLog.message,
     autoHideMenuBar: true,
-    backgroundColor: '#2090EA',
+    backgroundColor: '#3a76f0',
     show: false,
     modal: true,
     vibrancy: 'appearance-based',
@@ -711,7 +711,7 @@ async function showPermissionsPopupWindow() {
     resizable: false,
     title: locale.messages.allowAccess.message,
     autoHideMenuBar: true,
-    backgroundColor: '#2090EA',
+    backgroundColor: '#3a76f0',
     show: false,
     modal: true,
     vibrancy: 'appearance-based',
