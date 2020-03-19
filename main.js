67f94334c2c5ba755671804c8a19e1f7268f52d9
Vincent <vincent@loki.network>
2020-03-20 10:25:15 +1100

    hardcoded

--- main.js
+++ main.js
@@ -27,7 +27,8 @@ const {
   shell,
 } = electron;
 
-const appUserModelId = packageJson.build.appId;
+//const appUserModelId = packageJson.build.appId;
+const appUserModelId = 'com.loki-project.messenger-desktop';
 console.log('Set Windows Application User Model ID (AUMID)', {
   appUserModelId,
 });
