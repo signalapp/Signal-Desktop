553713c31147c32a776ea6319a1fb2a3bfc3c0d8
Vincent <vincent@loki.network>
2020-03-26 13:03:46 +1100

    revert

--- integration_test/test_test.js
+++ integration_test/test_test.js
@@ -1,45 +1,45 @@
-/* eslint-disable func-names  */
-/* eslint-disable import/no-extraneous-dependencies */
-const common = require('./common');
-const { afterEach, beforeEach, describe, it } = require('mocha');
+// /* eslint-disable func-names  */
+// /* eslint-disable import/no-extraneous-dependencies */
+// const common = require('./common');
+// const { afterEach, beforeEach, describe, it } = require('mocha');
 
 
-describe('Do Vince test', function() {
-  let app;
-  let app2;
-  this.timeout(60000);
-  this.slow(15000);
+// describe('Do Vince test', function() {
+//   let app;
+//   let app2;
+//   this.timeout(60000);
+//   this.slow(15000);
 
-  beforeEach(async () => {
-    await common.killallElectron();
-    await common.stopStubSnodeServer();
+//   beforeEach(async () => {
+//     await common.killallElectron();
+//     await common.stopStubSnodeServer();
 
-    const app1Props = {
-      mnemonic: common.TEST_MNEMONIC1,
-      displayName: common.TEST_DISPLAY_NAME1,
-      stubSnode: true,
-    };
+//     const app1Props = {
+//       mnemonic: common.TEST_MNEMONIC1,
+//       displayName: common.TEST_DISPLAY_NAME1,
+//       stubSnode: true,
+//     };
   
-    const app2Props = {
-      mnemonic: common.TEST_MNEMONIC2,
-      displayName: common.TEST_DISPLAY_NAME2,
-      stubSnode: true,
-    };
+//     const app2Props = {
+//       mnemonic: common.TEST_MNEMONIC2,
+//       displayName: common.TEST_DISPLAY_NAME2,
+//       stubSnode: true,
+//     };
 
-    [app, app2] = await Promise.all([
-      common.startAndStub(app1Props),
-      common.startAndStub2(app2Props),
-    ]);
-  });
+//     [app, app2] = await Promise.all([
+//       common.startAndStub(app1Props),
+//       common.startAndStub2(app2Props),
+//     ]);
+//   });
 
-  afterEach(async () => {
-    await common.stopApp(app);
-    await common.killallElectron();
-    await common.stopStubSnodeServer();
-  });
+//   afterEach(async () => {
+//     await common.stopApp(app);
+//     await common.killallElectron();
+//     await common.stopStubSnodeServer();
+//   });
 
-  it('Can do first check', async () => {
-    app = await common.startAndAssureCleanedApp();
-    return app;
-  });
-});
+//   it('Can do first check', async () => {
+//     app = await common.startAndAssureCleanedApp();
+//     return app;
+//   });
+// });
