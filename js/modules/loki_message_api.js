4384229b4c02f84c88e0c36ce3469faf5a0b4eb2
Ryan Tharp <neuro@interx.net>
2020-03-30 23:36:10 -0700

    log when _retrieveNextMessages's call to lokiRpc fails

--- js/modules/loki_message_api.js
+++ js/modules/loki_message_api.js
@@ -399,6 +399,11 @@ class LokiMessageAPI {
       nodeData
     );
 
+    if (result === false) {
+      // make a note of it because of caller doesn't care...
+      log.warn(`loki_message:::_retrieveNextMessages - lokiRpc returned false to ${nodeData.ip}:${nodeData.port}`);
+    }
+
     return result.messages || [];
   }
 
