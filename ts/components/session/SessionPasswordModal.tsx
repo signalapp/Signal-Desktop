9349ebd37ab70e3d8f7f619f7ae793de8703a498
Vincent <vincent@loki.network>
2020-03-31 17:56:23 +1100

    fancy syntax

--- ts/components/session/SessionPasswordModal.tsx
+++ ts/components/session/SessionPasswordModal.tsx
@@ -44,11 +44,7 @@ export class SessionPasswordModal extends React.Component<Props, State> {
 
   public componentDidMount() {
     setTimeout(() => {
-      if (!this.passwordInput.current) {
-        return;
-      }
-
-      this.passwordInput.current.focus();
+      this.passwordInput.current?.focus();
     }, 100);
   }
 
@@ -140,14 +136,10 @@ export class SessionPasswordModal extends React.Component<Props, State> {
   }
 
   private async setPassword(onSuccess: any) {
-    if (!this.passwordInput.current || !this.passwordInputConfirm.current) {
-      return;
-    }
-
     // Trim leading / trailing whitespace for UX
-    const enteredPassword = String(this.passwordInput.current.value).trim();
+    const enteredPassword = String(this.passwordInput.current?.value).trim();
     const enteredPasswordConfirm = String(
-      this.passwordInputConfirm.current.value
+      this.passwordInputConfirm.current?.value
     ).trim();
 
     if (enteredPassword.length === 0 || enteredPasswordConfirm.length === 0) {
