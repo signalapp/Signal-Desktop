0bffccb44120b6bd10bdbb70d398704932d478e0
Ryan Tharp <neuro@interx.net>
2020-04-02 00:19:25 -0700

    setValueWrapper refactor, add group prefix for easier grepping, DRY

--- integration_test/add_friends_test.js
+++ integration_test/add_friends_test.js
@@ -38,7 +38,7 @@ describe('Add friends', function() {
     await common.stopStubSnodeServer();
   });
 
-  it('can add a friend by sessionID', async () => {
+  it('addFriends: can add a friend by sessionID', async () => {
     const textMessage = common.generateSendMessageText();
 
     await app.client.element(ConversationPage.contactsButtonSection).click();
@@ -46,13 +46,16 @@ describe('Add friends', function() {
     await app.client.isExisting(ConversationPage.leftPaneOverlay).should
       .eventually.be.true;
 
-    await app.client
-      .element(ConversationPage.sessionIDInput)
-      .setValue(common.TEST_PUBKEY2);
+    await common.setValueWrapper(
+      app,
+      ConversationPage.sessionIDInput,
+      common.TEST_PUBKEY2
+    );
     await app.client
       .element(ConversationPage.sessionIDInput)
       .getValue()
       .should.eventually.equal(common.TEST_PUBKEY2);
+
     await app.client.element(ConversationPage.nextButton).click();
     await app.client.waitForExist(
       ConversationPage.sendFriendRequestTextarea,
@@ -68,6 +71,7 @@ describe('Add friends', function() {
       ConversationPage.existingFriendRequestText(textMessage),
       1000
     );
+
     // assure friend request message has been sent
     await common.timeout(3000);
     await app.client.isExisting(ConversationPage.retrySendButton).should
