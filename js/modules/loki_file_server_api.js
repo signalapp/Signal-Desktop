1ee2088343f0db99f86208dc5988c0829ca1103b
Ryan Tharp <neuro@interx.net>
2020-04-03 03:46:39 -0700

    Fix integration tests slipping into release
    Per Mikunj

--- js/modules/loki_file_server_api.js
+++ js/modules/loki_file_server_api.js
@@ -4,7 +4,6 @@
 /* global log: false */
 
 const LokiAppDotNetAPI = require('./loki_app_dot_net_api');
-const StubAppDotNetAPI = require('../../integration_test/stubs/stub_app_dot_net_api.js');
 
 const DEVICE_MAPPING_USER_ANNOTATION_TYPE =
   'network.loki.messenger.devicemapping';
@@ -60,6 +59,7 @@ class LokiFileServerInstance {
   async establishConnection(serverUrl, options) {
     // why don't we extend this?
     if (process.env.USE_STUBBED_NETWORK) {
+      const StubAppDotNetAPI = require('../../integration_test/stubs/stub_app_dot_net_api.js');
       this._server = new StubAppDotNetAPI(this.ourKey, serverUrl);
     } else {
       this._server = new LokiAppDotNetAPI(this.ourKey, serverUrl);
