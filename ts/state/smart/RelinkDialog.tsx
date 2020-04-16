4dc7631851184422ce9cb682155663978ccb81a7
Josh Perez <60019601+josh-signal@users.noreply.github.com>
2020-04-16 15:20:52 -0400

    Decouple RelinkDialog from NetworkStatusDialog

--- ts/state/smart/RelinkDialog.tsx
+++ ts/state/smart/RelinkDialog.tsx
@@ -1,18 +1,19 @@
 import { connect } from 'react-redux';
 import { mapDispatchToProps } from '../actions';
-import { NetworkStatus } from '../../components/NetworkStatus';
+import { RelinkDialog } from '../../components/RelinkDialog';
 import { StateType } from '../reducer';
 import { getIntl } from '../selectors/user';
 import { hasNetworkDialog } from '../selectors/network';
+import { isDone } from '../../util/registration';
 
 const mapStateToProps = (state: StateType) => {
   return {
-    ...state.network,
     hasNetworkDialog: hasNetworkDialog(state),
     i18n: getIntl(state),
+    isRegistrationDone: isDone(),
   };
 };
 
 const smart = connect(mapStateToProps, mapDispatchToProps);
 
-export const SmartNetworkStatus = smart(NetworkStatus);
+export const SmartRelinkDialog = smart(RelinkDialog);
