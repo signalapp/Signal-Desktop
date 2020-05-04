7bb7c0d1e1579eb087ba756a2af51ddac6de00fd
Ken Powers <ken@signal.org>
2020-05-04 15:20:01 -0400

    Debug Log: Copy URL instead of open in browser

--- debug_log_preload.js
+++ debug_log_preload.js
@@ -2,6 +2,7 @@
 
 const { ipcRenderer } = require('electron');
 const url = require('url');
+const copyText = require('copy-text-to-clipboard');
 const i18n = require('./js/modules/i18n');
 
 const config = url.parse(window.location.toString(), true).query;
@@ -11,6 +12,7 @@ const localeMessages = ipcRenderer.sendSync('locale-data');
 window.getVersion = () => config.version;
 window.theme = config.theme;
 window.i18n = i18n.setup(locale, localeMessages);
+window.copyText = copyText;
 
 // got.js appears to need this to successfully submit debug logs to the cloud
 window.nodeSetImmediate = setImmediate;
