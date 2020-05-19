aa6371b6b066f3ecf6a4179eb1a3f474b6e34543
Audric Ackermann <audric@loki.network>
2020-05-19 14:02:23 +1000

    fix bug friend request being auto accepted by unknown user from their secondary device

--- js/models/messages.js
+++ js/models/messages.js
@@ -1941,6 +1941,7 @@
       const { primaryDevicePubKey } = authorisation;
       // ensure the primary device is a friend
       const c = window.ConversationController.get(primaryDevicePubKey);
+
       if (!c || !await c.isFriendWithAnyDevice()) {
         return false;
       }
