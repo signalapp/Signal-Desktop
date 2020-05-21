15560a4cb58c185c41b96079e0fe0fef943a0fbd
Mikunj <mikunj@live.com.au>
2020-05-21 16:44:18 +1000

    Linting and comments

--- ts/session/messages/outgoing/OutgoingContentMessage.ts
+++ ts/session/messages/outgoing/OutgoingContentMessage.ts
@@ -5,16 +5,15 @@ export class OutgoingContentMessage implements OutgoingMessage {
   public timestamp: number;
   public identifier: string;
   public ttl: number;
-  constructor(
-    timestamp: number,
-    identifier: string,
-    ttl: number
-  ) {
+  constructor(timestamp: number, identifier: string, ttl: number) {
     this.timestamp = timestamp;
     this.identifier = identifier;
     this.ttl = ttl;
   }
 
+  // To discuss:
+  // should padding be the responsibility of the message or should it be the responsibility of the message sender to pad messages
+  // If it is the responsibility of the sender then `contentProto()` needs to become `protected` and not `public`
   public plainTextBuffer(): Uint8Array {
     const encoded = SignalService.Content.encode(this.contentProto()).finish();
 
