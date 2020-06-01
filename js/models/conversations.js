d4ee1d4133851f49ec848e2cef001fe70592205f
Ken Powers <ken@signal.org>
2020-06-01 18:00:13 -0400

    getProfile: Pull information from sendMetadata properly

--- js/models/conversations.js
+++ js/models/conversations.js
@@ -1966,7 +1966,8 @@
         }
 
         const sendMetadata = c.getSendMetadata({ disableMeCheck: true }) || {};
-        const getInfo = sendMetadata[c.id] || {};
+        const getInfo =
+          sendMetadata[c.get('e164')] || sendMetadata[c.get('uuid')] || {};
 
         if (getInfo.accessKey) {
           try {
