2d4e9455c2d11fa8a4dc3bfcab357a09a02de2de
Vincent <vincent@loki.network>
2020-07-14 13:31:51 +1000

    asd

--- ts/receiver/receiver.ts
+++ ts/receiver/receiver.ts
@@ -14,6 +14,7 @@ import { onError } from './errors';
 import {
   handleContentMessage,
   innerHandleContentMessage,
+  isBlocked,
   onDeliveryReceipt,
 } from './contentMessage';
 import _ from 'lodash';
