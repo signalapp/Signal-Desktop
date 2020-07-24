467f96acac22a72f696f81afb338ddd294291e5c
Audric Ackermann <audric@loki.network>
2020-07-24 14:48:56 +1000

    substract 1 hour for control of ttl message so PN can ignore them

--- ts/session/constants.ts
+++ ts/session/constants.ts
@@ -3,10 +3,10 @@ import { NumberUtils } from './utils';
 // Default TTL
 export const TTL_DEFAULT = {
   PAIRING_REQUEST: NumberUtils.timeAsMs(2, 'minutes'),
-  DEVICE_UNPAIRING: NumberUtils.timeAsMs(4, 'days'),
-  SESSION_REQUEST: NumberUtils.timeAsMs(4, 'days'),
-  SESSION_ESTABLISHED: NumberUtils.timeAsMs(2, 'days'),
-  END_SESSION_MESSAGE: NumberUtils.timeAsMs(4, 'days'),
+  DEVICE_UNPAIRING: NumberUtils.timeAsMs(4 * 24 - 1, 'hours'),
+  SESSION_REQUEST: NumberUtils.timeAsMs(4 * 24 - 1, 'hours'),
+  SESSION_ESTABLISHED: NumberUtils.timeAsMs(2 * 24 - 1, 'hours'),
+  END_SESSION_MESSAGE: NumberUtils.timeAsMs(4 * 24 - 1, 'hours'),
   TYPING_MESSAGE: NumberUtils.timeAsMs(1, 'minute'),
   ONLINE_BROADCAST: NumberUtils.timeAsMs(1, 'minute'),
   REGULAR_MESSAGE: NumberUtils.timeAsMs(2, 'days'),
