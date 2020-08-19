a2af3a161fa5cb77ea6a3eb9278fd257843abc04
Sidney Keese <me@sidke.com>
2020-08-19 15:53:56 -0700

    Migrate ShortcutGuide to Storybook

---
+++ ts/components/ShortcutGuide.stories.tsx
@@ -0,0 +1,47 @@
+import * as React from 'react';
+import { action } from '@storybook/addon-actions';
+import { storiesOf } from '@storybook/react';
+import { boolean, select } from '@storybook/addon-knobs';
+
+// @ts-ignore
+import { setup as setupI18n } from '../../js/modules/i18n';
+
+// @ts-ignore
+import enMessages from '../../_locales/en/messages.json';
+
+import { Props, ShortcutGuide } from './ShortcutGuide';
+
+const i18n = setupI18n('en', enMessages);
+const story = storiesOf('Components/ShortcutGuide', module);
+
+const createProps = (overrideProps: Partial<Props> = {}): Props => ({
+  i18n,
+  close: action('close'),
+  hasInstalledStickers: boolean(
+    'hasInstalledStickers',
+    overrideProps.hasInstalledStickers || false
+  ),
+  platform: select(
+    'platform',
+    {
+      macOS: 'darwin',
+      other: 'other',
+    },
+    overrideProps.platform || 'other'
+  ),
+});
+
+story.add('Default', () => {
+  const props = createProps({});
+  return <ShortcutGuide {...props} />;
+});
+
+story.add('Mac', () => {
+  const props = createProps({ platform: 'darwin' });
+  return <ShortcutGuide {...props} />;
+});
+
+story.add('Has Stickers', () => {
+  const props = createProps({ hasInstalledStickers: true });
+  return <ShortcutGuide {...props} />;
+});
