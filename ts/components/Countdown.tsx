6aeaabf505826b196d7ab6bf1ecc47590a838f3d
Chris Svenningsen <chris@carbonfive.com>
2020-08-20 15:08:12 -0700

    Migrate countdown to storybook

--- ts/components/Countdown.tsx
+++ ts/components/Countdown.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 // import classNames from 'classnames';
 
-interface Props {
+export interface Props {
   duration: number;
   expiresAt: number;
   onComplete?: () => unknown;
