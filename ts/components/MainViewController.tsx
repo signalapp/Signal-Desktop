291982c2aaa44a89ecd6ef99f4821cdcddcbb7c6
Audric Ackermann <audric@loki.network>
2020-08-25 15:57:37 +1000

    cleanup css and themify missing stuff

--- ts/components/MainViewController.tsx
+++ ts/components/MainViewController.tsx
@@ -24,12 +24,9 @@ export class MessageView extends React.Component {
         <div className="conversation placeholder">
           <div className="conversation-header" />
           <div className="container">
-            <div className="content">
-              <img
-                src="images/session/full-logo.svg"
-                className="session-full-logo"
-                alt="full-brand-logo"
-              />
+            <div className="content session-full-logo">
+              <img src="images/session/brand.svg" className="session-brand-logo" alt="full-brand-logo" />
+              <img src="images/session/session-text.svg" className="session-text-logo" alt="full-brand-logo"/>
             </div>
           </div>
         </div>
