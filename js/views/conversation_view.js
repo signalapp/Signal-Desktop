aaed0db2e5d77ae636785dbca5332952d88636e3
Evan Hahn <69474926+EvanHahn-Signal@users.noreply.github.com>
2020-08-26 17:16:59 -0500

    Verify sticker data in getDataFromLink

--- js/views/conversation_view.js
+++ js/views/conversation_view.js
@@ -2963,11 +2963,13 @@
           pack.status === 'downloaded' ||
           pack.status === 'installed');
 
-      let id;
-      let key;
+      const dataFromLink = window.Signal.Stickers.getDataFromLink(url);
+      if (!dataFromLink) {
+        return null;
+      }
+      const { id, key } = dataFromLink;
 
       try {
-        ({ id, key } = window.Signal.Stickers.getDataFromLink(url));
         const keyBytes = window.Signal.Crypto.bytesFromHexString(key);
         const keyBase64 = window.Signal.Crypto.arrayBufferToBase64(keyBytes);
 
