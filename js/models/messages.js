bd32a55c15c30bc64bf55f7a0effc7533bc50bff
Evan Hahn <69474926+EvanHahn-Signal@users.noreply.github.com>
2020-09-02 13:39:22 -0500

    Fix issue with dates on inbound link previews

--- js/models/messages.js
+++ js/models/messages.js
@@ -778,7 +778,6 @@
         isStickerPack: window.Signal.LinkPreviews.isStickerPack(preview.url),
         domain: window.Signal.LinkPreviews.getDomain(preview.url),
         image: preview.image ? this.getPropsForAttachment(preview.image) : null,
-        date: preview.date ? preview.date.toNumber() : null,
       }));
     },
     getPropsForQuote() {
