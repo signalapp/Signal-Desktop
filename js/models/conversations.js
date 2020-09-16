0fe7e3039876c2922193185c0202252ca75993f3
Josh Perez <60019601+josh-signal@users.noreply.github.com>
2020-09-16 14:04:28 -0400

    Storage Service: More logging

--- js/models/conversations.js
+++ js/models/conversations.js
@@ -3100,6 +3100,9 @@
         return;
       }
 
+      window.log.info(
+        `storageService[captureChange] marking ${this.debugID()} as needing sync`
+      );
       this.set({ needsStorageServiceSync: true });
 
       this.queueJob(() => {
