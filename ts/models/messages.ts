b9ff4f07d3ac210fc9592a6f770221c53551524c
Evan Hahn <69474926+EvanHahn-Signal@users.noreply.github.com>
2020-10-06 11:38:58 -0500

    Remove unnecessary non-null assertion from Message#send

--- ts/models/messages.ts
+++ ts/models/messages.ts
@@ -2148,8 +2148,7 @@ export class MessageModel extends window.Backbone.Model<MessageAttributesType> {
 
         // If we successfully sent to a user, we can remove our unregistered flag.
         result.successfulIdentifiers.forEach((identifier: string) => {
-          // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-          const c = window.ConversationController.get(identifier)!;
+          const c = window.ConversationController.get(identifier);
           if (c && c.isEverUnregistered()) {
             c.setRegistered();
           }
