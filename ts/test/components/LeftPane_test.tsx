de5595514bc79411b14cef7acb935276302b1b55
Evan Hahn <69474926+EvanHahn-Signal@users.noreply.github.com>
2020-10-16 17:05:40 -0500

    Ensure left pane properly handles just pinned/archived

--- ts/test/components/LeftPane_test.tsx
+++ ts/test/components/LeftPane_test.tsx
@@ -34,6 +34,86 @@ describe('LeftPane', () => {
   describe('getRowFromIndex', () => {
     let leftPane: LeftPane;
 
+    describe('with pinned, non-pinned, and archived chats', () => {
+      it('returns headers, conversations, and an archived button', () => {
+        leftPane = new LeftPane({
+          ...defaultProps,
+          pinnedConversations: [
+            {
+              id: 'philly-convo',
+              isPinned: true,
+              isSelected: false,
+              lastUpdated: Date.now(),
+              title: 'Philip Glass',
+              type: 'direct',
+            },
+            {
+              id: 'robbo-convo',
+              isPinned: true,
+              isSelected: false,
+              lastUpdated: Date.now(),
+              title: 'Robert Moog',
+              type: 'direct',
+            },
+          ],
+          conversations: [
+            {
+              id: 'etta-convo',
+              isSelected: false,
+              lastUpdated: Date.now(),
+              title: 'Etta James',
+              type: 'direct',
+            },
+            {
+              id: 'kimbra-convo',
+              isPinned: false,
+              isSelected: false,
+              lastUpdated: Date.now(),
+              title: 'Kimbra',
+              type: 'direct',
+            },
+          ],
+          archivedConversations: [
+            {
+              id: 'jerry-convo',
+              isSelected: false,
+              lastUpdated: Date.now(),
+              title: 'Jerry Jordan',
+              type: 'direct',
+            },
+          ],
+        });
+
+        expect(leftPane.getRowFromIndex(0)).to.eql({
+          headerType: HeaderType.Pinned,
+          type: RowType.Header,
+        });
+        expect(leftPane.getRowFromIndex(1)).to.eql({
+          index: 0,
+          type: RowType.PinnedConversation,
+        });
+        expect(leftPane.getRowFromIndex(2)).to.eql({
+          index: 1,
+          type: RowType.PinnedConversation,
+        });
+        expect(leftPane.getRowFromIndex(3)).to.eql({
+          headerType: HeaderType.Chats,
+          type: RowType.Header,
+        });
+        expect(leftPane.getRowFromIndex(4)).to.eql({
+          index: 0,
+          type: RowType.Conversation,
+        });
+        expect(leftPane.getRowFromIndex(5)).to.eql({
+          index: 1,
+          type: RowType.Conversation,
+        });
+        expect(leftPane.getRowFromIndex(6)).to.eql({
+          type: RowType.ArchiveButton,
+        });
+      });
+    });
+
     describe('given only pinned chats', () => {
       beforeEach(function beforeEach() {
         const props: PropsType = {
@@ -158,6 +238,65 @@ describe('LeftPane', () => {
         });
       });
     });
+
+    describe('given only pinned and archived chats', () => {
+      it('shows the pinned chats with no headers', () => {
+        leftPane = new LeftPane({
+          ...defaultProps,
+          pinnedConversations: [
+            {
+              id: 'philly-convo',
+              isPinned: true,
+              isSelected: false,
+              lastUpdated: Date.now(),
+              title: 'Philip Glass',
+              type: 'direct',
+            },
+          ],
+          archivedConversations: [
+            {
+              id: 'fred-convo',
+              isSelected: false,
+              lastUpdated: Date.now(),
+              title: 'Fred Willard',
+              type: 'direct',
+            },
+          ],
+        });
+
+        expect(leftPane.getRowFromIndex(0)).to.eql({
+          index: 0,
+          type: RowType.PinnedConversation,
+        });
+
+        expect(leftPane.getRowFromIndex(1)).to.eql({
+          type: RowType.ArchiveButton,
+        });
+      });
+    });
+
+    describe("given only archived conversations, which we're not showing", () => {
+      it('returns a single row, the archive button', () => {
+        leftPane = new LeftPane({
+          ...defaultProps,
+          archivedConversations: [
+            {
+              id: 'jerry-convo',
+              isSelected: false,
+              lastUpdated: Date.now(),
+              title: 'Jerry Jordan',
+              type: 'direct',
+            },
+          ],
+          showArchived: false,
+        });
+
+        expect(leftPane.getRowFromIndex(0)).to.eql({
+          type: RowType.ArchiveButton,
+        });
+      });
+    });
+
     describe('given not showing archive with archived conversation', () => {
       beforeEach(function beforeEach() {
         const props: PropsType = {
