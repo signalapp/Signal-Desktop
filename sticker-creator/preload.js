779487df017d8ff2db55cbda96f8fdf22f9415a7
Romain Ricard <romain.ricard@mines-ales.org>
2020-10-31 00:43:34 +0100

    Fix max size for animated stickers
    Animated stickers are allowed to be up to 300kB since
    bdd71e489859eb6c56434ad4b71663b3251b2e7c.
    This commit fixes a bug that was still capping them to 100kB.

--- sticker-creator/preload.js
+++ sticker-creator/preload.js
@@ -15,6 +15,7 @@ const { nativeTheme } = remote.require('electron');
 const STICKER_SIZE = 512;
 const MIN_STICKER_DIMENSION = 10;
 const MAX_STICKER_DIMENSION = STICKER_SIZE;
+const MAX_WEBP_STICKER_BYTE_LENGTH = 100 * 1024;
 const MAX_ANIMATED_STICKER_BYTE_LENGTH = 300 * 1024;
 
 window.ROOT_PATH = window.location.href.startsWith('file') ? '../../' : '/';
@@ -128,6 +129,12 @@ window.processStickerImage = async path => {
       })
       .webp()
       .toBuffer();
+    if (processedBuffer.byteLength > MAX_WEBP_STICKER_BYTE_LENGTH) {
+      throw processStickerError(
+        'Sticker file was too large',
+        'StickerCreator--Toasts--tooLarge'
+      );
+    }
   }
 
   return {
