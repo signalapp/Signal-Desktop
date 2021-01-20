38300881bd283eaefac45b90b68d855737346983
Audric Ackermann <audric@loki.network>
2021-01-20 14:02:10 +1100

    revert to 20 for now

--- preload.js
+++ preload.js
@@ -85,7 +85,7 @@ window.CONSTANTS = new (function() {
   this.DEFAULT_PUBLIC_CHAT_URL = appConfig.get('defaultPublicChatServer');
   this.MAX_LINKED_DEVICES = 1;
   this.MAX_CONNECTION_DURATION = 5000;
-  this.CLOSED_GROUP_SIZE_LIMIT = 100;
+  this.CLOSED_GROUP_SIZE_LIMIT = 20;
   // Number of seconds to turn on notifications after reconnect/start of app
   this.NOTIFICATION_ENABLE_TIMEOUT_SECONDS = 10;
   this.SESSION_ID_LENGTH = 66;
