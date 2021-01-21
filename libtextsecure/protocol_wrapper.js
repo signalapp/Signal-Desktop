ec9bd4a3a9d87a7c16837e57c099dea7a4227ab2
Audric Ackermann <audric@loki.network>
2021-01-22 10:26:22 +1100

    remove unused proto files and ProvisioningCipher.js

--- libtextsecure/protocol_wrapper.js
+++ libtextsecure/protocol_wrapper.js
@@ -1,12 +1,11 @@
 /* global window, textsecure, SignalProtocolStore, libsignal */
 
 // eslint-disable-next-line func-names
-(function() {
+(function () {
   window.textsecure = window.textsecure || {};
   window.textsecure.storage = window.textsecure.storage || {};
   textsecure.storage.protocol = new SignalProtocolStore();
 
-  textsecure.ProvisioningCipher = libsignal.ProvisioningCipher;
   textsecure.startWorker = libsignal.worker.startWorker;
   textsecure.stopWorker = libsignal.worker.stopWorker;
 })();
