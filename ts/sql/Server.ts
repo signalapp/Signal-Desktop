12cfea05a56b52e6ea9992ef8f633eacc6780780
Quentin Hibon <qh.public@yahoo.com>
2021-04-09 20:55:06 +0200

    Use LIKE instead of MATCH to search for messages
    Fixes #5149.

--- ts/sql/Server.ts
+++ ts/sql/Server.ts
@@ -2648,9 +2648,9 @@ async function searchMessages(
         FROM
           messages_fts
         WHERE
-          messages_fts.body MATCH $query;
+          messages_fts.body LIKE $query;
       `
-    ).run({ query });
+    ).run({ query: `%${query}%` });
 
     if (conversationId === undefined) {
       db.prepare<Query>(
@@ -2701,11 +2701,11 @@ async function searchMessages(
         INNER JOIN messages
           ON messages.rowid = tmp_filtered_results.rowid
         WHERE
-          messages_fts.body MATCH $query
+          messages_fts.body LIKE $query
         ORDER BY messages.received_at DESC, messages.sent_at DESC;
         `
       )
-      .all({ query });
+      .all({ query: `%${query}%` });
 
     db.exec(
       `
@@ -2713,6 +2713,14 @@ async function searchMessages(
       DROP TABLE tmp_filtered_results;
       `
     );
+
+    const re = new RegExp(query, 'gi');
+    for (let index = 0; index < result.length; index += 1) {
+      result[index].snippet = result[index].snippet.replace(
+        re,
+        '<<left>>$&<<right>>'
+      );
+    }
 
     return result;
   })();
