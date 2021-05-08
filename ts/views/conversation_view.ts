f42d0cd4857ed0ed6341a56f585f680519537ad1
David Sanders <dsanders11@ucsbalum.com>
2021-05-08 01:08:50 -0700

    Fix typo in method name

--- ts/views/conversation_view.ts
+++ ts/views/conversation_view.ts
@@ -3571,7 +3571,7 @@ Whisper.ConversationView = Whisper.View.extend({
 
     const contact = this.quotedMessage.getContact();
     if (contact) {
-      this.listenTo(contact, 'change', this.renderQuotedMesage);
+      this.listenTo(contact, 'change', this.renderQuotedMessage);
     }
 
     this.quoteView = new Whisper.ReactWrapperView({
