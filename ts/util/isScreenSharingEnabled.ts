5d08fb8f5e46f336d17c001dce14de7eec0d336f
Josh Perez <60019601+josh-signal@users.noreply.github.com>
2021-05-25 18:58:16 -0400

    Enable screensharing flag for beta users

--- ts/util/isScreenSharingEnabled.ts
+++ ts/util/isScreenSharingEnabled.ts
@@ -2,11 +2,13 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 
 import * as RemoteConfig from '../RemoteConfig';
+import { isBeta } from './version';
 
 // We can remove this function once screen sharing has been turned on for everyone
 export function isScreenSharingEnabled(): boolean {
   return (
     RemoteConfig.isEnabled('desktop.worksAtSignal') ||
-    RemoteConfig.isEnabled('desktop.screensharing')
+    RemoteConfig.isEnabled('desktop.screensharing') ||
+    isBeta(window.getVersion())
   );
 }
