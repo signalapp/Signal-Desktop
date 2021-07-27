ed5f9147a7c02699fc96d8093aed548cc833ed2e
automated-signal <37887102+automated-signal@users.noreply.github.com>
2021-07-27 07:25:06 -0700

    More logging in addDeliveryIssue, properly wait in getProfiles
    Co-authored-by: Scott Nonnenberg <scott@signal.org>

--- ts/background.ts
+++ ts/background.ts
@@ -928,7 +928,7 @@ export async function startApp(): Promise<void> {
           `retryPlaceholders/interval: Found ${expired.length} expired items`
         );
         expired.forEach(item => {
-          const { conversationId, senderUuid } = item;
+          const { conversationId, senderUuid, sentAt } = item;
           const conversation = window.ConversationController.get(
             conversationId
           );
@@ -940,6 +940,7 @@ export async function startApp(): Promise<void> {
                 receivedAt,
                 receivedAtCounter,
                 senderUuid,
+                sentAt,
               })
             );
           }
