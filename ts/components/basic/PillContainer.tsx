1560c83b2b4544cbd71a3c778a4ffc7b8b59c970
Warrick Corfe-Tan <warrickct@gmail.com>
2021-08-13 15:36:33 +1000

    Adding hover over tooltip for open group pills.

--- ts/components/basic/PillContainer.tsx
+++ ts/components/basic/PillContainer.tsx
@@ -6,6 +6,8 @@ type PillContainerProps = {
   margin?: string;
   padding?: string;
   onClick?: () => void;
+  onMouseEnter?: () => void;
+  onMouseLeave?: () => void;
 };
 
 const StyledPillContainer = styled.div<PillContainerProps>`
@@ -13,6 +15,50 @@ const StyledPillContainer = styled.div<PillContainerProps>`
   background: none;
   flex-direction: 'row';
   flex-grow: 1;
+  flex: 1 1 40%;
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  align-items: center;
+  padding: ${props => props.padding || ''};
+  margin: ${props => props.margin || ''};
+  border-radius: 300px;
+  cursor: pointer;
+  border: 1px solid ${props => props.theme.colors.pillDividerColor};
+  transition: ${props => props.theme.common.animations.defaultDuration};
+  &:hover {
+    background: ${props => props.theme.colors.clickableHovered};
+  }
+`;
+
+const StyledPillContainerHoverable = styled.div<PillContainerProps>`
+  background: none;
+  
+  position: relative;
+  flex-direction: 'row';
+  
+  width: 50%;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  align-items: center;
+  padding: ${props => props.padding || ''};
+  margin: ${props => props.margin || ''};
+`;
+
+
+const StyledPillInner = styled.div<PillContainerProps>`
+  background: green;
+  background: none;
+
+  display: flex;
+  flex-direction: 'row';
+  flex-grow: 1;
+  flex: 1 1 40%;
+
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+
   align-items: center;
   padding: ${props => props.padding || ''};
   margin: ${props => props.margin || ''};
@@ -27,4 +73,12 @@ const StyledPillContainer = styled.div<PillContainerProps>`
 
 export const PillContainer = (props: PillContainerProps) => {
   return <StyledPillContainer {...props}>{props.children}</StyledPillContainer>;
+};
+
+export const PillTooltipWrapper = (props: PillContainerProps) => {
+  return <StyledPillContainerHoverable {...props}>{props.children}</StyledPillContainerHoverable>;
+};
+
+export const PillContainerHoverable = (props: PillContainerProps) => {
+  return <StyledPillInner {...props}>{props.children}</StyledPillInner>;
 };
