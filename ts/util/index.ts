27bdf6e1a3f85b73031a3f9923c840953a7dae29
Fedor Indutny <79877362+indutny-signal@users.noreply.github.com>
2021-09-08 16:43:13 -0700

    Flush message counter on shutdown

--- ts/util/index.ts
+++ ts/util/index.ts
@@ -14,7 +14,10 @@ import { getStringForProfileChange } from './getStringForProfileChange';
 import { getTextWithMentions } from './getTextWithMentions';
 import { getUserAgent } from './getUserAgent';
 import { hasExpired } from './hasExpired';
-import { incrementMessageCounter } from './incrementMessageCounter';
+import {
+  incrementMessageCounter,
+  flushMessageCounter,
+} from './incrementMessageCounter';
 import { isFileDangerous } from './isFileDangerous';
 import { makeLookup } from './makeLookup';
 import {
@@ -50,6 +53,7 @@ export {
   createWaitBatcher,
   deleteForEveryone,
   downloadAttachment,
+  flushMessageCounter,
   fromWebSafeBase64,
   generateSecurityNumber,
   getStringForProfileChange,
