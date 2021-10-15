0bfa41c7b8c3a343474fd51f507590fb4083312c
Audric Ackermann <audric@loki.network>
2021-10-15 16:13:16 +1100

    switch to our turn servers

--- ts/session/utils/CallManager.ts
+++ ts/session/utils/CallManager.ts
@@ -64,11 +64,11 @@ const configuration = {
     offerToReceiveVideo: true,
   },
   iceServers: [
-    { urls: 'stun:stun.l.google.com:19302' },
-    { urls: 'stun:stun1.l.google.com:19302' },
-    { urls: 'stun:stun2.l.google.com:19302' },
-    { urls: 'stun:stun3.l.google.com:19302' },
-    { urls: 'stun:stun4.l.google.com:19302' },
+    {
+      urls: 'turn:freyr.getsession.org',
+      username: 'webrtc',
+      credential: 'webrtc',
+    },
   ],
 };
 
