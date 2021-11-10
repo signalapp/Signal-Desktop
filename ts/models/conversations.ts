a76059eac5f902ee5daba3d59d1238ac28a9c25e
automated-signal <37887102+automated-signal@users.noreply.github.com>
2021-11-09 16:57:57 -0800

    Less strict uuid fetch in areWeAdmin
    Co-authored-by: Fedor Indutny <79877362+indutny-signal@users.noreply.github.com>

--- ts/models/conversations.ts
+++ ts/models/conversations.ts
@@ -4629,7 +4629,7 @@ export class ConversationModel extends window.Backbone
 
     const memberEnum = Proto.Member.Role;
     const members = this.get('membersV2') || [];
-    const ourUuid = window.textsecure.storage.user.getCheckedUuid().toString();
+    const ourUuid = window.textsecure.storage.user.getUuid()?.toString();
     const me = members.find(item => item.uuid === ourUuid);
     if (!me) {
       return false;
