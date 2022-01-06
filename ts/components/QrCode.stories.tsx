88b1b77cea58c1217a0b77581cb0b06527d19dea
Evan Hahn <69474926+EvanHahn-Signal@users.noreply.github.com>
2022-01-06 09:10:33 -0600

    Switch to qrcode-generator library
    (cherry picked from commit c8d32f03273a3a550aa44e26807dfcf5f6afe610)

---
+++ ts/components/QrCode.stories.tsx
@@ -0,0 +1,17 @@
+// Copyright 2022 Signal Messenger, LLC
+// SPDX-License-Identifier: AGPL-3.0-only
+
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+
+import { QrCode } from './QrCode';
+
+const story = storiesOf('Components/QrCode', module);
+
+story.add('Default', () => (
+  <QrCode
+    aria-label="Scan this little code!"
+    data="sgnl://linkdevice?uuid=gCkj0T2xiSUaPRhMYiF24w&pub_key=7RshtQrb3UTMowITe79uW9dgw_CLTGWenj0OT80i0HpH"
+    size={256}
+  />
+));
