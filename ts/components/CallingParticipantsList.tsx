8f85b32d9b096b46ab19cf50919f5135fe07cf74
Scott Nonnenberg <scott@signal.org>
2022-01-17 19:26:05 -0800

    Calling Lobby: Ensure participant list can be dismissed

--- ts/components/CallingParticipantsList.tsx
+++ ts/components/CallingParticipantsList.tsx
@@ -5,6 +5,8 @@
 
 import React from 'react';
 import { createPortal } from 'react-dom';
+import FocusTrap from 'focus-trap-react';
+
 import { Avatar } from './Avatar';
 import { ContactName } from './conversation/ContactName';
 import { InContactsIcon } from './InContactsIcon';
@@ -60,94 +62,97 @@ export const CallingParticipantsList = React.memo(
     }
 
     return createPortal(
-      <div
-        className="module-calling-participants-list__overlay"
-        onClick={handleCancel}
-        role="presentation"
-      >
-        <div className="module-calling-participants-list">
-          <div className="module-calling-participants-list__header">
-            <div className="module-calling-participants-list__title">
-              {!participants.length && i18n('calling__in-this-call--zero')}
-              {participants.length === 1 && i18n('calling__in-this-call--one')}
-              {participants.length > 1 &&
-                i18n('calling__in-this-call--many', [
-                  String(participants.length),
-                ])}
+      <FocusTrap>
+        <div
+          className="module-calling-participants-list__overlay"
+          onClick={handleCancel}
+          role="presentation"
+        >
+          <div className="module-calling-participants-list">
+            <div className="module-calling-participants-list__header">
+              <div className="module-calling-participants-list__title">
+                {!participants.length && i18n('calling__in-this-call--zero')}
+                {participants.length === 1 &&
+                  i18n('calling__in-this-call--one')}
+                {participants.length > 1 &&
+                  i18n('calling__in-this-call--many', [
+                    String(participants.length),
+                  ])}
+              </div>
+              <button
+                type="button"
+                className="module-calling-participants-list__close"
+                onClick={onClose}
+                tabIndex={0}
+                aria-label={i18n('close')}
+              />
             </div>
-            <button
-              type="button"
-              className="module-calling-participants-list__close"
-              onClick={onClose}
-              tabIndex={0}
-              aria-label={i18n('close')}
-            />
+            <ul className="module-calling-participants-list__list">
+              {sortedParticipants.map(
+                (participant: ParticipantType, index: number) => (
+                  <li
+                    className="module-calling-participants-list__contact"
+                    // It's tempting to use `participant.uuid` as the `key` here, but that
+                    //   can result in duplicate keys for participants who have joined on
+                    //   multiple devices.
+                    key={index}
+                  >
+                    <div>
+                      <Avatar
+                        acceptedMessageRequest={
+                          participant.acceptedMessageRequest
+                        }
+                        avatarPath={participant.avatarPath}
+                        badge={undefined}
+                        color={participant.color}
+                        conversationType="direct"
+                        i18n={i18n}
+                        isMe={participant.isMe}
+                        profileName={participant.profileName}
+                        title={participant.title}
+                        sharedGroupNames={participant.sharedGroupNames}
+                        size={32}
+                      />
+                      {participant.uuid === ourUuid ? (
+                        <span className="module-calling-participants-list__name">
+                          {i18n('you')}
+                        </span>
+                      ) : (
+                        <>
+                          <ContactName
+                            module="module-calling-participants-list__name"
+                            title={participant.title}
+                          />
+                          {isInSystemContacts(participant) ? (
+                            <span>
+                              {' '}
+                              <InContactsIcon
+                                className="module-calling-participants-list__contact-icon"
+                                i18n={i18n}
+                              />
+                            </span>
+                          ) : null}
+                        </>
+                      )}
+                    </div>
+                    <div>
+                      {participant.hasRemoteAudio === false ? (
+                        <span className="module-calling-participants-list__muted--audio" />
+                      ) : null}
+                      {participant.hasRemoteVideo === false ? (
+                        <span className="module-calling-participants-list__muted--video" />
+                      ) : null}
+                      {participant.presenting ? (
+                        <span className="module-calling-participants-list__presenting" />
+                      ) : null}
+                    </div>
+                  </li>
+                )
+              )}
+            </ul>
           </div>
-          <ul className="module-calling-participants-list__list">
-            {sortedParticipants.map(
-              (participant: ParticipantType, index: number) => (
-                <li
-                  className="module-calling-participants-list__contact"
-                  // It's tempting to use `participant.uuid` as the `key` here, but that
-                  //   can result in duplicate keys for participants who have joined on
-                  //   multiple devices.
-                  key={index}
-                >
-                  <div>
-                    <Avatar
-                      acceptedMessageRequest={
-                        participant.acceptedMessageRequest
-                      }
-                      avatarPath={participant.avatarPath}
-                      badge={undefined}
-                      color={participant.color}
-                      conversationType="direct"
-                      i18n={i18n}
-                      isMe={participant.isMe}
-                      profileName={participant.profileName}
-                      title={participant.title}
-                      sharedGroupNames={participant.sharedGroupNames}
-                      size={32}
-                    />
-                    {participant.uuid === ourUuid ? (
-                      <span className="module-calling-participants-list__name">
-                        {i18n('you')}
-                      </span>
-                    ) : (
-                      <>
-                        <ContactName
-                          module="module-calling-participants-list__name"
-                          title={participant.title}
-                        />
-                        {isInSystemContacts(participant) ? (
-                          <span>
-                            {' '}
-                            <InContactsIcon
-                              className="module-calling-participants-list__contact-icon"
-                              i18n={i18n}
-                            />
-                          </span>
-                        ) : null}
-                      </>
-                    )}
-                  </div>
-                  <div>
-                    {participant.hasRemoteAudio === false ? (
-                      <span className="module-calling-participants-list__muted--audio" />
-                    ) : null}
-                    {participant.hasRemoteVideo === false ? (
-                      <span className="module-calling-participants-list__muted--video" />
-                    ) : null}
-                    {participant.presenting ? (
-                      <span className="module-calling-participants-list__presenting" />
-                    ) : null}
-                  </div>
-                </li>
-              )
-            )}
-          </ul>
         </div>
-      </div>,
+      </FocusTrap>,
       root
     );
   }
