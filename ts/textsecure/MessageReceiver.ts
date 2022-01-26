e2f7c2dd45b5202211a670c8af394b19525174c8
Fedor Indutny <79877362+indutny-signal@users.noreply.github.com>
2022-01-25 18:09:40 -0800

    MessageReceiver.maybeUpdateTimestamp: Annotate group identifier for log
    Co-authored-by: Scott Nonnenberg <scott@signal.org>

--- ts/textsecure/MessageReceiver.ts
+++ ts/textsecure/MessageReceiver.ts
@@ -1847,8 +1847,11 @@ export default class MessageReceiver
 
     try {
       if (!conversation) {
+        const idForLogging = envelope.groupId
+          ? `groupv2(${envelope.groupId})`
+          : envelope.sourceUuid;
         log.info(
-          `maybeUpdateTimestamp/${timestamp}: No conversation found for identifier ${identifier}`
+          `maybeUpdateTimestamp/${timestamp}: No conversation found for identifier ${idForLogging}`
         );
         return envelope;
       }
