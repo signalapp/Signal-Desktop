6578679166a1bfae02466b40277de3235c6564d9
Fedor Indutny <79877362+indutny-signal@users.noreply.github.com>
2022-01-27 13:28:04 -0800

    Improve performance of isEmojiOnlyText

--- ts/util/isEmojiOnlyText.tsx
+++ ts/util/isEmojiOnlyText.tsx
@@ -1,9 +1,22 @@
-// Copyright 2021 Signal Messenger, LLC
+// Copyright 2021-2022 Signal Messenger, LLC
 // SPDX-License-Identifier: AGPL-3.0-only
 
-import * as grapheme from './grapheme';
-import { getEmojiCount } from '../components/emoji/lib';
+import emojiRegex from 'emoji-regex';
 
 export function isEmojiOnlyText(text: string): boolean {
-  return grapheme.count(text) === getEmojiCount(text);
+  if (text.length === 0) {
+    return false;
+  }
+
+  const regex = emojiRegex();
+  let len = 0;
+  for (const match of text.matchAll(regex)) {
+    // Skipped some non-emoji text, return early
+    if (match.index !== len) {
+      return false;
+    }
+
+    len += match[0].length;
+  }
+  return len === text.length;
 }
