7c53ac21e8533c6807fcac8c88e97fd223d189c4
automated-signal <37887102+automated-signal@users.noreply.github.com>
2022-03-16 10:32:23 -0700

    Fix third item in Whats's New dialog
    Co-authored-by: Scott Nonnenberg <scott@signal.org>

--- ts/components/WhatsNewModal.tsx
+++ ts/components/WhatsNewModal.tsx
@@ -32,7 +32,7 @@ const releaseNotes: ReleaseNotesType = {
   features: [
     { key: 'WhatsNew__v5.36--1', components: undefined },
     { key: 'WhatsNew__v5.36--2', components: undefined },
-    { key: 'WhatsNew__v5.36--3  ', components: undefined },
+    { key: 'WhatsNew__v5.36--3', components: undefined },
   ],
 };
 
