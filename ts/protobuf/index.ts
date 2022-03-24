07a362e698cd42da6510b4e4dbfb4b08c5a13ae1
automated-signal <37887102+automated-signal@users.noreply.github.com>
2022-03-24 11:24:13 -0700

    Configure `Long` before requiring compiled protobuf
    Co-authored-by: Fedor Indutny <79877362+indutny-signal@users.noreply.github.com>

--- ts/protobuf/index.ts
+++ ts/protobuf/index.ts
@@ -1,12 +1,8 @@
 // Copyright 2018-2020 Signal Messenger, LLC
 // SPDX-License-Identifier: AGPL-3.0-only
 
-import * as protobuf from 'protobufjs/minimal';
-import Long from 'long';
+import './wrap';
 
 import { signalservice as SignalService } from './compiled';
-
-protobuf.util.Long = Long;
-protobuf.configure();
 
 export { SignalService };
