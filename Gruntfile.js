5cf76c5f084083273398c76494dad8bbab718b55
William Grant <willmgrant@gmail.com>
2022-05-23 16:36:03 +1000

    feat: build sass files using parcel instead of grunt
    added sass watch command for auto building sass on file changes, cleaned up IE7 fallback css

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,26 +1,8 @@
-const importOnce = require('node-sass-import-once');
-const sass = require('node-sass');
-
 /* eslint-disable more/no-then, no-console  */
 
 module.exports = grunt => {
-  grunt.loadNpmTasks('grunt-sass');
-
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    sass: {
-      options: {
-        implementation: sass,
-        sourceMap: true,
-        importer: importOnce,
-      },
-
-      dist: {
-        files: {
-          'stylesheets/manifest.css': 'stylesheets/manifest.scss',
-        },
-      },
-    },
     exec: {
       transpile: {
         cmd: 'yarn transpile',
@@ -63,11 +45,5 @@ module.exports = grunt => {
   });
 
   grunt.registerTask('date', ['gitinfo']);
-  grunt.registerTask('default', [
-    'exec:build-protobuf',
-    'sass',
-    'exec:transpile',
-    'date',
-    'getCommitHash',
-  ]);
+  grunt.registerTask('default', ['exec:build-protobuf', 'exec:transpile', 'date', 'getCommitHash']);
 };
