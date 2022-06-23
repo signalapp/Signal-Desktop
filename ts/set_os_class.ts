6ad091bfe3a4208e578a90ee486e93c36b7e5d15
Fedor Indutny <79877362+indutny-signal@users.noreply.github.com>
2022-06-23 08:40:36 -0700

    Shift titlebar on Windows 11

--- ts/set_os_class.ts
+++ ts/set_os_class.ts
@@ -5,9 +5,6 @@
   let className: string;
   if (window.SignalContext.OS.isWindows()) {
     className = 'os-windows';
-    if (window.SignalContext.OS.isWindows11()) {
-      document.body.classList.add('os-windows-11');
-    }
   } else if (window.SignalContext.OS.isMacOS()) {
     className = 'os-macos';
   } else if (window.SignalContext.OS.isLinux()) {
