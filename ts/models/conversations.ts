908a7cc31f764d21c5c88263b9dbb422c72a4f88
automated-signal <37887102+automated-signal@users.noreply.github.com>
2022-07-11 10:41:29 -0700

    enqueueMessageForSend: Capture storage service change on unarchive
    Co-authored-by: Scott Nonnenberg <scott@signal.org>

--- ts/models/conversations.ts
+++ ts/models/conversations.ts
@@ -3997,9 +3997,11 @@ export class ConversationModel extends window.Backbone
         const { clearUnreadMetrics } = window.reduxActions.conversations;
         clearUnreadMetrics(this.id);
 
-        const enableProfileSharing = Boolean(
+        const enabledProfileSharing = Boolean(
           mandatoryProfileSharingEnabled && !this.get('profileSharing')
         );
+        const unarchivedConversation = Boolean(this.get('isArchived'));
+
         this.doAddSingleMessage(model, { isJustSent: true });
 
         const draftProperties = dontClearDraft
@@ -4013,15 +4015,18 @@ export class ConversationModel extends window.Backbone
 
         this.set({
           ...draftProperties,
-          ...(enableProfileSharing ? { profileSharing: true } : {}),
+          ...(enabledProfileSharing ? { profileSharing: true } : {}),
           ...this.incrementSentMessageCount({ dry: true }),
           active_at: now,
           timestamp: now,
-          isArchived: false,
+          ...(unarchivedConversation ? { isArchived: false } : {}),
         });
 
-        if (enableProfileSharing) {
-          this.captureChange('mandatoryProfileSharing');
+        if (enabledProfileSharing) {
+          this.captureChange('enqueueMessageForSend/mandatoryProfileSharing');
+        }
+        if (unarchivedConversation) {
+          this.captureChange('enqueueMessageForSend/unarchive');
         }
 
         extraReduxActions?.();
