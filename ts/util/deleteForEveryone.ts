f735a9cfa7981157d8330bd48f8e542f7dbc956a
Evan Hahn <69474926+EvanHahn-Signal@users.noreply.github.com>
2022-08-05 11:05:30 -0500

    Always respect DOE from your own devices

--- ts/util/deleteForEveryone.ts
+++ ts/util/deleteForEveryone.ts
@@ -1,25 +1,24 @@
-// Copyright 2020 Signal Messenger, LLC
+// Copyright 2020-2022 Signal Messenger, LLC
 // SPDX-License-Identifier: AGPL-3.0-only
 
 import type { DeleteModel } from '../messageModifiers/Deletes';
 import type { MessageModel } from '../models/messages';
 import * as log from '../logging/log';
 import { DAY } from './durations';
+import { getContactId } from '../messages/helpers';
 
 export async function deleteForEveryone(
   message: MessageModel,
   doe: DeleteModel,
   shouldPersist = true
 ): Promise<void> {
-  const messageTimestamp =
-    message.get('serverTimestamp') || message.get('sent_at') || 0;
+  if (isDeletionByMe(message, doe)) {
+    await message.handleDeleteForEveryone(doe, shouldPersist);
+    return;
+  }
 
-  // Make sure the server timestamps for the DOE and the matching message
-  // are less than one day apart
-  const delta = Math.abs(doe.get('serverTimestamp') - messageTimestamp);
-
-  if (delta > DAY) {
-    log.info('Received late DOE. Dropping.', {
+  if (isDeletionTooOld(message, doe)) {
+    log.warn('Received late DOE. Dropping.', {
       fromId: doe.get('fromId'),
       targetSentTimestamp: doe.get('targetSentTimestamp'),
       messageServerTimestamp: message.get('serverTimestamp'),
@@ -30,4 +29,26 @@ export async function deleteForEveryone(
   }
 
   await message.handleDeleteForEveryone(doe, shouldPersist);
+}
+
+function isDeletionByMe(
+  message: Readonly<MessageModel>,
+  doe: Readonly<DeleteModel>
+): boolean {
+  const ourConversationId =
+    window.ConversationController.getOurConversationIdOrThrow();
+  return (
+    getContactId(message.attributes) === ourConversationId &&
+    doe.get('fromId') === ourConversationId
+  );
+}
+
+function isDeletionTooOld(
+  message: Readonly<MessageModel>,
+  doe: Readonly<DeleteModel>
+): boolean {
+  const messageTimestamp =
+    message.get('serverTimestamp') || message.get('sent_at') || 0;
+  const delta = Math.abs(doe.get('serverTimestamp') - messageTimestamp);
+  return delta > DAY;
 }
