8d7cbeef6465b4979724e5c56ac324e9dbfa8ff6
Alvaro Carrasco <alvaro@signal.org>
2022-09-08 16:26:05 -0600

    v5.59.0-beta.1

--- ts/components/WhatsNewModal.tsx
+++ ts/components/WhatsNewModal.tsx
@@ -31,7 +31,7 @@ const releaseNotes: ReleaseNotesType = {
   version: window.getVersion?.(),
   features: [
     {
-      key: 'WhatsNew__bugfixes--4',
+      key: 'WhatsNew__bugfixes--3',
       components: undefined,
     },
   ],
