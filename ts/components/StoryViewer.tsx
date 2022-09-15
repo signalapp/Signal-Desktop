51f7034cab4ffb91c6038354e675ce45ae08fb8b
Jamie Kyle <113370520+jamiebuilds-signal@users.noreply.github.com>
2022-09-14 17:33:18 -0700

    Fix storyviewer title for groups

--- ts/components/StoryViewer.tsx
+++ ts/components/StoryViewer.tsx
@@ -584,12 +584,12 @@ export const StoryViewer = ({
                   />
                 )}
                 <div className="StoryViewer__meta--title">
-                  {group
-                    ? i18n('Stories__from-to-group', {
-                        name: title,
-                        group: group.title,
-                      })
-                    : title}
+                  {(group &&
+                    i18n('Stories__from-to-group', {
+                      name: isMe ? i18n('you') : title,
+                      group: group.title,
+                    })) ||
+                    (isMe ? i18n('you') : title)}
                 </div>
                 <MessageTimestamp
                   i18n={i18n}
