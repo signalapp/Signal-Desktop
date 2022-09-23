649faf00928613ac34965eb1ade6dff20cd7a326
Josh Perez <60019601+josh-signal@users.noreply.github.com>
2022-09-23 14:46:25 -0400

    v5.61 release notes

--- ts/components/WhatsNewModal.tsx
+++ ts/components/WhatsNewModal.tsx
@@ -31,7 +31,11 @@ const releaseNotes: ReleaseNotesType = {
   version: window.getVersion?.(),
   features: [
     {
-      key: 'WhatsNew__bugfixes--3',
+      key: 'WhatsNew__v5.61--0',
+      components: undefined,
+    },
+    {
+      key: 'WhatsNew__v5.61--1',
       components: undefined,
     },
   ],
