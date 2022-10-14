be21f298758f469e6da770a5cc6ec20ee10fa4e6
William Grant <willmgrant@gmail.com>
2022-10-14 15:16:48 +1100

    fix: pr review - remove unneeded export

--- ts/themes/SessionTheme.tsx
+++ ts/themes/SessionTheme.tsx
@@ -5,7 +5,7 @@ import { classicDark } from './';
 import { declareCSSVariables, THEME_GLOBALS } from './globals';
 
 // Defaults to Classic Dark theme
-export const SessionGlobalStyles = createGlobalStyle`
+const SessionGlobalStyles = createGlobalStyle`
   html {
     ${declareCSSVariables(THEME_GLOBALS)}
     ${declareCSSVariables(classicDark)}
