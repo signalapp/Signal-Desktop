e5f0cd49787c4e701241de91b8b8bab42aed8885
Michael Kuckuk <8076094+LBBO@users.noreply.github.com>
2022-11-07 18:22:18 +0100

    Add categories to slider

--- ts/components/gifs/GifPicker.tsx
+++ ts/components/gifs/GifPicker.tsx
@@ -3,6 +3,7 @@ import FocusTrap from 'focus-trap-react';
 import type { GifResult } from '@giphy/js-fetch-api';
 import { GiphyFetch } from '@giphy/js-fetch-api';
 import { Grid } from '@giphy/react-components';
+import classNames from 'classnames';
 import type { LocalizerType } from '../../types/Util';
 
 type GiphyGif = GifResult['data'];
@@ -11,6 +12,18 @@ type Gif = {
   url: string;
   alt: string;
 };
+
+const CATEGORIES = [
+  'trending',
+  'celebrate',
+  'love',
+  'thumbsup',
+  'surprised',
+  'excited',
+  'sad',
+  'angry',
+] as const;
+type CategoryName = 'recent' | 'search' | typeof CATEGORIES[number];
 
 export type Props = Readonly<{
   i18n: LocalizerType;
@@ -28,29 +41,102 @@ const API_KEY = 'ZsUpUm2L6cVbvei347EQNp7HrROjbOdc';
 
 const giphy = new GiphyFetch(API_KEY);
 
+function isMutableRef<T>(val: unknown): val is React.MutableRefObject<T> {
+  return Boolean(
+    val && Object.getOwnPropertyDescriptor(val, 'current') !== undefined
+  );
+}
+
 export const GifPicker = React.memo(
-  React.forwardRef<HTMLDivElement, Props>(({ style, onPickGif }, ref) => {
-    const fetchGifs = React.useCallback(
-      (offset: number) => giphy.trending({ offset, limit: 15, type: 'gifs' }),
-      []
-    );
-    return (
-      <FocusTrap>
-        <div className="module-gif-picker" ref={ref} style={style}>
-          <div className="module-gif-picker__header" />
-          <input />
-          <div className="module-gif-picker__body">
-            <Grid
-              columns={3}
-              fetchGifs={fetchGifs}
-              width={100}
-              noLink
-              onGifClick={onPickGif}
-              hideAttribution
-            />
+  React.forwardRef<HTMLDivElement, Props>(
+    ({ style, onPickGif, recentGifs, i18n }, ref) => {
+      const popupWidth = React.useMemo((): number => {
+        const defaultWidth = 332;
+        if (isMutableRef(ref)) {
+          return ref.current?.offsetWidth ?? defaultWidth;
+        }
+        return defaultWidth;
+      }, [ref]);
+
+      const hasRecentGifs = recentGifs.length > 0;
+      const [currentTabName, setCurrentTabName] = React.useState<CategoryName>(
+        hasRecentGifs ? 'recent' : 'search'
+      );
+      const onCategoryClickFactory = (categoryName: CategoryName) => () => {
+        setCurrentTabName(categoryName);
+      };
+
+      const fetchGifs = React.useCallback((offset: number) => {
+        return giphy.trending({ offset, limit: 15, type: 'gifs' });
+      }, []);
+
+      return (
+        <FocusTrap>
+          <div className="module-gif-picker" ref={ref} style={style}>
+            <div className="module-gif-picker__header">
+              <div className="module-gif-picker__header__categories">
+                <div className="module-gif-picker__header__categories__slider">
+                  {hasRecentGifs || true ? (
+                    <button
+                      type="button"
+                      onClick={onCategoryClickFactory('recent')}
+                      className={classNames(
+                        'module-gif-picker__header__categories__slider__button',
+                        'module-gif-picker__header__categories__slider__button--recents',
+                        {
+                          'module-gif-picker__header__categories__slider__button--selected':
+                            currentTabName === 'recent',
+                        }
+                      )}
+                      aria-label={i18n('gifs--GifPicker--Recents')}
+                    />
+                  ) : null}
+                  <button
+                    type="button"
+                    onClick={onCategoryClickFactory('search')}
+                    className={classNames(
+                      'module-gif-picker__header__categories__slider__button',
+                      'module-gif-picker__header__categories__slider__button--search',
+                      {
+                        'module-gif-picker__header__categories__slider__button--selected':
+                          currentTabName === 'search',
+                      }
+                    )}
+                    aria-label={i18n('gifs--GifPicker--Search')}
+                  />
+                  {CATEGORIES.map(category => (
+                    <button
+                      type="button"
+                      key={category}
+                      onClick={onCategoryClickFactory(category)}
+                      className={classNames(
+                        'module-gif-picker__header__categories__slider__button',
+                        `module-gif-picker__header__categories__slider__button--${category}`,
+                        {
+                          'module-gif-picker__header__categories__slider__button--selected':
+                            currentTabName === category,
+                        }
+                      )}
+                      aria-label={i18n(`gifs--GifPicker--${category}`)}
+                    />
+                  ))}
+                </div>
+              </div>
+            </div>
+            <input />
+            <div className="module-gif-picker__body">
+              <Grid
+                columns={3}
+                fetchGifs={fetchGifs}
+                width={popupWidth}
+                noLink
+                onGifClick={onPickGif}
+                hideAttribution
+              />
+            </div>
           </div>
-        </div>
-      </FocusTrap>
-    );
-  })
+        </FocusTrap>
+      );
+    }
+  )
 );
