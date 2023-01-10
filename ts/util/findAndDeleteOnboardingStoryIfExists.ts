be99c7dfdf14c34e1c289c2b3cbf3f2fbfa45f8a
Fedor Indutny <79877362+indutny-signal@users.noreply.github.com>
2023-01-09 17:44:20 -0800

    Repair on-boarding story if needed

--- ts/util/findAndDeleteOnboardingStoryIfExists.ts
+++ ts/util/findAndDeleteOnboardingStoryIfExists.ts
@@ -4,6 +4,7 @@
 import * as log from '../logging/log';
 import { getMessageById } from '../messages/getMessageById';
 import { calculateExpirationTimestamp } from './expirationTimer';
+import { DAY } from './durations';
 
 export async function findAndDeleteOnboardingStoryIfExists(): Promise<void> {
   const existingOnboardingStoryMessageIds = window.storage.get(
@@ -24,7 +25,11 @@ export async function findAndDeleteOnboardingStoryIfExists(): Promise<void> {
 
       const expires = calculateExpirationTimestamp(message.attributes) ?? 0;
 
-      return expires < Date.now();
+      const now = Date.now();
+      const isExpired = expires < now;
+      const needsRepair = expires > now + 2 * DAY;
+
+      return isExpired || needsRepair;
     }
 
     return true;
