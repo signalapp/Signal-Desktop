42b4406ad5d30e0985622260491b98b7bcc6d66f
Alvaro <110414366+alvaro-signal@users.noreply.github.com>
2023-01-27 10:51:53 -0700

    Formatting voice recorder error

--- ts/state/ducks/audioRecorder.ts
+++ ts/state/ducks/audioRecorder.ts
@@ -14,6 +14,7 @@ import { stringToMIMEType } from '../../types/MIME';
 import type { BoundActionCreatorsMapObject } from '../../hooks/useBoundActions';
 import { useBoundActions } from '../../hooks/useBoundActions';
 import { getComposerStateForConversation } from './composer';
+import * as Errors from '../../types/errors';
 import {
   ErrorDialogAudioRecorderType,
   RecordingState,
@@ -116,7 +117,7 @@ function startRecording(
         });
       }
     } catch (err) {
-      log.error('AudioRecorder/ERROR_RECORDING', err);
+      log.error('AudioRecorder/ERROR_RECORDING', Errors.toLogFormat(err));
       dispatch({
         type: ERROR_RECORDING,
         payload: ErrorDialogAudioRecorderType.ErrorRecording,
