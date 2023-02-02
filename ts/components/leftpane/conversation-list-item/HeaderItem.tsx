5b59b3eef21482d82d456d8590791753c19c6efa
William Grant <willmgrant@gmail.com>
2023-02-02 11:10:47 +1100

    fix: extract max unread count to a constant

--- ts/components/leftpane/conversation-list-item/HeaderItem.tsx
+++ ts/components/leftpane/conversation-list-item/HeaderItem.tsx
@@ -5,6 +5,7 @@ import styled from 'styled-components';
 import { Data } from '../../../data/data';
 import { useConversationPropsById, useIsPinned } from '../../../hooks/useParamSelector';
 import { getUsBlindedInThatServer } from '../../../session/apis/open_group_api/sogsv3/knownBlindedkeys';
+import { CONVERSATION } from '../../../session/constants';
 import { UserUtils } from '../../../session/utils';
 import {
   openConversationToSpecificMessage,
@@ -172,7 +173,7 @@ export const ConversationListItemHeaderItem = () => {
     ) : null;
     unreadCountDiv = (
       <p className="module-conversation-list-item__unread-count">
-        {unreadCount > 9999 ? '9999+' : unreadCount}
+        {unreadCount > CONVERSATION.MAX_UNREAD_COUNT ? `${CONVERSATION.MAX_UNREAD_COUNT}+` : unreadCount}
       </p>
     );
   }
