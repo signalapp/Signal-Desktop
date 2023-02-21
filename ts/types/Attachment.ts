5daa820e4e9436fdb3a0da1bb33b97ef15ba4032
Alvaro <110414366+alvaro-signal@users.noreply.github.com>
2023-02-21 08:24:19 -0700

    Fixed voice notes to download with mp3 extension

--- ts/types/Attachment.ts
+++ ts/types/Attachment.ts
@@ -982,6 +982,8 @@ export const getFileExtension = (
   switch (attachment.contentType) {
     case 'video/quicktime':
       return 'mov';
+    case 'audio/mpeg':
+      return 'mp3';
     default:
       return attachment.contentType.split('/')[1];
   }
