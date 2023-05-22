99011b0a56cf793e01b2dfa0da161994ff931fe1
Fedor Indutny <79877362+indutny-signal@users.noreply.github.com>
2023-05-22 22:24:33 +0200

    Fix position of lightbox arrows for a video

--- ts/components/Lightbox.tsx
+++ ts/components/Lightbox.tsx
@@ -484,7 +484,6 @@ export function Lightbox({
   const caption = attachment?.caption;
 
   let content: JSX.Element;
-  let hasControls = false;
   if (!contentType) {
     content = <>{children}</>;
   } else {
@@ -551,8 +550,6 @@ export function Lightbox({
           <source src={objectURL} />
         </video>
       );
-
-      hasControls = true;
     } else if (isUnsupportedImageType || isUnsupportedVideoType) {
       content = (
         <button
@@ -590,7 +587,6 @@ export function Lightbox({
         <div
           className={classNames('Lightbox Lightbox__container', {
             'Lightbox__container--zoom': isZoomed,
-            'Lightbox__container--has-controls': hasControls,
           })}
           onClick={(event: React.MouseEvent<HTMLDivElement>) => {
             event.stopPropagation();
