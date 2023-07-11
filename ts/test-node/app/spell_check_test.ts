f7e1458ad9063964efd455c1c2e8a14428f16898
Carlos Tarmeno <carlos-100-800@hotmail.com>
2023-07-10 20:58:55 -0500

    Added context-menu copy & paste test

--- ts/test-node/app/spell_check_test.ts
+++ ts/test-node/app/spell_check_test.ts
@@ -3,7 +3,51 @@
 
 import { assert } from 'chai';
 
+import { clipboard } from 'electron';
 import { getLanguages } from '../../../app/spell_check';
+
+type ContextMenuParams = {
+  x: number;
+  y: number;
+  editFlags: {
+    canUndo: boolean;
+    canRedo: boolean;
+    canCut: boolean;
+    canPaste: boolean;
+    canSelectAll: boolean;
+    canCopy: boolean;
+    canDelete: boolean;
+    canEditRichly: boolean;
+  };
+  frame: Electron.WebFrameMain;
+  linkURL: string;
+  linkText: string;
+  pageURL: string;
+  frameURL: string;
+  srcURL: string;
+  mediaType: string;
+  hasImageContents: boolean;
+  isEditable: boolean;
+  selectionText: string;
+  titleText: string;
+  altText: string;
+  suggestedFilename: string;
+  selectionRect: {
+    x: number;
+    y: number;
+    width: number;
+    height: number;
+  };
+  selectionStartOffset: number;
+  referrerPolicy: Electron.Referrer;
+  misspelledWord: string;
+  dictionarySuggestions: Array<string>;
+  frameCharset: string;
+  inputFieldType: string;
+  spellcheckEnabled: boolean;
+  menuSourceType: string;
+  mediaFlags: Electron.MediaFlags;
+};
 
 describe('SpellCheck', () => {
   describe('getLanguages', () => {
@@ -58,6 +102,56 @@ describe('SpellCheck', () => {
         'en',
         'fr',
       ]);
+    });
+  });
+});
+
+describe('context-menu copy & paste', () => {
+  const params: ContextMenuParams = {
+    x: 0,
+    y: 0,
+    editFlags: {
+      canUndo: false,
+      canRedo: false,
+      canCut: false,
+      canPaste: false,
+      canSelectAll: false,
+      canCopy: true,
+      canDelete: false,
+      canEditRichly: false,
+    },
+    frame: {} as Electron.WebFrameMain,
+    linkURL: '',
+
+    // Propiedades adicionales requeridas por 'ContextMenuParams'
+    linkText: '',
+    pageURL: '',
+    frameURL: '',
+    srcURL: '',
+    mediaType: 'none',
+    hasImageContents: false,
+    isEditable: false,
+    selectionText: 'This is a test message \n',
+    titleText: '',
+    altText: '',
+    suggestedFilename: '',
+    selectionRect: { x: 0, y: 0, width: 0, height: 0 },
+    selectionStartOffset: 0,
+    referrerPolicy: {} as Electron.Referrer,
+    misspelledWord: '',
+    dictionarySuggestions: [],
+    frameCharset: '',
+    inputFieldType: '',
+    spellcheckEnabled: false,
+    menuSourceType: 'none',
+    mediaFlags: {} as Electron.MediaFlags,
+  };
+
+  beforeEach(() => {
+    it('should not leave trailing a newline when copying text', () => {
+      const copyText = params.selectionText.toString();
+      clipboard.writeText(copyText);
+      assert.notInclude(copyText, '\n');
     });
   });
 });
