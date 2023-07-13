a29433dd402669b682e60195fc9d7d8e1f16a6dd
Fedor Indutnyy <indutny@signal.org>
2023-07-13 11:12:17 -0700

    Release notes for v6.26

--- ts/components/WhatsNewModal.tsx
+++ ts/components/WhatsNewModal.tsx
@@ -29,7 +29,24 @@ export function WhatsNewModal({
   const releaseNotes: ReleaseNotesType = {
     date: new Date(window.getBuildCreation?.() || Date.now()),
     version: window.getVersion?.(),
-    features: [<Intl i18n={i18n} id="icu:WhatsNew__v6.25--0" />],
+    features: [
+      <Intl i18n={i18n} id="icu:WhatsNew__v6.26--0" />,
+      <Intl
+        i18n={i18n}
+        id="icu:WhatsNew__v6.26--1"
+        components={{
+          linkToGithub: (
+            <a
+              href="https://github.com/nrayburn-tech"
+              target="_blank"
+              rel="noreferrer"
+            >
+              @nrayburn-tech
+            </a>
+          ),
+        }}
+      />,
+    ],
   };
 
   if (releaseNotes.features.length === 1) {
