71958f8a01fcd2512458a607c813b65c293f3e5b
Fedor Indutny <79877362+indutny-signal@users.noreply.github.com>
2023-08-16 19:44:33 +0200

    Attachment controller fixes

--- ts/textsecure/WebAPI.ts
+++ ts/textsecure/WebAPI.ts
@@ -553,7 +553,7 @@ const WEBSOCKET_CALLS = new Set<keyof typeof URL_CALLS>([
   // ProfileController
   'profile',
 
-  // AttachmentControllerV2
+  // AttachmentControllerV3
   'attachmentId',
 
   // RemoteConfigController
@@ -2757,6 +2757,12 @@ export function initialize({
         type: 'PUT',
         version,
         data: encryptedBin,
+        redactUrl: () => {
+          const tmp = new URL(uploadLocation);
+          tmp.search = '';
+          tmp.pathname = '';
+          return `${tmp}[REDACTED]`;
+        },
       });
 
       return cdnKey;
