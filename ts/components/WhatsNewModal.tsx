d6571b04acc9f33ecdf2481facbd566da7c251c4
Fedor Indutnyy <indutny@signal.org>
2023-08-21 15:09:57 -0700

    Release notes

--- ts/components/WhatsNewModal.tsx
+++ ts/components/WhatsNewModal.tsx
@@ -29,10 +29,7 @@ export function WhatsNewModal({
   const releaseNotes: ReleaseNotesType = {
     date: new Date(window.getBuildCreation?.() || Date.now()),
     version: window.getVersion?.(),
-    features: [
-      <Intl i18n={i18n} id="icu:WhatsNew__v6.29--0" />,
-      <Intl i18n={i18n} id="icu:WhatsNew__v6.29--1" />,
-    ],
+    features: [<Intl i18n={i18n} id="icu:WhatsNew__v6.30--0" />],
   };
 
   if (releaseNotes.features.length === 1) {
