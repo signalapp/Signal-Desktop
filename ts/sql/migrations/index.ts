960beeb6bb4ed7ebda83f5bbfdcfc5b935da6347
automated-signal <37887102+automated-signal@users.noreply.github.com>
2023-09-21 09:18:50 -0700

    Update database error confirmation dialogs
    Co-authored-by: trevor-signal <131492920+trevor-signal@users.noreply.github.com>

--- ts/sql/migrations/index.ts
+++ ts/sql/migrations/index.ts
@@ -2011,6 +2011,10 @@ export const SCHEMA_VERSIONS = [
   updateToSchemaVersion940,
 ];
 
+export class DBVersionFromFutureError extends Error {
+  override name = 'DBVersionFromFutureError';
+}
+
 export function updateSchema(db: Database, logger: LoggerType): void {
   const sqliteVersion = getSQLiteVersion(db);
   const sqlcipherVersion = getSQLCipherVersion(db);
@@ -2027,9 +2031,9 @@ export function updateSchema(db: Database, logger: LoggerType): void {
   );
 
   if (startingVersion > MAX_VERSION) {
-    throw new Error(
+    throw new DBVersionFromFutureError(
       `SQL: User version is ${startingVersion} but the expected maximum version ` +
-        `is ${MAX_VERSION}. Did you try to start an old version of Signal?`
+        `is ${MAX_VERSION}.`
     );
   }
 
