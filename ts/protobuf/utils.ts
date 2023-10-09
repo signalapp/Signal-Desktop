02f1645226d1d76f99492e147d847c8b6b47d04d
William Grant <willmgrant@gmail.com>
2023-10-09 15:32:08 +1100

    fix: resolved lint errors for hasDefinedProperty
    part of the protobuf utils

--- ts/protobuf/utils.ts
+++ ts/protobuf/utils.ts
@@ -6,8 +6,11 @@
  * @param property - The property you want make sure is not undefined
  * @returns true if the property is defined or false if undefined or using a type-specific default value
  */
-function hasDefinedProperty<A extends {}, B extends PropertyKey & keyof A>(object: A, property: B) {
-  return object.hasOwnProperty(property) !== false;
+function hasDefinedProperty<A extends object, B extends PropertyKey & keyof A>(
+  object: A,
+  property: B
+) {
+  return Object.prototype.hasOwnProperty.call(object, property) !== false;
 }
 
 export const ProtobufUtils = {
