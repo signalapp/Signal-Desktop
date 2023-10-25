a49a6ec078edc69a600f9c60b12145cfa64bdce2
Scott Nonnenberg <scott@signal.org>
2023-10-25 16:46:10 -0700

    Add release notes for 6.37

--- ts/components/WhatsNewModal.tsx
+++ ts/components/WhatsNewModal.tsx
@@ -29,10 +29,7 @@ export function WhatsNewModal({
   const releaseNotes: ReleaseNotesType = {
     date: new Date(window.getBuildCreation?.() || Date.now()),
     version: window.getVersion?.(),
-    features: [
-      <Intl i18n={i18n} id="icu:WhatsNew__v6.36--0" />,
-      <Intl i18n={i18n} id="icu:WhatsNew__v6.36--1" />,
-    ],
+    features: [<Intl i18n={i18n} id="icu:WhatsNew__v6.37--0" />],
   };
 
   if (releaseNotes.features.length === 1) {
