c1222a748dc50b56dc8ddbc7bfec299be08ef3c3
Audric Ackermann <audric@loki.network>
2023-10-30 10:37:53 +1100

    fix: handle dataextraction notification even if read receipts are off

--- ts/receiver/contentMessage.ts
+++ ts/receiver/contentMessage.ts
@@ -836,7 +836,7 @@ export async function handleDataExtractionNotification(
   await removeFromCache(envelope);
 
   const convo = getConversationController().get(source);
-  if (!convo || !convo.isPrivate() || !Storage.get(SettingsKey.settingsReadReceipt)) {
+  if (!convo || !convo.isPrivate()) {
     window?.log?.info(
       'Got DataNotification for unknown or non private convo or read receipt not enabled'
     );
