07d81ebd27222551a272efc1a9186474f365926c
trevor-signal <trevor@signal.org>
2023-11-02 09:54:04 -0400

    Add release notes for 6.38

--- ts/components/WhatsNewModal.tsx
+++ ts/components/WhatsNewModal.tsx
@@ -29,7 +29,7 @@ export function WhatsNewModal({
   const releaseNotes: ReleaseNotesType = {
     date: new Date(window.getBuildCreation?.() || Date.now()),
     version: window.getVersion?.(),
-    features: [<Intl i18n={i18n} id="icu:WhatsNew__v6.37--0" />],
+    features: [<Intl i18n={i18n} id="icu:WhatsNew__v6.38--0" />],
   };
 
   if (releaseNotes.features.length === 1) {
