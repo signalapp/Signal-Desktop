7a182636d2550d241de7817fe553d2e872638ac7
Scott Nonnenberg <scott@signal.org>
2023-11-21 11:15:39 -0800

    EmojiPicker: Don't send on enter, just add emoji to draft message

--- ts/components/emoji/EmojiPicker.tsx
+++ ts/components/emoji/EmojiPicker.tsx
@@ -34,7 +34,6 @@ export type EmojiPickDataType = {
 export type OwnProps = {
   readonly i18n: LocalizerType;
   readonly onPickEmoji: (o: EmojiPickDataType) => unknown;
-  readonly doSend?: () => unknown;
   readonly skinTone?: number;
   readonly onSetSkinTone?: (tone: number) => unknown;
   readonly recentEmojis?: ReadonlyArray<string>;
@@ -71,7 +70,6 @@ export const EmojiPicker = React.memo(
     (
       {
         i18n,
-        doSend,
         onPickEmoji,
         skinTone = 0,
         onSetSkinTone,
@@ -153,11 +151,7 @@ export const EmojiPicker = React.memo(
           const { shortName } = e.currentTarget.dataset;
           if ('key' in e) {
             if (e.key === 'Enter') {
-              if (doSend) {
-                doSend();
-                e.stopPropagation();
-                e.preventDefault();
-              } else if (shortName) {
+              if (shortName) {
                 onPickEmoji({ skinTone: selectedTone, shortName });
                 e.stopPropagation();
                 e.preventDefault();
@@ -169,7 +163,7 @@ export const EmojiPicker = React.memo(
             onPickEmoji({ skinTone: selectedTone, shortName });
           }
         },
-        [doSend, onPickEmoji, selectedTone]
+        [onPickEmoji, selectedTone]
       );
 
       // Handle key presses, particularly Escape
