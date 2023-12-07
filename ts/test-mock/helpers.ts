726bfa30e749cd5df2a227d5aa79aaa9710d578d
automated-signal <37887102+automated-signal@users.noreply.github.com>
2023-12-06 19:01:33 -0600

    Reduce flake on edit mock test
    Co-authored-by: trevor-signal <131492920+trevor-signal@users.noreply.github.com>

--- ts/test-mock/helpers.ts
+++ ts/test-mock/helpers.ts
@@ -1,6 +1,8 @@
 // Copyright 2023 Signal Messenger, LLC
 // SPDX-License-Identifier: AGPL-3.0-only
 
+import type { Locator } from 'playwright';
+
 export function bufferToUuid(buffer: Buffer): string {
   const hex = buffer.toString('hex');
 
@@ -11,4 +13,22 @@ export function bufferToUuid(buffer: Buffer): string {
     hex.substring(16, 20),
     hex.substring(20),
   ].join('-');
+}
+
+export async function type(input: Locator, text: string): Promise<void> {
+  let currentValue = '';
+
+  try {
+    currentValue = await input.inputValue();
+  } catch (e) {
+    // if input is actually not an input (e.g. contenteditable)
+    currentValue = (await input.textContent()) ?? '';
+  }
+
+  // Type with a reasonably human delay
+  await input.type(text, { delay: 100 });
+
+  // Wait to ensure that the input (and react state controlling it) has actually
+  // updated with the right value
+  await input.locator(`:text("${currentValue}${text}")`).waitFor();
 }
