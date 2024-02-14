4320f5485a3c1b69b4b4b8c79907c972f30a3f28
automated-signal <37887102+automated-signal@users.noreply.github.com>
2024-02-14 15:49:47 -0600

    Fix MessageRequestResponse sync processing

--- ts/test-mock/messaging/unknown_contact_test.ts
+++ ts/test-mock/messaging/unknown_contact_test.ts
@@ -74,4 +74,70 @@ describe('unknown contacts', function (this: Mocha.Suite) {
       0
     );
   });
+
+  it('blocks incoming calls from unknown contacts & shows message request', async () => {
+    const { desktop } = bootstrap;
+
+    debug('sending calling offer message');
+    await unknownContact.sendRaw(desktop, {
+      callingMessage: {
+        offer: {
+          callId: new Long(Math.floor(Math.random() * 1e10)),
+          type: Proto.CallingMessage.Offer.Type.OFFER_AUDIO_CALL,
+          opaque: new Uint8Array(0),
+        },
+      },
+    });
+
+    debug('opening conversation');
+    const leftPane = page.locator('#LeftPane');
+
+    const conversationListItem = leftPane.getByRole('button', {
+      name: 'Chat with Unknown contact',
+    });
+    await conversationListItem.getByText('Message Request').click();
+
+    const conversationStack = page.locator('.Inbox__conversation-stack');
+    await conversationStack.getByText('Missed voice call').waitFor();
+
+    debug('accepting message request');
+    await page.getByText('message you and share your name').waitFor();
+    await page.getByRole('button', { name: 'Accept' }).click();
+    assert.strictEqual(
+      await page.getByText('message you and share your name').count(),
+      0
+    );
+  });
+
+  it('syncs message request state', async () => {
+    const { phone, desktop } = bootstrap;
+
+    debug('sending regular text message');
+    await unknownContact.sendText(desktop, 'hello');
+
+    debug('opening conversation');
+    const leftPane = page.locator('#LeftPane');
+
+    const conversationListItem = leftPane.getByRole('button', {
+      name: 'Chat with Unknown contact',
+    });
+    await conversationListItem.getByText('Message Request').click();
+
+    debug('sending message request sync');
+    await phone.sendRaw(desktop, {
+      syncMessage: {
+        messageRequestResponse: {
+          type: Proto.SyncMessage.MessageRequestResponse.Type.ACCEPT,
+          threadAci: unknownContact.device.aci,
+        },
+      },
+    });
+
+    debug('verifying that compose is now visible');
+    const composeArea = page.locator(
+      '.composition-area-wrapper, .Inbox__conversation .ConversationView'
+    );
+    const input = composeArea.locator('[data-testid=CompositionInput]');
+    await input.waitFor();
+  });
 });
