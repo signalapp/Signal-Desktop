bd9d4dc8217b13b99341e9ec61edccf516fb862a
ayumi-signal <ayumi@signal.org>
2024-03-13 13:17:12 -0700

    Release notes for 7.3

--- ts/components/WhatsNewModal.tsx
+++ ts/components/WhatsNewModal.tsx
@@ -42,7 +42,10 @@ export function WhatsNewModal({
   const releaseNotes: ReleaseNotesType = {
     date: new Date(window.getBuildCreation?.() || Date.now()),
     version: window.getVersion?.(),
-    features: [<Intl i18n={i18n} id="icu:WhatsNew__v7.2--0" />],
+    features: [
+      <Intl i18n={i18n} id="icu:WhatsNew__v7.3--0" />,
+      <Intl i18n={i18n} id="icu:WhatsNew__v7.3--1" />,
+    ],
   };
 
   if (releaseNotes.features.length === 1 && !releaseNotes.header) {
