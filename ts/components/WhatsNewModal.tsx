7236ca1391562ee6e24831c36fd491cd77bdc690
Scott Nonnenberg <scott@signal.org>
2024-03-20 17:43:49 -0700

    Release notes for 7.4

--- ts/components/WhatsNewModal.tsx
+++ ts/components/WhatsNewModal.tsx
@@ -42,10 +42,7 @@ export function WhatsNewModal({
   const releaseNotes: ReleaseNotesType = {
     date: new Date(window.getBuildCreation?.() || Date.now()),
     version: window.getVersion?.(),
-    features: [
-      <Intl i18n={i18n} id="icu:WhatsNew__v7.3--0" />,
-      <Intl i18n={i18n} id="icu:WhatsNew__v7.3--1" />,
-    ],
+    features: [<Intl i18n={i18n} id="icu:WhatsNew__v7.4--0" />],
   };
 
   if (releaseNotes.features.length === 1 && !releaseNotes.header) {
