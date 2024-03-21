aeae6d3622f51ff2f28b153b70df9a74b2435f7c
automated-signal <37887102+automated-signal@users.noreply.github.com>
2024-03-21 15:39:59 -0500

    Cleanup attachment download flow

--- ts/types/Attachment.ts
+++ ts/types/Attachment.ts
@@ -413,19 +413,15 @@ export async function captureDimensionsAndScreenshot(
   if (!attachment.path) {
     return attachment;
   }
-  logger.info('captureDimensionsAndScreenshot: starting');
 
   const absolutePath = getAbsoluteAttachmentPath(attachment.path);
 
   if (GoogleChrome.isImageTypeSupported(contentType)) {
     try {
-      logger.info('captureDimensionsAndScreenshot: getting image dimensions');
       const { width, height } = await getImageDimensionsFromURL({
         objectUrl: absolutePath,
         logger,
       });
-
-      logger.info('captureDimensionsAndScreenshot: generating thumbnail');
       const thumbnailBuffer = await blobToArrayBuffer(
         await makeImageThumbnail({
           size: THUMBNAIL_SIZE,
@@ -434,7 +430,7 @@ export async function captureDimensionsAndScreenshot(
           logger,
         })
       );
-      logger.info('captureDimensionsAndScreenshot: writing thumbnail');
+
       const thumbnailPath = await writeNewAttachmentData(
         new Uint8Array(thumbnailBuffer)
       );
@@ -461,7 +457,6 @@ export async function captureDimensionsAndScreenshot(
 
   let screenshotObjectUrl: string | undefined;
   try {
-    logger.info('captureDimensionsAndScreenshot: making video screenshot');
     const screenshotBuffer = await blobToArrayBuffer(
       await makeVideoScreenshot({
         objectUrl: absolutePath,
@@ -473,17 +468,14 @@ export async function captureDimensionsAndScreenshot(
       screenshotBuffer,
       THUMBNAIL_CONTENT_TYPE
     );
-    logger.info('captureDimensionsAndScreenshot: getting image dimensions');
     const { width, height } = await getImageDimensionsFromURL({
       objectUrl: screenshotObjectUrl,
       logger,
     });
-    logger.info('captureDimensionsAndScreenshot: writing attachment data');
     const screenshotPath = await writeNewAttachmentData(
       new Uint8Array(screenshotBuffer)
     );
 
-    logger.info('captureDimensionsAndScreenshot: making thumbnail');
     const thumbnailBuffer = await blobToArrayBuffer(
       await makeImageThumbnail({
         size: THUMBNAIL_SIZE,
@@ -493,7 +485,6 @@ export async function captureDimensionsAndScreenshot(
       })
     );
 
-    logger.info('captureDimensionsAndScreenshot: writing thumbnail');
     const thumbnailPath = await writeNewAttachmentData(
       new Uint8Array(thumbnailBuffer)
     );
