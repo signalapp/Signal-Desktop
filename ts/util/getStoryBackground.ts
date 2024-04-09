4caa260a22824406a7ff83dc3740709fc2679448
Fedor Indutny <79877362+indutny-signal@users.noreply.github.com>
2024-04-09 23:38:27 +0200

    Update story gradients

--- ts/util/getStoryBackground.ts
+++ ts/util/getStoryBackground.ts
@@ -15,6 +15,24 @@ export function getBackgroundColor({
   color,
   gradient,
 }: Pick<TextAttachmentType, 'color' | 'gradient'>): string {
+  if (
+    gradient?.colors?.length &&
+    gradient?.colors.length === gradient?.positions?.length
+  ) {
+    const values = [`${gradient.angle}deg`];
+    for (const [i, step] of gradient.colors.entries()) {
+      const position = gradient.positions[i] ?? 1;
+      const stepHex = getHexFromNumber(step || COLOR_WHITE_INT);
+      if (position == null) {
+        values.push(stepHex);
+      } else {
+        values.push(`${stepHex} ${position * 100}%`);
+      }
+    }
+
+    return `linear-gradient(${values.join(', ')}) border-box`;
+  }
+
   if (gradient) {
     return `linear-gradient(${gradient.angle}deg, ${getHexFromNumber(
       gradient.startColor || COLOR_WHITE_INT
