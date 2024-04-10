ddb201b9aeb1ba7f962ee05ac49df444a4b0c0b2
Fedor Indutny <79877362+indutny-signal@users.noreply.github.com>
2024-04-10 23:53:35 +0200

    Clear old conversation merges on Note to Self

---
+++ ts/sql/migrations/1020-self-merges.ts
@@ -0,0 +1,54 @@
+// Copyright 2024 Signal Messenger, LLC
+// SPDX-License-Identifier: AGPL-3.0-only
+
+import type { Database } from '@signalapp/better-sqlite3';
+
+import type { LoggerType } from '../../types/Logging';
+import { sql } from '../util';
+import { getOurUuid } from './41-uuid-keys';
+
+export const version = 1020;
+
+export function updateToSchemaVersion1020(
+  currentVersion: number,
+  db: Database,
+  logger: LoggerType
+): void {
+  if (currentVersion >= 1020) {
+    return;
+  }
+
+  db.transaction(() => {
+    const ourAci = getOurUuid(db);
+
+    if (ourAci == null) {
+      logger.info('updateToSchemaVersion1020: not linked');
+      return;
+    }
+
+    const [selectQuery, selectParams] = sql`
+      SELECT id FROM conversations
+      WHERE serviceId IS ${ourAci}
+    `;
+    const ourConversationId = db.prepare(selectQuery).pluck().get(selectParams);
+    if (ourConversationId == null) {
+      logger.error('updateToSchemaVersion1020: no conversation');
+      return;
+    }
+
+    const [deleteQuery, deleteParams] = sql`
+      DELETE FROM messages
+      WHERE
+        conversationId IS ${ourConversationId} AND
+        type IS 'conversation-merge'
+    `;
+    const { changes } = db.prepare(deleteQuery).run(deleteParams);
+    if (changes !== 0) {
+      logger.warn(`updateToSchemaVersion1020: removed ${changes} self merges`);
+    }
+  })();
+
+  db.pragma('user_version = 1020');
+
+  logger.info('updateToSchemaVersion1020: success!');
+}
