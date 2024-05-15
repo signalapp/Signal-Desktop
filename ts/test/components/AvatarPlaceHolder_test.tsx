192cbd268e0db119646f9370d653785152dca25e
William Grant <willmgrant@gmail.com>
2024-05-15 17:01:50 +1000

    feat: added react testing library and chai dom
    setup inital work for unit testing avatar placeholder bg color generation

---
+++ ts/test/components/AvatarPlaceHolder_test.tsx
@@ -0,0 +1,21 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import chai from 'chai';
+import chaiDom from 'chai-dom';
+
+chai.use(chaiDom);
+
+describe('AvatarPlaceHolder', () => {
+  it('should render', async () => {
+    // ARRANGE
+    // TODO have proper values
+    // render(<AvatarPlaceHolder diameter={32} name={'0'} pubkey={'0xasdfgggjkdks'} />);
+    // ACT
+    // TODO proper actions
+    // await userEvent.click(screen.getByText('0xasdfgggjkdks'));
+    // await screen.findByRole('heading');
+    // ASSERT
+    // TODO proper expectations
+    // expect(screen.getByRole('heading')).text('hello there');
+    // expect(screen.getByRole('button')).to.have.attribute('disabled').match('true');
+  });
+});
