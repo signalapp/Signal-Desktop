d662b7d17bdac3b54f9c3c830ad064143f57303d
William Grant <willmgrant@gmail.com>
2024-05-16 16:27:24 +1000

    fix: use recoveryPasswordOutline for light themes
    updated to use building block svgs

--- ts/components/leftpane/LeftPaneSectionHeader.tsx
+++ ts/components/leftpane/LeftPaneSectionHeader.tsx
@@ -9,6 +9,7 @@ import {
 import { disableRecoveryPhrasePrompt } from '../../state/ducks/userConfig';
 import { getFocusedSection, getLeftOverlayMode } from '../../state/selectors/section';
 import { useHideRecoveryPasswordEnabled } from '../../state/selectors/settings';
+import { getTheme } from '../../state/selectors/theme';
 import { getShowRecoveryPhrasePrompt } from '../../state/selectors/userConfig';
 import { isSignWithRecoveryPhrase } from '../../util/storage';
 import { Flex } from '../basic/Flex';
@@ -60,7 +61,7 @@ const StyledBanner = styled(Flex)`
 
   svg {
     margin-top: -3px;
-    margin-left: var(--margins-sm);
+    margin-left: var(--margins-xs);
   }
 `;
 
@@ -78,6 +79,7 @@ const StyledLeftPaneBanner = styled.div`
 `;
 
 export const LeftPaneBanner = () => {
+  const theme = useSelector(getTheme);
   const section = useSelector(getFocusedSection);
   const isSignInWithRecoveryPhrase = isSignWithRecoveryPhrase();
   const hideRecoveryPassword = useHideRecoveryPasswordEnabled();
@@ -109,7 +111,7 @@ export const LeftPaneBanner = () => {
         <Flex container={true} width={'100%'} alignItems="flex-start">
           <StyledBannerTitle>{window.i18n('saveRecoveryPassword')}</StyledBannerTitle>
           <SessionIcon
-            iconType="recoveryPasswordFill"
+            iconType={theme.includes('dark') ? 'recoveryPasswordFill' : 'recoveryPasswordOutline'}
             iconSize="medium"
             iconColor="var(--text-primary-color)"
           />
