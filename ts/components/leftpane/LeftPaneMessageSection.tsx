0f89b1134d5c8ee5d2dfb79d6210a8361f27a441
William Grant <willmgrant@gmail.com>
2024-05-27 14:17:24 +1000

    fix: revert naming convo render functions as components
    it broke the search input for some reason

--- ts/components/leftpane/LeftPaneMessageSection.tsx
+++ ts/components/leftpane/LeftPaneMessageSection.tsx
@@ -82,7 +82,7 @@ export const LeftPaneMessageSection = () => {
     return <ConversationListItem key={key} style={style} conversationId={conversationId} />;
   };
 
-  const ConversationList = () => {
+  const renderList = () => {
     if (!isEmpty(searchTerm)) {
       return <SearchResults />;
     }
@@ -113,7 +113,7 @@ export const LeftPaneMessageSection = () => {
     );
   };
 
-  const Conversations = () => {
+  const renderConversations = () => {
     return (
       <StyledConversationListContent>
         <SessionSearchInput />
@@ -122,7 +122,7 @@ export const LeftPaneMessageSection = () => {
             window.inboxStore?.dispatch(setLeftOverlayMode('message-requests'));
           }}
         />
-        <ConversationList />
+        {renderList()}
       </StyledConversationListContent>
     );
   };
@@ -130,7 +130,7 @@ export const LeftPaneMessageSection = () => {
   return (
     <StyledLeftPaneContent>
       <LeftPaneSectionHeader />
-      {leftOverlayMode ? <ClosableOverlay /> : <Conversations />}
+      {leftOverlayMode ? <ClosableOverlay /> : renderConversations()}
     </StyledLeftPaneContent>
   );
 };
