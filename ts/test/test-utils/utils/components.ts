b219a8526e57422b9a29b307b7430c4ed7c671bf
William Grant <willmgrant@gmail.com>
2024-06-12 18:01:50 +1000

    fix: suggest window log stubbing if it's not working when printing render or html elements

--- ts/test/test-utils/utils/components.ts
+++ ts/test/test-utils/utils/components.ts
@@ -3,14 +3,18 @@ import { enableLogRedirect } from './stubbing';
 
 const printHTMLElement = async (element: HTMLElement, name?: string) => {
   if (!window.log || !enableLogRedirect) {
-    throw Error('window.log is not defined. Have you turned on enableLogRedirect?');
+    throw Error(
+      'window.log is not defined. Have you turned on enableLogRedirect / called stubWindowLog() ?'
+    );
   }
 
   return window.log.debug(`\nHTML Element${name ? ` (${name})` : ''}:\n${prettyDOM(element)}\n`);
 };
 const printRenderResult = async (result: RenderResult, name?: string) => {
   if (!window.log || !enableLogRedirect) {
-    throw Error('window.log is not defined. Have you turned on enableLogRedirect?');
+    throw Error(
+      'window.log is not defined. Have you turned on enableLogRedirect / called stubWindowLog() ?'
+    );
   }
 
   return window.log.debug(
