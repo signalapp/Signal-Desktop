2633776aaa6beef8abab3ebf894bd0fde7fc0cf4
automated-signal <37887102+automated-signal@users.noreply.github.com>
2024-07-11 17:59:53 -0500

    Use a fork of @electron/symbolicate-mac for speed
    Co-authored-by: Fedor Indutny <79877362+indutny-signal@users.noreply.github.com>

--- ts/scripts/symbolicate-mac.d.ts
+++ ts/scripts/symbolicate-mac.d.ts
@@ -1,7 +1,7 @@
 // Copyright 2024 Signal Messenger, LLC
 // SPDX-License-Identifier: AGPL-3.0-only
 
-declare module '@electron/symbolicate-mac' {
+declare module '@indutny/symbolicate-mac' {
   export function symbolicate(options: {
     file: string;
     force?: boolean;
