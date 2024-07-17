3cec66697994e9001e1519c564e4c60caf6b9166
William Grant <willmgrant@gmail.com>
2024-07-17 14:50:33 +1000

    feat: commment on libsession constants
    give context that CONSTANTS is not a object but an immediately invoked
    function

--- ts/session/utils/libsession/libsession_constants.ts
+++ ts/session/utils/libsession/libsession_constants.ts
@@ -1,5 +1,6 @@
 import { CONSTANTS, ConstantsType } from 'libsession_util_nodejs';
 
+// NOTE CONSTANTS is an immediately invoked function that returns the libsession constants object
 const { CONTACT_MAX_NAME_LENGTH, BASE_GROUP_MAX_NAME_LENGTH, GROUP_INFO_MAX_NAME_LENGTH } =
   CONSTANTS;
 
