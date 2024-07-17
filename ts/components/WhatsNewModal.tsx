82178d8268fcf1aca068435b239e388520e08c8e
Fedor Indutny <indutny@signal.org>
2024-07-17 09:43:32 -0700

    Release notes for 7.17

--- ts/components/WhatsNewModal.tsx
+++ ts/components/WhatsNewModal.tsx
@@ -42,7 +42,7 @@ export function WhatsNewModal({
   const releaseNotes: ReleaseNotesType = {
     date: new Date(window.getBuildCreation?.() || Date.now()),
     version: window.getVersion?.(),
-    features: [<I18n i18n={i18n} id="icu:WhatsNew__v7.16--0" />],
+    features: [<I18n i18n={i18n} id="icu:WhatsNew__v7.17--0" />],
   };
 
   if (releaseNotes.features.length === 1 && !releaseNotes.header) {
