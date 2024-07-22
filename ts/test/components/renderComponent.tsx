362e360f405fb255c100954e75de34e4f57e4c44
yougotwill <willmgrant@gmail.com>
2024-07-22 12:01:45 +1000

    fix: replace chai with react-test-renderer for unit testing components
    rewrite avatar placeholder test

--- ts/test/components/renderComponent.tsx
+++ ts/test/components/renderComponent.tsx
@@ -1,9 +1,9 @@
-/* eslint-disable import/no-extraneous-dependencies */
-import { render, RenderOptions } from '@testing-library/react';
 import { AnimatePresence, MotionGlobalConfig } from 'framer-motion';
+import { isArray, isEqual, unset } from 'lodash';
 import { ReactElement, ReactNode } from 'react';
-import { SessionTheme } from '../../themes/SessionTheme';
 import { ErrorBoundary } from 'react-error-boundary';
+import TestRenderer from 'react-test-renderer';
+import { SessionTheme } from '../../themes/SessionTheme';
 
 const Providers = ({ children }: { children: ReactNode }) => {
   MotionGlobalConfig.skipAnimations = false;
@@ -21,7 +21,38 @@ const Providers = ({ children }: { children: ReactNode }) => {
   );
 };
 
-const renderComponent = (ui: ReactElement, options?: Omit<RenderOptions, 'wrapper'>) =>
-  render(ui, { wrapper: Providers, ...options });
+function renderComponent(children: ReactElement): TestRenderer.ReactTestRenderer {
+  return TestRenderer.create(<Providers>{children}</Providers>);
+}
 
-export { renderComponent };
+function getComponentTree(
+  result: TestRenderer.ReactTestRenderer
+): Array<TestRenderer.ReactTestRendererTree> {
+  const trees = result.toTree();
+  return !trees ? [] : isArray(trees) ? trees : [trees];
+}
+
+function findByDataTestId(
+  renderResult: TestRenderer.ReactTestRenderer,
+  dataTestId: string
+): TestRenderer.ReactTestInstance {
+  return renderResult.root.findByProps({ 'data-testid': dataTestId });
+}
+
+function areResultsEqual(
+  renderResult: TestRenderer.ReactTestRenderer,
+  renderResult2: TestRenderer.ReactTestRenderer,
+  ignoreDataTestIds?: boolean
+): boolean {
+  if (ignoreDataTestIds) {
+    const obj = renderResult.toJSON();
+    const obj2 = renderResult2.toJSON();
+    unset(obj, "props['data-testid']");
+    unset(obj2, "props['data-testid']");
+    return isEqual(obj, obj2);
+  }
+
+  return isEqual(renderResult.toJSON(), renderResult2.toJSON());
+}
+
+export { areResultsEqual, findByDataTestId, getComponentTree, renderComponent };
