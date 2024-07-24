16864e381a1ded6fe60b5acc4eec8e282497911b
ayumi-signal <143036029+ayumi-signal@users.noreply.github.com>
2024-07-24 09:23:17 -0700

    Linux: Detect changes in safeStorage backend
    Co-authored-by: Fedor Indutny <79877362+indutny-signal@users.noreply.github.com>

---
+++ ts/util/linuxPasswordStoreFlags.ts
@@ -0,0 +1,11 @@
+// Copyright 2024 Signal Messenger, LLC
+// SPDX-License-Identifier: AGPL-3.0-only
+
+// Mapping of safeStorage backends to flags used to activate them.
+// See https://www.electronjs.org/docs/latest/api/safe-storage
+export const LINUX_PASSWORD_STORE_FLAGS: Record<string, string> = {
+  basic_text: 'basic',
+  gnome_libsecret: 'gnome-libsecret',
+  kwallet: 'kwallet',
+  kwallet5: 'kwallet5',
+};
