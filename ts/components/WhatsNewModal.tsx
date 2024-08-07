6bb518f2f789f1f9dfea544d3aa75e5bf782f5af
Jamie Kyle <jamie@signal.org>
2024-08-07 14:44:01 -0700

    Release notes for 7.20

--- ts/components/WhatsNewModal.tsx
+++ ts/components/WhatsNewModal.tsx
@@ -43,8 +43,8 @@ export function WhatsNewModal({
     date: new Date(window.getBuildCreation?.() || Date.now()),
     version: window.getVersion?.(),
     features: [
-      <I18n i18n={i18n} id="icu:WhatsNew__v7.19--0" />,
-      <I18n i18n={i18n} id="icu:WhatsNew__v7.19--1" />,
+      <I18n i18n={i18n} id="icu:WhatsNew__v7.20--0" />,
+      <I18n i18n={i18n} id="icu:WhatsNew__v7.20--1" />,
     ],
   };
 
