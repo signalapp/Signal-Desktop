087515d9c7faae4c79816a685d6e5c92c51938aa
automated-signal <37887102+automated-signal@users.noreply.github.com>
2024-08-14 19:41:48 -0500

    Make messaging/relink mock test more reliable
    Co-authored-by: Scott Nonnenberg <scott@signal.org>
    Co-authored-by: Fedor Indutny <indutny@signal.org>

--- ts/background.ts
+++ ts/background.ts
@@ -35,6 +35,7 @@ import { ChallengeHandler } from './challenge';
 import * as durations from './util/durations';
 import { drop } from './util/drop';
 import { explodePromise } from './util/explodePromise';
+import type { ExplodePromiseResultType } from './util/explodePromise';
 import { isWindowDragElement } from './util/isWindowDragElement';
 import { assertDev, strictAssert } from './util/assert';
 import { filter } from './util/iterables';
@@ -1462,7 +1463,7 @@ export async function startApp(): Promise<void> {
       log.info(`Startup/syncTasks: Queueing ${syncTasks.length} sync tasks`);
       await queueSyncTasks(syncTasks, DataWriter.removeSyncTaskById);
 
-      log.info('`Startup/syncTasks: Done');
+      log.info('Startup/syncTasks: Done');
     }
 
     log.info('listening for registration events');
@@ -1702,14 +1703,29 @@ export async function startApp(): Promise<void> {
   }
 
   let connectCount = 0;
-  let connecting = false;
+  let connectPromise: ExplodePromiseResultType<void> | undefined;
   let remotelyExpired = false;
   async function connect(firstRun?: boolean) {
-    if (connecting) {
+    if (connectPromise && !firstRun) {
       log.warn('background: connect already running', {
         connectCount,
         firstRun,
       });
+      return;
+    }
+    if (connectPromise && firstRun) {
+      while (connectPromise) {
+        log.warn(
+          'background: connect already running; waiting for previous run',
+          {
+            connectCount,
+            firstRun,
+          }
+        );
+        // eslint-disable-next-line no-await-in-loop
+        await connectPromise.promise;
+      }
+      await connect(firstRun);
       return;
     }
 
@@ -1721,8 +1737,7 @@ export async function startApp(): Promise<void> {
     strictAssert(server !== undefined, 'WebAPI not connected');
 
     try {
-      connecting = true;
-
+      connectPromise = explodePromise();
       // Reset the flag and update it below if needed
       setIsInitialSync(false);
 
@@ -1798,12 +1813,13 @@ export async function startApp(): Promise<void> {
         window.textsecure.storage.user.getDeviceId() !== 1
       ) {
         log.info('Boot after upgrading. Requesting contact sync');
-        window.getSyncRequest();
-
-        void StorageService.reprocessUnknownFields();
-        void runStorageService();
 
         try {
+          window.getSyncRequest();
+
+          void StorageService.reprocessUnknownFields();
+          void runStorageService();
+
           const manager = window.getAccountManager();
           await Promise.all([
             manager.maybeUpdateDeviceName(),
@@ -1811,7 +1827,7 @@ export async function startApp(): Promise<void> {
           ]);
         } catch (e) {
           log.error(
-            'Problem with account manager updates after starting new version: ',
+            "Problem with 'boot after upgrade' tasks: ",
             Errors.toLogFormat(e)
           );
         }
@@ -1989,7 +2005,15 @@ export async function startApp(): Promise<void> {
 
       reconnectBackOff.reset();
     } finally {
-      connecting = false;
+      if (connectPromise) {
+        connectPromise.resolve();
+        connectPromise = undefined;
+      } else {
+        log.warn('background connect: in finally, no connectPromise!', {
+          connectCount,
+          firstRun,
+        });
+      }
     }
   }
 
@@ -3089,6 +3113,10 @@ export async function startApp(): Promise<void> {
       );
     } finally {
       await Registration.markEverDone();
+
+      if (window.SignalCI) {
+        window.SignalCI.handleEvent('unlinkCleanupComplete', null);
+      }
     }
   }
 
