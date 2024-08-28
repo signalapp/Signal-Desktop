f086809dcc245152f5539e24f4408ee3e0797327
trevor-signal <trevor@signal.org>
2024-08-28 15:44:23 -0400

    Release notes for 7.23

--- ts/components/WhatsNewModal.tsx
+++ ts/components/WhatsNewModal.tsx
@@ -42,7 +42,10 @@ export function WhatsNewModal({
   const releaseNotes: ReleaseNotesType = {
     date: new Date(window.getBuildCreation?.() || Date.now()),
     version: window.getVersion?.(),
-    features: [<I18n i18n={i18n} id="icu:WhatsNew__v7.22--0" />],
+    features: [
+      <I18n i18n={i18n} id="icu:WhatsNew__v7.23--0" />,
+      <I18n i18n={i18n} id="icu:WhatsNew__v7.23--1" />,
+    ],
   };
 
   if (releaseNotes.features.length === 1 && !releaseNotes.header) {
