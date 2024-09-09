1922864a49f535e8f78bd428315551d9d0c4f983
Fedor Indutny <79877362+indutny-signal@users.noreply.github.com>
2024-09-09 10:41:32 -0700

    Save artifacts on preload cache build failure

--- ts/scripts/generate-preload-cache.ts
+++ ts/scripts/generate-preload-cache.ts
@@ -1,10 +1,14 @@
 // Copyright 2024 Signal Messenger, LLC
 // SPDX-License-Identifier: AGPL-3.0-only
 
-import { spawnSync } from 'node:child_process';
+import { spawn } from 'node:child_process';
 import { join } from 'node:path';
 import { tmpdir } from 'node:os';
-import { mkdtemp, rm } from 'node:fs/promises';
+import { mkdir, mkdtemp, rm, rename } from 'node:fs/promises';
+import pTimeout from 'p-timeout';
+
+import { MINUTE } from '../util/durations';
+import { explodePromise } from '../util/explodePromise';
 
 const ROOT_DIR = join(__dirname, '..', '..');
 
@@ -18,30 +22,55 @@ const ELECTRON = join(
 async function main(): Promise<void> {
   const storagePath = await mkdtemp(join(tmpdir(), 'signal-preload-cache-'));
 
-  let status: number | null;
-  try {
-    ({ status } = spawnSync(
-      ELECTRON,
-      ['--js-args="--predictable --random-seed 1"', 'ci.js'],
-      {
-        cwd: ROOT_DIR,
-        env: {
-          ...process.env,
-          GENERATE_PRELOAD_CACHE: 'on',
-          SIGNAL_CI_CONFIG: JSON.stringify({
-            storagePath,
-          }),
-        },
-        // Since we run `.cmd` file on Windows - use shell
-        shell: process.platform === 'win32',
-      }
-    ));
-  } finally {
-    await rm(storagePath, { recursive: true });
-  }
+  const proc = spawn(
+    ELECTRON,
+    ['--js-args="--predictable --random-seed 1"', 'ci.js'],
+    {
+      cwd: ROOT_DIR,
+      env: {
+        ...process.env,
+        GENERATE_PRELOAD_CACHE: 'on',
+        SIGNAL_CI_CONFIG: JSON.stringify({
+          storagePath,
+        }),
+      },
+      // Since we run `.cmd` file on Windows - use shell
+      shell: process.platform === 'win32',
+    }
+  );
 
-  if (status !== 0) {
-    throw new Error(`Exit code: ${status}`);
+  try {
+    const { promise, resolve, reject } = explodePromise<number | null>();
+    proc.on('exit', status => resolve(status));
+    proc.on('error', error => reject(error));
+
+    const status = await pTimeout(promise, 5 * MINUTE);
+
+    if (status !== 0) {
+      throw new Error(`Exit code: ${status}`);
+    }
+  } catch (error) {
+    const { ARTIFACTS_DIR } = process.env;
+    if (!ARTIFACTS_DIR) {
+      console.error(
+        'Not saving artifacts. Please set ARTIFACTS_DIR env variable'
+      );
+    } else {
+      console.error(`Saving logs to ${ARTIFACTS_DIR}`);
+      await mkdir(ARTIFACTS_DIR, { recursive: true });
+
+      const logsDir = join(storagePath, 'logs');
+      await rename(logsDir, join(ARTIFACTS_DIR, 'logs'));
+    }
+
+    throw error;
+  } finally {
+    try {
+      proc.kill();
+    } catch {
+      // Ignore
+    }
+    await rm(storagePath, { recursive: true });
   }
 }
 
