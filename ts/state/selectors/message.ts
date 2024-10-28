8a1f19b3d8c7a32d8c233eff6a1ef67c38324b4f
automated-signal <37887102+automated-signal@users.noreply.github.com>
2024-10-28 18:34:41 -0500

    Only show download button if message has attachments to download
    Co-authored-by: Scott Nonnenberg <scott@signal.org>

--- ts/state/selectors/message.ts
+++ ts/state/selectors/message.ts
@@ -2068,15 +2068,19 @@ export function canDownload(
   message: MessageWithUIFieldsType,
   conversationSelector: GetConversationByIdType
 ): boolean {
-  if (isOutgoing(message)) {
-    return true;
-  }
-
   const conversation = getConversation(message, conversationSelector);
   const isAccepted = Boolean(
     conversation && conversation.acceptedMessageRequest
   );
-  if (!isAccepted) {
+  if (isIncoming(message) && !isAccepted) {
+    return false;
+  }
+
+  if (message.sticker) {
+    return false;
+  }
+
+  if (isTapToView(message)) {
     return false;
   }
 
@@ -2086,7 +2090,7 @@ export function canDownload(
     return attachments.every(attachment => Boolean(attachment.path));
   }
 
-  return true;
+  return false;
 }
 
 export function getLastChallengeError(
