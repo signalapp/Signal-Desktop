d9084b09758304afb60377941cb35ab0e7a63aac
khuddite <biliie811028@hotmail.com>
2024-11-05 09:50:49 -0500

    fix isImage flag being true for emojis in the context menu

--- app/spell_check.ts
+++ app/spell_check.ts
@@ -97,7 +97,10 @@ export const setup = (
     const isMisspelled = Boolean(params.misspelledWord);
     const isLink = Boolean(params.linkURL);
     const isImage =
-      params.mediaType === 'image' && params.hasImageContents && params.srcURL;
+      params.mediaType === 'image' &&
+      params.hasImageContents &&
+      params.srcURL &&
+      !editFlags.canCopy;
     const showMenu =
       params.isEditable || editFlags.canCopy || isLink || isImage;
 
