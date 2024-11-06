c39d1359fdb21578e3d59748df018b7001a07edc
Jamie Kyle <113370520+jamiebuilds-signal@users.noreply.github.com>
2024-11-06 10:33:05 -0800

    Fix dark mode when in light mode os theme

--- ts/util/getThemeType.ts
+++ ts/util/getThemeType.ts
@@ -2,20 +2,27 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 
 import { SystemThemeType, ThemeType } from '../types/Util';
+import { missingCaseError } from './missingCaseError';
 
 export async function getThemeType(): Promise<ThemeType> {
   const themeSetting = await window.Events.getThemeSetting();
 
-  if (
-    themeSetting === 'light' ||
-    window.systemTheme === SystemThemeType.light
-  ) {
+  if (themeSetting === 'light') {
     return ThemeType.light;
   }
 
-  if (themeSetting === 'dark' || window.systemTheme === SystemThemeType.dark) {
+  if (themeSetting === 'dark') {
     return ThemeType.dark;
   }
 
-  return window.systemTheme;
+  if (themeSetting === 'system') {
+    if (window.systemTheme === SystemThemeType.light) {
+      return ThemeType.light;
+    }
+    if (window.systemTheme === SystemThemeType.dark) {
+      return ThemeType.dark;
+    }
+    throw missingCaseError(window.systemTheme);
+  }
+  throw missingCaseError(themeSetting);
 }
