84b7cb4116389b1ca84fb9e38534cff873190f32
Fedor Indutny <79877362+indutny-signal@users.noreply.github.com>
2024-11-11 11:57:55 -0800

    Refactor force-update updater mode

--- ts/updater/macos.ts
+++ ts/updater/macos.ts
@@ -16,7 +16,9 @@ export class MacOSUpdater extends Updater {
     // No installers are cache on macOS
   }
 
-  protected async installUpdate(updateFilePath: string): Promise<void> {
+  protected async installUpdate(
+    updateFilePath: string
+  ): Promise<() => Promise<void>> {
     const { logger } = this;
 
     logger.info('downloadAndInstall: handing download to electron...');
@@ -38,11 +40,11 @@ export class MacOSUpdater extends Updater {
     // At this point, closing the app will cause the update to be installed automatically
     //   because Squirrel has cached the update file and will do the right thing.
 
-    this.setUpdateListener(async () => {
+    return async () => {
       logger.info('downloadAndInstall: restarting...');
       this.markRestarting();
       autoUpdater.quitAndInstall();
-    });
+    };
   }
 
   private async handToAutoUpdate(filePath: string): Promise<void> {
