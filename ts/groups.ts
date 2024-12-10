7124ba36cd947f4e24264186e8cdbff111734f0f
automated-signal <37887102+automated-signal@users.noreply.github.com>
2024-12-10 12:04:36 -0600

    Groups: Only set active_at when changes have happened
    Co-authored-by: Scott Nonnenberg <scott@signal.org>

--- ts/groups.ts
+++ ts/groups.ts
@@ -3157,7 +3157,12 @@ async function updateGroup(
   // By updating activeAt we force this conversation into the left panel. We don't want
   //   all groups to show up on link, and we don't want Unknown Group in the left pane.
   let activeAt = conversation.get('active_at') || null;
-  if (!viaFirstStorageSync && newAttributes.name) {
+  const justDiscoveredGroupName =
+    !conversation.get('name') && newAttributes.name;
+  if (
+    !viaFirstStorageSync &&
+    (justDiscoveredGroupName || groupChangeMessages.length)
+  ) {
     activeAt = initialSentAt;
   }
 
