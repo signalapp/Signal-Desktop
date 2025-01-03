85c74e7e6816f06c0d76eb2e682cabe8edd8df3b
Jamie Kyle <113370520+jamiebuilds-signal@users.noreply.github.com>
2025-01-03 10:03:28 -0800

    Upgrade electron-builder

--- scripts/prepare_alpha_build.js
+++ scripts/prepare_alpha_build.js
@@ -36,10 +36,6 @@ const APP_ID_PATH = 'build.appId';
 const PRODUCTION_APP_ID = 'org.whispersystems.signal-desktop';
 const ALPHA_APP_ID = 'org.whispersystems.signal-desktop-alpha';
 
-const STARTUP_WM_CLASS_PATH = 'build.linux.desktop.StartupWMClass';
-const PRODUCTION_STARTUP_WM_CLASS = 'Signal';
-const ALPHA_STARTUP_WM_CLASS = 'Signal Alpha';
-
 const DESKTOP_NAME_PATH = 'desktopName';
 
 // Note: we're avoiding dashes in our .desktop name due to xdg-settings behavior
@@ -61,7 +57,6 @@ function checkValue(object, objectPath, expected) {
 checkValue(packageJson, NAME_PATH, PRODUCTION_NAME);
 checkValue(packageJson, PRODUCT_NAME_PATH, PRODUCTION_PRODUCT_NAME);
 checkValue(packageJson, APP_ID_PATH, PRODUCTION_APP_ID);
-checkValue(packageJson, STARTUP_WM_CLASS_PATH, PRODUCTION_STARTUP_WM_CLASS);
 checkValue(packageJson, DESKTOP_NAME_PATH, PRODUCTION_DESKTOP_NAME);
 
 // -------
@@ -69,7 +64,6 @@ checkValue(packageJson, DESKTOP_NAME_PATH, PRODUCTION_DESKTOP_NAME);
 _.set(packageJson, NAME_PATH, ALPHA_NAME);
 _.set(packageJson, PRODUCT_NAME_PATH, ALPHA_PRODUCT_NAME);
 _.set(packageJson, APP_ID_PATH, ALPHA_APP_ID);
-_.set(packageJson, STARTUP_WM_CLASS_PATH, ALPHA_STARTUP_WM_CLASS);
 _.set(packageJson, DESKTOP_NAME_PATH, ALPHA_DESKTOP_NAME);
 
 // -------
