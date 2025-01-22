f0d112fa1b63c54e1dc802ef3d918f56839e0c9e
automated-signal <37887102+automated-signal@users.noreply.github.com>
2025-01-22 15:09:22 -0600

    Enable link & sync in beta

--- ts/textsecure/Provisioner.ts
+++ ts/textsecure/Provisioner.ts
@@ -422,7 +422,7 @@ export class Provisioner {
       .toAppUrl({
         uuid,
         pubKey: Bytes.toBase64(cipher.getPublicKey()),
-        capabilities: isLinkAndSyncEnabled(this.#appVersion) ? ['backup'] : [],
+        capabilities: isLinkAndSyncEnabled(this.#appVersion) ? ['backup2'] : [],
       })
       .toString();
 
