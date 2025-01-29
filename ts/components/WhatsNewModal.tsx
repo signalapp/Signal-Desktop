73682eb4e29b356721d84ae9e10df017bc0c25f9
Yash <yash@signal.org>
2025-01-29 14:09:44 -0600

    release notes for 7.41

--- ts/components/WhatsNewModal.tsx
+++ ts/components/WhatsNewModal.tsx
@@ -39,12 +39,10 @@ export function WhatsNewModal({
 }: PropsType): JSX.Element {
   let contentNode: ReactNode;
 
-  // Use later: icu:WhatsNew__v7.40--1
-
   const releaseNotes: ReleaseNotesType = {
     date: new Date(window.getBuildCreation?.() || Date.now()),
     version: window.getVersion?.(),
-    features: [<I18n i18n={i18n} id="icu:WhatsNew__v7.40--0" />],
+    features: [<I18n i18n={i18n} id="icu:WhatsNew__v7.40--1" />],
   };
 
   if (releaseNotes.features.length === 1 && !releaseNotes.header) {
