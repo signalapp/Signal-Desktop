419692d5487807ae39869a8284fc7fc2a26af822
Fedor Indutny <79877362+indutny-signal@users.noreply.github.com>
2025-02-27 13:46:37 -0800

    Show update UI on asar changes on Linux

--- ts/updater/linux.ts
+++ ts/updater/linux.ts
@@ -1,11 +1,24 @@
 // Copyright 2023 Signal Messenger, LLC
 // SPDX-License-Identifier: AGPL-3.0-only
 
-import { version as osVersion } from 'os';
+import { version as osVersion } from 'node:os';
+import { watch } from 'node:fs';
+import { join } from 'node:path';
+import { app, ipcMain } from 'electron';
+import { extractFile } from '@electron/asar';
+import z from 'zod';
 
+import { markShouldQuit } from '../../app/window_state';
 import type { LoggerType } from '../types/Logging';
+import { DialogType } from '../types/Dialogs';
+import * as Errors from '../types/errors';
+import type { UpdaterOptionsType } from './common';
 
 const MIN_UBUNTU_VERSION = '20.04';
+
+const PackageSchema = z.object({
+  version: z.string(),
+});
 
 export function getUbuntuVersion(): string | undefined {
   if (process.platform !== 'linux') {
@@ -30,4 +43,56 @@ export function isLinuxVersionSupported(logger?: LoggerType): boolean {
   }
 
   return true;
+}
+
+export function initLinux({ logger, getMainWindow }: UpdaterOptionsType): void {
+  if (!app.isPackaged) {
+    throw new Error('Linux updates are not supported in development');
+  }
+
+  if (!isLinuxVersionSupported(logger)) {
+    getMainWindow()?.webContents.send(
+      'show-update-dialog',
+      DialogType.UnsupportedOS
+    );
+  }
+
+  ipcMain.handle('start-update', () => {
+    logger?.info('updater/linux: restarting');
+    markShouldQuit();
+    app.relaunch();
+    app.quit();
+  });
+
+  const asarPath = join(__dirname, '..', '..');
+  if (!asarPath.endsWith('.asar')) {
+    throw new Error('updater/linux: not running from ASAR');
+  }
+
+  watch(asarPath, event => {
+    if (event !== 'change') {
+      return;
+    }
+
+    let version: string;
+    try {
+      const file = extractFile(asarPath, 'package.json').toString();
+      ({ version } = PackageSchema.parse(JSON.parse(file)));
+    } catch (error) {
+      logger?.error(
+        'updater/linux: failed to parse updated asar',
+        Errors.toLogFormat(error)
+      );
+      return;
+    }
+
+    logger?.info(`updater/linux: asar updated to version=${version}`);
+    getMainWindow()?.webContents.send(
+      'show-update-dialog',
+      DialogType.AutoUpdate,
+      {
+        version,
+      }
+    );
+  }).unref();
 }
