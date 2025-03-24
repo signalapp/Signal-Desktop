c94849d3a1f24f77a4b91939653ba8a3c607d988
ayumi-signal <143036029+ayumi-signal@users.noreply.github.com>
2025-03-24 10:19:02 -0700

    Improve error messaging for startup database error

---
+++ ts/util/getAppErrorIcon.ts
@@ -0,0 +1,17 @@
+// Copyright 2025 Signal Messenger, LLC
+// SPDX-License-Identifier: AGPL-3.0-only
+
+import { nativeImage } from 'electron';
+import type { NativeImage } from 'electron';
+import { join } from 'path';
+
+export function getAppErrorIcon(): NativeImage {
+  const iconPath = join(
+    __dirname,
+    '..',
+    '..',
+    'images',
+    'app-icon-with-error.png'
+  );
+  return nativeImage.createFromPath(iconPath);
+}
