3b51156e901120ce84ad9a9087e625963c22b6cd
trevor-signal <131492920+trevor-signal@users.noreply.github.com>
2025-04-17 12:03:35 -0400

    Update feature flag setup for funPicker

--- ts/components/fun/isFunPickerEnabled.tsx
+++ ts/components/fun/isFunPickerEnabled.tsx
@@ -1,20 +1,7 @@
 // Copyright 2025 Signal Messenger, LLC
 // SPDX-License-Identifier: AGPL-3.0-only
 import * as RemoteConfig from '../../RemoteConfig';
-import { isBeta, isProduction } from '../../util/version';
 
 export function isFunPickerEnabled(): boolean {
-  const version = window.getVersion?.();
-
-  if (version != null) {
-    if (isProduction(version)) {
-      return RemoteConfig.isEnabled('desktop.funPicker.prod');
-    }
-
-    if (isBeta(version)) {
-      return RemoteConfig.isEnabled('desktop.funPicker.beta');
-    }
-  }
-
   return RemoteConfig.isEnabled('desktop.funPicker');
 }
