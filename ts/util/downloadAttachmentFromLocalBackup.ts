a75a0f91436741d6f17c5bdac8530a6594c52bb1
ayumi-signal <143036029+ayumi-signal@users.noreply.github.com>
2025-05-15 08:27:46 -0700

    Handle locally missing attachments for local backups

--- ts/util/downloadAttachmentFromLocalBackup.ts
+++ ts/util/downloadAttachmentFromLocalBackup.ts
@@ -1,13 +1,12 @@
 // Copyright 2025 Signal Messenger, LLC
 // SPDX-License-Identifier: AGPL-3.0-only
 
+import { existsSync } from 'node:fs';
 import { isNumber } from 'lodash';
 import {
   type AttachmentType,
   getAttachmentIdForLogging,
 } from '../types/Attachment';
-import * as log from '../logging/log';
-import { toLogFormat } from '../types/errors';
 import {
   decryptAndReencryptLocally,
   type ReencryptedAttachmentV2,
@@ -23,15 +22,7 @@ export async function downloadAttachmentFromLocalBackup(
   const dataId = `${attachmentId}`;
   const logId = `downloadAttachmentFromLocalBackup(${dataId})`;
 
-  try {
-    return await doDownloadFromLocalBackup(attachment, { logId });
-  } catch (error) {
-    log.error(
-      `${logId}: error when copying from local backup`,
-      toLogFormat(error)
-    );
-    throw new AttachmentPermanentlyUndownloadableError();
-  }
+  return doDownloadFromLocalBackup(attachment, { logId });
 }
 
 async function doDownloadFromLocalBackup(
@@ -48,6 +39,12 @@ async function doDownloadFromLocalBackup(
   strictAssert(localKey, `${logId}: missing localKey`);
   strictAssert(localBackupPath, `${logId}: missing localBackupPath`);
   strictAssert(isNumber(size), `${logId}: missing size`);
+
+  if (!existsSync(localBackupPath)) {
+    throw new AttachmentPermanentlyUndownloadableError(
+      'No file at attachment localBackupPath'
+    );
+  }
 
   return decryptAndReencryptLocally({
     type: 'local',
