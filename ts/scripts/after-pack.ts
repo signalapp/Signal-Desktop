3219a722a77b0a074676b137a0240a5cf5e156bb
automated-signal <37887102+automated-signal@users.noreply.github.com>
2025-09-30 18:36:58 -0500

    Ensure file permissions when building for Linux and set sha length for test builds
    Co-authored-by: ayumi-signal <143036029+ayumi-signal@users.noreply.github.com>

--- ts/scripts/after-pack.ts
+++ ts/scripts/after-pack.ts
@@ -5,9 +5,11 @@ import type { AfterPackContext } from 'electron-builder';
 import { afterPack as fuseElectron } from './fuse-electron.js';
 import { afterPack as copyPacks } from './copy-language-packs.js';
 import { afterPack as pruneMacOSRelease } from './prune-macos-release.js';
+import { afterPack as ensureLinuxFilePermissions } from './ensure-linux-file-permissions.js';
 
 export async function afterPack(context: AfterPackContext): Promise<void> {
   await pruneMacOSRelease(context);
   await fuseElectron(context);
   await copyPacks(context);
+  await ensureLinuxFilePermissions(context);
 }
