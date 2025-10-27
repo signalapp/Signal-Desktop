39081785d46aa5615e1444843e77019c5278f357
Fedor Indutny <79877362+indutny-signal@users.noreply.github.com>
2025-10-27 14:59:40 -0700

    Upgrade electron to 38.4.0, electron-builder to 26.1.0

--- scripts/prepare_alpha_build.js
+++ scripts/prepare_alpha_build.js
@@ -45,6 +45,10 @@ const DESKTOP_NAME_PATH = 'desktopName';
 const PRODUCTION_DESKTOP_NAME = 'signal.desktop';
 const ALPHA_DESKTOP_NAME = 'signal alpha.desktop';
 
+const EXECUTABLE_NAME_PATH = 'build.linux.executableName';
+const PRODUCTION_EXECUTABLE_NAME = 'signal-desktop';
+const ALPHA_EXECUTABLE_NAME = 'signal-desktop-alpha';
+
 // -------
 
 function checkValue(object, objectPath, expected) {
@@ -61,6 +65,7 @@ checkValue(packageJson, PRODUCT_NAME_PATH, PRODUCTION_PRODUCT_NAME);
 checkValue(packageJson, APP_ID_PATH, PRODUCTION_APP_ID);
 checkValue(packageJson, STARTUP_WM_CLASS_PATH, PRODUCTION_WM_CLASS);
 checkValue(packageJson, DESKTOP_NAME_PATH, PRODUCTION_DESKTOP_NAME);
+checkValue(packageJson, EXECUTABLE_NAME_PATH, PRODUCTION_EXECUTABLE_NAME);
 
 // -------
 
@@ -69,6 +74,7 @@ _.set(packageJson, PRODUCT_NAME_PATH, ALPHA_PRODUCT_NAME);
 _.set(packageJson, APP_ID_PATH, ALPHA_APP_ID);
 _.set(packageJson, STARTUP_WM_CLASS_PATH, ALPHA_WM_CLASS);
 _.set(packageJson, DESKTOP_NAME_PATH, ALPHA_DESKTOP_NAME);
+_.set(packageJson, EXECUTABLE_NAME_PATH, ALPHA_EXECUTABLE_NAME);
 
 // -------
 
