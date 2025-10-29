b2f8f671ab3522fbb9e801b443ac48c4244bcd1b
automated-signal <37887102+automated-signal@users.noreply.github.com>
2025-10-29 18:09:57 -0500

    Chat folder fixes
    Co-authored-by: Jamie <113370520+jamiebuilds-signal@users.noreply.github.com>

--- ts/state/smart/LeftPaneChatFolders.preload.tsx
+++ ts/state/smart/LeftPaneChatFolders.preload.tsx
@@ -33,7 +33,7 @@ export const SmartLeftPaneChatFolders = memo(
     );
     const location = useSelector(getSelectedLocation);
 
-    const { updateSelectedChangeFolderId } = useChatFolderActions();
+    const { updateSelectedChatFolderId } = useChatFolderActions();
     const { changeLocation } = useNavActions();
     const { markChatFolderRead, setChatFolderMuteExpiration } =
       useConversationsActions();
@@ -66,7 +66,7 @@ export const SmartLeftPaneChatFolders = memo(
         allChatFoldersUnreadStats={allChatFoldersUnreadStats}
         allChatFoldersMutedStats={allChatFoldersMutedStats}
         selectedChatFolder={selectedChatFolder}
-        onSelectedChatFolderIdChange={updateSelectedChangeFolderId}
+        onSelectedChatFolderIdChange={updateSelectedChatFolderId}
         onChatFolderMarkRead={markChatFolderRead}
         onChatFolderUpdateMute={setChatFolderMuteExpiration}
         onChatFolderOpenSettings={handleChatFolderOpenSettings}
