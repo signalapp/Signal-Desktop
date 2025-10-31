332f19ac78b9ee42a50629c58d0f41b23e0beace
yash-signal <yash@signal.org>
2025-10-31 12:01:10 -0500

    Fix reply/quoting breaking for poll messages

--- ts/util/getQuoteBodyText.std.ts
+++ ts/util/getQuoteBodyText.std.ts
@@ -2,12 +2,18 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 
 import type { ReadonlyMessageAttributesType } from '../model-types.d.ts';
+import type { LocalizerType } from '../types/Util.std.js';
 import * as EmbeddedContact from '../types/EmbeddedContact.std.js';
 
-export function getQuoteBodyText(
-  messageAttributes: ReadonlyMessageAttributesType,
-  id: number | null
-): string | undefined {
+export function getQuoteBodyText({
+  messageAttributes,
+  id,
+  i18n,
+}: {
+  messageAttributes: ReadonlyMessageAttributesType;
+  id: number | null;
+  i18n: LocalizerType;
+}): string | undefined {
   const storyReactionEmoji = messageAttributes.storyReaction?.emoji;
 
   if (id != null) {
@@ -20,11 +26,17 @@ export function getQuoteBodyText(
     }
   }
 
-  const { body, contact: embeddedContact } = messageAttributes;
+  const { body, contact: embeddedContact, poll } = messageAttributes;
   const embeddedContactName =
     embeddedContact && embeddedContact.length > 0
       ? EmbeddedContact.getName(embeddedContact[0])
       : '';
 
-  return body || embeddedContactName || storyReactionEmoji;
+  const pollText = poll
+    ? i18n('icu:Poll--preview', {
+        pollQuestion: poll.question,
+      })
+    : undefined;
+
+  return body || embeddedContactName || pollText || storyReactionEmoji;
 }
