347ad6645ac6a89842f087d98e298c99c634db79
automated-signal <37887102+automated-signal@users.noreply.github.com>
2025-11-14 08:35:53 -0600

    Fix loading screen theme
    Co-authored-by: trevor-signal <131492920+trevor-signal@users.noreply.github.com>

--- ts/windows/loading/start.dom.ts
+++ ts/windows/loading/start.dom.ts
@@ -1,5 +1,6 @@
 // Copyright 2020 Signal Messenger, LLC
 // SPDX-License-Identifier: AGPL-3.0-only
+import '../sandboxedInit.dom.js';
 
 const message = document.getElementById('message');
 if (message) {
