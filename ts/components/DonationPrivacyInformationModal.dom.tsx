6862dc58a27c8f83739b0a79e92b66a0b3d7b670
automated-signal <37887102+automated-signal@users.noreply.github.com>
2026-02-11 09:15:29 -0600

    Update donation faq dialog
    Co-authored-by: ayumi-signal <143036029+ayumi-signal@users.noreply.github.com>

--- ts/components/DonationPrivacyInformationModal.dom.tsx
+++ ts/components/DonationPrivacyInformationModal.dom.tsx
@@ -1,12 +1,12 @@
 // Copyright 2025 Signal Messenger, LLC
 // SPDX-License-Identifier: AGPL-3.0-only
 
-import React, { useCallback } from 'react';
+import React from 'react';
 import type { LocalizerType } from '../types/Util.std.js';
 import { openLinkInWebBrowser } from '../util/openLinkInWebBrowser.dom.js';
 import { AxoDialog } from '../axo/AxoDialog.dom.js';
-import { I18n } from './I18n.dom.js';
 import { tw } from '../axo/tw.dom.js';
+import { AxoSymbol } from '../axo/AxoSymbol.dom.js';
 
 export type DonationPrivacyInformationModalProps = {
   i18n: LocalizerType;
@@ -23,11 +23,6 @@ export function DonationPrivacyInformationModal({
     );
   };
 
-  const paragraphComponent = useCallback(
-    (parts: Array<string | React.JSX.Element>) => <p>{parts}</p>,
-    []
-  );
-
   return (
     <AxoDialog.Root
       open
@@ -43,20 +38,49 @@ export function DonationPrivacyInformationModal({
         disableMissingAriaDescriptionWarning
       >
         <AxoDialog.Header>
-          <AxoDialog.Title>
+          <AxoDialog.Title screenReaderOnly>
             {i18n('icu:PreferencesDonations__privacy-modal-title')}
           </AxoDialog.Title>
           <AxoDialog.Close aria-label={i18n('icu:PinMessageDialog__Close')} />
         </AxoDialog.Header>
         <AxoDialog.Body>
+          <img
+            className={tw('m-auto mb-3')}
+            alt={i18n(
+              'icu:PreferencesDonations__privacy-modal-icon-accessibility-label'
+            )}
+            src="images/signal-heart.svg"
+          />
           <div className={tw('flex flex-col gap-4 pt-1.5 pb-4')}>
-            <I18n
-              components={{
-                paragraph: paragraphComponent,
-              }}
-              i18n={i18n}
-              id="icu:PreferencesDonations__privacy-modal-content"
-            />
+            <div className={tw('mt-1 mb-2')}>
+              {i18n('icu:PreferencesDonations__privacy-modal-intro')}
+            </div>
+            <ul className={tw('flex flex-col gap-6')}>
+              <li className={tw('flex flex-row gap-4')}>
+                <div>
+                  <AxoSymbol.Icon size={20} symbol="badge-set" label={null} />
+                </div>
+                <div>
+                  {i18n('icu:PreferencesDonations__privacy-modal-list-1')}
+                </div>
+              </li>
+              <li className={tw('flex gap-x-4')}>
+                <div>
+                  <AxoSymbol.Icon size={20} symbol="lock" label={null} />
+                </div>
+                <div>
+                  {i18n('icu:PreferencesDonations__privacy-modal-list-2')}
+                </div>
+              </li>
+              <li className={tw('flex gap-x-4')}>
+                <div>
+                  <AxoSymbol.Icon size={20} symbol="heart" label={null} />
+                </div>
+                <div>
+                  {i18n('icu:PreferencesDonations__privacy-modal-list-3')}
+                </div>
+              </li>
+            </ul>
           </div>
         </AxoDialog.Body>
         <AxoDialog.Footer>
