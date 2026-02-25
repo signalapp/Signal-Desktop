98260fc646e15a90208d95c3494e08b4260b4125
Scott Nonnenberg <scott@signal.org>
2026-02-25 15:06:34 -0800

    Release notes for 8.1

--- ts/components/WhatsNewModal.dom.tsx
+++ ts/components/WhatsNewModal.dom.tsx
@@ -43,7 +43,7 @@ export function WhatsNewModal({
   const releaseNotes: ReleaseNotesType = {
     date: new Date(window.getBuildCreation?.() || Date.now()),
     version: window.getVersion?.(),
-    features: [<I18n i18n={i18n} id="icu:WhatsNew__8.0--0" />],
+    features: [<I18n i18n={i18n} id="icu:WhatsNew__8.1--0" />],
   };
 
   if (releaseNotes.features.length === 1 && !releaseNotes.header) {
