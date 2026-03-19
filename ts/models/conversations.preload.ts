4788298e50dd39d6d0271faff42760a7dc54b51d
Indibar Sarkar <indibarsarkar37@gmail.com>
2026-03-19 22:36:52 +0530

    fix: include quote header when sending a sticker reply

--- ts/models/conversations.preload.ts
+++ ts/models/conversations.preload.ts
@@ -3991,7 +3991,14 @@ export class ConversationModel {
     return getQuoteAttachment(attachments, preview, sticker);
   }
 
-  async sendStickerMessage(packId: string, stickerId: number): Promise<void> {
+  async sendStickerMessage(
+    packId: string,
+    stickerId: number,
+    options: {
+      quote?: QuoteType;
+      extraReduxActions?: () => void;
+    } = {}
+  ): Promise<void> {
     const packData = Stickers.getStickerPack(packId);
     const stickerData = Stickers.getSticker(packId, stickerId);
     if (!stickerData || !packData) {
@@ -4047,8 +4054,12 @@ export class ConversationModel {
           body: undefined,
           attachments: [],
           sticker,
+          quote: options.quote,
         },
-        { dontClearDraft: true }
+        {
+          dontClearDraft: true,
+          extraReduxActions: options.extraReduxActions,
+        }
       )
     );
     window.reduxActions.stickers.useSticker(packId, stickerId);
