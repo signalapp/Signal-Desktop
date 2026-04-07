159712ee2402e47e0a4deb7e86796c996c7c9171
Fedor Indutny <79877362+indutny-signal@users.noreply.github.com>
2026-04-07 11:21:01 -0700

    Fix schema of whoami response

--- ts/textsecure/WebAPI.preload.ts
+++ ts/textsecure/WebAPI.preload.ts
@@ -1014,7 +1014,7 @@ export type GetSenderCertificateResultType = Readonly<{ certificate: string }>;
 
 const whoamiResultZod = z.object({
   uuid: z.string(),
-  pni: z.string(),
+  pni: untaggedPniSchema,
   number: z.string(),
   usernameHash: z.string().nullish(),
   usernameLinkHandle: z.string().nullish(),
