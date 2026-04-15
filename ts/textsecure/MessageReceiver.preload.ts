1891adbb19d87f9e96def54171fc9ab7d73e43ac
automated-signal <37887102+automated-signal@users.noreply.github.com>
2026-04-15 11:52:03 -0500

    Bump libsignal to 0.92.1
    Co-authored-by: andrew-signal <andrew@signal.org>
    Co-authored-by: Fedor Indutny <indutny@signal.org>

--- ts/textsecure/MessageReceiver.preload.ts
+++ ts/textsecure/MessageReceiver.preload.ts
@@ -1773,9 +1773,8 @@ export default class MessageReceiver
       Address.create(sealedSenderIdentifier, envelope.sourceDevice)
     );
 
-    const ourAci = this.#storage.user.getCheckedAci();
     const ourDeviceID = this.#storage.user.getCheckedDeviceId();
-    const localAddress = ProtocolAddress.new(ourAci, ourDeviceID);
+    const localAddress = ProtocolAddress.new(destinationServiceId, ourDeviceID);
     const sourceAddress = ProtocolAddress.new(
       sealedSenderIdentifier,
       envelope.sourceDevice
@@ -1909,9 +1908,11 @@ export default class MessageReceiver
       }
       const preKeySignalMessage = PreKeySignalMessage.deserialize(ciphertext);
 
-      const ourAci = this.#storage.user.getCheckedAci();
       const ourDeviceID = this.#storage.user.getCheckedDeviceId();
-      const localAddress = ProtocolAddress.new(ourAci, ourDeviceID);
+      const localAddress = ProtocolAddress.new(
+        destinationServiceId,
+        ourDeviceID
+      );
       const sourceAddress = ProtocolAddress.new(identifier, sourceDevice);
 
       const plaintext = await signalProtocolStore.enqueueSessionJob(
