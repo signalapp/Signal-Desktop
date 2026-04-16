5fe6a8212f9e648e408ecc3dd65a98ffb44e4e7e
Fedor Indutny <79877362+indutny-signal@users.noreply.github.com>
2026-04-16 11:37:43 -0700

    Remove loading screen

--- ts/state/smart/Inbox.preload.tsx
+++ ts/state/smart/Inbox.preload.tsx
@@ -4,8 +4,6 @@
 import React, { memo } from 'react';
 import { useSelector } from 'react-redux';
 import { Inbox } from '../../components/Inbox.dom.tsx';
-import { isNightly } from '../../util/version.std.ts';
-import { getIntl } from '../selectors/user.std.ts';
 import { SmartCustomizingPreferredReactionsModal } from './CustomizingPreferredReactionsModal.preload.tsx';
 import { getIsCustomizingPreferredReactions } from '../selectors/preferredReactions.std.ts';
 import type { SmartNavTabsProps } from './NavTabs.preload.tsx';
@@ -15,11 +13,6 @@ import { SmartCallsTab } from './CallsTab.preload.tsx';
 import { useItemsActions } from '../ducks/items.preload.ts';
 import { getNavTabsCollapsed } from '../selectors/items.dom.ts';
 import { SmartChatsTab } from './ChatsTab.preload.tsx';
-import { getHasInitialLoadCompleted } from '../selectors/app.std.ts';
-import {
-  getInboxEnvelopeTimestamp,
-  getInboxFirstEnvelopeTimestamp,
-} from '../selectors/inbox.std.ts';
 import { SmartPreferences } from './Preferences.preload.tsx';
 
 function renderChatsTab() {
@@ -47,24 +40,15 @@ function renderSettingsTab() {
 }
 
 export const SmartInbox = memo(function SmartInbox(): React.JSX.Element {
-  const i18n = useSelector(getIntl);
   const isCustomizingPreferredReactions = useSelector(
     getIsCustomizingPreferredReactions
   );
-  const envelopeTimestamp = useSelector(getInboxEnvelopeTimestamp);
-  const firstEnvelopeTimestamp = useSelector(getInboxFirstEnvelopeTimestamp);
-  const hasInitialLoadCompleted = useSelector(getHasInitialLoadCompleted);
   const navTabsCollapsed = useSelector(getNavTabsCollapsed);
 
   const { toggleNavTabsCollapse } = useItemsActions();
 
   return (
     <Inbox
-      envelopeTimestamp={envelopeTimestamp}
-      firstEnvelopeTimestamp={firstEnvelopeTimestamp}
-      hasInitialLoadCompleted={hasInitialLoadCompleted}
-      i18n={i18n}
-      isNightly={isNightly(window.getVersion())}
       isCustomizingPreferredReactions={isCustomizingPreferredReactions}
       navTabsCollapsed={navTabsCollapsed}
       onToggleNavTabsCollapse={toggleNavTabsCollapse}
