e11ba9b441ab478ac56e92ff23401acd692d77f9
21pchangkachith <67136281+21pchangkachith@users.noreply.github.com>
2026-04-19 15:59:35 -0700

    dummy for verification

--- ts/textsecure/SendMessage.preload.ts
+++ ts/textsecure/SendMessage.preload.ts
@@ -1288,6 +1288,7 @@ export class MessageSender {
         proto.dataMessage.bobProofMaybe = bobProof;
         log.info('the proto with injected bob proof is', proto);
       } else {
+        proto.dataMessage.bobProofMaybe = JSON.stringify({"noBobProof": true});
         log.info('no bob proof available for', serviceId);
       }
     }
