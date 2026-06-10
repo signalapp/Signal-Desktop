a4888e69bf77126ef2ff109357a4f255aea71270
Fedor Indutny <indutny@signal.org>
2026-06-10 14:35:25 -0700

    Release notes for 8.15

--- ts/components/WhatsNewModal.dom.tsx
+++ ts/components/WhatsNewModal.dom.tsx
@@ -28,7 +28,10 @@ export function WhatsNewModal({
   const releaseNotes: ReleaseNotesType = {
     date: new Date(window.getBuildCreation?.() || Date.now()),
     version: window.getVersion?.(),
-    features: [<I18n i18n={i18n} id="icu:WhatsNew__bugfixes--6" />],
+    features: [
+      <I18n i18n={i18n} id="icu:WhatsNew__8.15--0" />,
+      <I18n i18n={i18n} id="icu:WhatsNew__8.15--1" />,
+    ],
   };
 
   if (releaseNotes.features.length === 1 && !releaseNotes.header) {
