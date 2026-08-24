cb78bb2fb5aa0ee657d6d943da2b6ce0c222e406
automated-signal <37887102+automated-signal@users.noreply.github.com>
2026-08-24 13:04:53 -0500

    Fix all Sass deprecation warnings
    Co-authored-by: Jamie <79877362+indutny-signal@users.noreply.github.com>

--- sticker-creator/vite.config.ts
+++ sticker-creator/vite.config.ts
@@ -3,12 +3,21 @@
 
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import * as sass from 'sass';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   css: {
     modules: {
       localsConvention: 'camelCaseOnly',
+    },
+    preprocessorOptions: {
+      scss: {
+        // setting the version number is not working for some reason
+        fatalDeprecations: Object.values(sass.deprecations)
+          .filter(dep => dep.status !== 'obsolete')
+          .map(dep => dep.id),
+      },
     },
   },
   worker: {
